993e76b7b9e247338b6ba1096e74d1e4c8cb3933
Alexander Schmidt <2f5ed1c5@opayq.com>
2016-09-16 16:51:09 +0200

    small source code update

--- application/modules/default/views/helpers/FetchProjectFileTypes.php
+++ application/modules/default/views/helpers/FetchProjectFileTypes.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *  ocs-webserver
  *
@@ -19,13 +20,13 @@
  *    You should have received a copy of the GNU Affero General Public License
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **/
-
 class Default_View_Helper_FetchProjectFileTypes extends Zend_View_Helper_Abstract
 {
+
     public function FetchProjectFileTypes($projectId)
-    {        
-    	
-    	 $tbl = new Default_Model_DbTable_ProjectFileType();
-    	 return $tbl->getProjectFileTypesString($projectId);
+    {
+        $tbl = new Default_Model_DbTable_ProjectFileType();
+        return $tbl->getProjectFileTypesString($projectId);
     }
+
 }
