7ac862403ca56e976269b4961a611e46d7287ad5
Ronald von Schlegell <rvs75@email.de>
2016-09-19 17:07:33 +0200

    rename filetype

--- sql_code/20160919_file_type_rename.sql
+++ sql_code/20160919_file_type_rename.sql
@@ -3,9 +3,10 @@ ALTER TABLE `package_types`
 	CHANGE COLUMN `filetype_id` `package_type_id` INT(11) NOT NULL AUTO_INCREMENT FIRST;
 
 
+
 ALTER TABLE `project_file_type`
 	ALTER `filetype_id` DROP DEFAULT;
 ALTER TABLE `project_file_type`
 	CHANGE COLUMN `filetype_id` `package_type_id` INT(11) NOT NULL AFTER `file_id`;
 
-
+RENAME TABLE `project_file_type` TO `project_package_type`;
