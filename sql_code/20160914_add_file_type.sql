58fc25a6e055942e47ea5a9a4374b046395f9d68
Alexander Schmidt <2f5ed1c5@opayq.com>
2016-10-07 18:13:42 +0200

    rss feed add category after productname

--- sql_code/20160914_add_file_type.sql
+++ sql_code/20160914_add_file_type.sql
@@ -5,7 +5,6 @@ CREATE TABLE `file_types` (
 	`is_active` INT(1) NULL DEFAULT '1',
 	PRIMARY KEY (`filetype_id`)
 )
-COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 ;
 CREATE TABLE `project_file_type` (
@@ -14,14 +13,13 @@ CREATE TABLE `project_file_type` (
 	`file_type_id` INT NOT NULL,
 	PRIMARY KEY (`project_id`, `file_id`)
 )
-COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 ;
 
-INSERT INTO `pling-import`.`file_types` (`filetype_id`, `text`, `order`) VALUES ('1', 'AppImage', '1');
-INSERT INTO `pling-import`.`file_types` (`filetype_id`, `text`, `order`) VALUES ('2', 'Android (APK)', '2');
-INSERT INTO `pling-import`.`file_types` (`filetype_id`, `text`, `order`) VALUES ('3', 'OS X compatible', '3');
-INSERT INTO `pling-import`.`file_types` (`filetype_id`, `text`, `order`) VALUES ('4', 'Windows executable', '4');
+INSERT INTO `file_types` (`filetype_id`, `text`, `order`) VALUES ('1', 'AppImage', '1');
+INSERT INTO `file_types` (`filetype_id`, `text`, `order`) VALUES ('2', 'Android (APK)', '2');
+INSERT INTO `file_types` (`filetype_id`, `text`, `order`) VALUES ('3', 'OS X compatible', '3');
+INSERT INTO `file_types` (`filetype_id`, `text`, `order`) VALUES ('4', 'Windows executable', '4');
 
 ALTER TABLE `file_types`
 CHANGE COLUMN `text` `name` VARCHAR(255) NOT NULL ;
