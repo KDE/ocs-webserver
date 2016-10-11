41a588c43be3fb51950b84b3ecd3915baf44dfe3
Alexander Schmidt <2f5ed1c5@opayq.com>
2016-10-11 20:47:56 +0200

    updated sql for creating index

--- sql_code/20161011_add_index_stat_table_views.sql
+++ sql_code/20161011_add_index_stat_table_views.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `stat_page_views`
-ADD INDEX `idx_created` (`created_at` DESC, `member_id` ASC);
-
 ALTER TABLE `activity_log`
-  ADD INDEX `idx_time` (`time` DESC, `member_id` ASC);
+  ADD INDEX `idx_time` (`member_id` ASC,`time` DESC);
+
+ALTER TABLE `stat_page_views`
+  ADD INDEX `idx_member` (`member_id` ASC, `created_at` ASC);
