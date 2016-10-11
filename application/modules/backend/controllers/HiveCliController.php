6898c31c174843ea9ff3a18dcbb6403e84026452
Alexander Schmidt <2f5ed1c5@opayq.com>
2016-10-11 14:37:41 +0200

    dev branch synchronized with master

--- application/modules/backend/controllers/HiveCliController.php
+++ application/modules/backend/controllers/HiveCliController.php
@@ -30,6 +30,7 @@ class Backend_HiveCliController extends Local_Controller_Action_CliAbstract
     //var $hive_import_categories = array(15,8,64,8300,7700,8000,8100,7800,8400,8700,7600,7900,7500);
     //var $hive_import_categories = arra(25);
     //var $hive_import_categories = array(648, 692, 649);
+    //var $hive_import_categories = array(322,323,324,325,326);
     //var $hive_import_categories = array(637);
     var $hive_import_categories = array(635);
     protected $_allowed = array(
