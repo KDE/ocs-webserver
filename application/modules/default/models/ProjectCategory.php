f7995d5f51cbd3955630593fcccd6e1994675590
Alexander Schmidt <2f5ed1c5@opayq.com>
2017-04-10 20:19:44 +0200

    Hotfix: updated explore page
    (cherry picked from commit 3af445de41b253f93698027ba821db35d4adb105)

---
+++ application/modules/default/models/ProjectCategory.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ *  ocs-webserver
+ *
+ *  Copyright 2016 by pling GmbH.
+ *
+ *    This file is part of ocs-webserver.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+class Default_Model_ProjectCategory
+{
+
+    /** @var string */
+    protected $_dataTableName;
+    /** @var  Default_Model_DbTable_ProjectCategory */
+    protected $_dataTable;
+
+    /**
+     * PHP 5 allows developers to declare constructor methods for classes.
+     * Classes which have a constructor method call this method on each newly-created object,
+     * so it is suitable for any initialization that the object may need before it is used.
+     *
+     * Note: Parent constructors are not called implicitly if the child class defines a constructor.
+     * In order to run a parent constructor, a call to parent::__construct() within the child constructor is required.
+     *
+     * param [ mixed $args [, $... ]]
+     * @param string $_dataTableName
+     * @link http://php.net/manual/en/language.oop5.decon.php
+     */
+    public function __construct($_dataTableName = 'Default_Model_DbTable_ProjectCategory')
+    {
+        $this->_dataTableName = $_dataTableName;
+        $this->_dataTable = new $this->_dataTableName;
+    }
+
+    public static function fetchCatIdsForCurrentStore()
+    {
+        return Zend_Registry::isRegistered('store_category_list') ? Zend_Registry::get('store_category_list') : null;
+    }
+
+    /**
+     * @param int|null $store_id If not set, the tree for the current store will be returned
+     * @return array
+     */
+    public function fetchCategoryTreeForStore($store_id = null)
+    {
+        if (empty($store_id)) {
+            $store_id = Zend_Registry::get('store_config')['store_id'];
+        }
+
+        /** @var Zend_Cache_Core $cache */
+        $cache = Zend_Registry::get('cache');
+        $cache_id = __FUNCTION__ . '_' . $store_id;
+
+        if (false === ($tree = $cache->load($cache_id))) {
+            $modelCategoryStore = new Default_Model_DbTable_ConfigStoreCategory();
+            $rows = $modelCategoryStore->fetchAllCategoriesForStore((int)$store_id);
+            $tree = $this->buildTree($rows);
+            $cache->save($tree, $cache_id, array(), 300);
+        }
+
+        return $tree;
+    }
+
+    public function fetchCategoryTreeCurrentStore()
+    {
+        $store_id = Zend_Registry::get('store_config')['store_id'];
+
+        /** @var Zend_Cache_Core $cache */
+        $cache = Zend_Registry::get('cache');
+        $cache_id = __FUNCTION__ . '_' . $store_id;
+
+        if (false === ($tree = $cache->load($cache_id))) {
+            $rows = self::fetchCatIdsForCurrentStore();
+            $tree = $this->buildTree($rows);
+            $cache->save($tree, $cache_id, array(), 300);
+        }
+
+        return $tree;
+    }
+
+    private function buildTree($list)
+    {
+        if (false === is_array($list)) {
+            $list = array($list);
+        }
+        $modelCategories = new Default_Model_DbTable_ProjectCategory();
+        $modelProject = new Default_Model_Project();
+        $result = array();
+        foreach ($list as $cat_id) {
+            $currentCategory = $modelCategories->fetchElement($cat_id);
+            $countProduct = $modelProject->countProductsInCategory($cat_id, true);
+
+            $result_element = array('id'=> $cat_id, 'title' => $currentCategory['title'], 'product_count' => $countProduct, 'has_children' => false);
+            //has children?
+            if (($currentCategory['rgt'] - $currentCategory['lft']) > 1) {
+                $result_element['has_children'] = true;
+                $ids = $modelCategories->fetchImmediateChildrenIds($currentCategory['project_category_id'], $modelCategories::ORDERED_TITLE);
+                $result_element['children'] = $this->buildTree($ids);
+            }
+            $result[] = $result_element;
+        }
+        return $result;
+    }
+
+}
