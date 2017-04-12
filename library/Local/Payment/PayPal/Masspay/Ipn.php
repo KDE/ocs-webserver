2c4b7b5335aefc38d3b0f98b7cb38a68a804c2c9
Ronald von Schlegell <rvs75@email.de>
2017-04-12 16:46:33 +0200

    masspay

--- library/Local/Payment/PayPal/Masspay/Ipn.php
+++ library/Local/Payment/PayPal/Masspay/Ipn.php
@@ -120,10 +120,10 @@ abstract class Local_Payment_PayPal_Masspay_Ipn extends Local_Payment_PayPal_Bas
         $this->_logger->info('Masspay '.__FUNCTION__ . '::IPN verify result: ' . print_r($res));
         
         // inspect IPN validation result and act accordingly
-        if (strcmp ($res, "VERIFIED") == 0) {
+        if ($res == 1) {
             // The IPN is verified, process it
             return true;
-        } else if (strcmp ($res, "INVALID") == 0) {
+        } else {
             // IPN invalid, log for manual investigation
             return false;
         }
