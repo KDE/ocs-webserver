1dfd8dbf642865db4ff2785925043307fcae811d
Alexander Schmidt <2f5ed1c5@opayq.com>
2017-04-19 18:41:22 +0200

    users back end: payout tab updated

--- application/modules/default/models/StatDownload.php
+++ application/modules/default/models/StatDownload.php
@@ -39,12 +39,15 @@ class Default_Model_StatDownload
                     member_dl_plings.*,
                     project.title,
                     project.image_small,
+                    project_category.title as cat_title,
                     member_payout.amount,
                     member_payout.`status`
                 FROM
                     member_dl_plings
                         STRAIGHT_JOIN
                     project ON project.project_id = member_dl_plings.project_id
+                        STRAIGHT_JOIN 
+                    project_category ON project_category.project_category_id = member_dl_plings.project_category_id
                         LEFT JOIN
                     member_payout ON member_payout.member_id = member_dl_plings.member_id
                         AND member_payout.yearmonth = member_dl_plings.yearmonth
