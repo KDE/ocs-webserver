68d25d22c4f46735dc8fa1fa7f5ea4906574e639
Ronald von Schlegell <rvs75@email.de>
2017-04-25 15:53:28 +0200

    payout

--- application/modules/default/controllers/UserController.php
+++ application/modules/default/controllers/UserController.php
@@ -315,4 +315,21 @@ class UserController extends Local_Controller_Action_DomainSwitch
         }
         
     }
+    
+    public function payoutAction()
+    {
+
+        $tableMember = new Default_Model_Member();
+        $this->view->view_member = $tableMember->fetchMemberData($this->_memberId);
+        
+        //backdore for admins
+        $helperUserRole = new Backend_View_Helper_UserRole();
+        $userRoleName = $helperUserRole->userRole();
+        if (Default_Model_DbTable_MemberRole::ROLE_NAME_ADMIN == $userRoleName) {
+            $this->view->member = $this->view->view_member;
+        } else {
+            $this->view->member = $this->_authMember;
+        }
+        
+    }
 }
