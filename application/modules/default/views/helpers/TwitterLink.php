7469fc697e77e3ba1f129f1d76343e9e58f78db1
Alexander Schmidt <2f5ed1c5@opayq.com>
2017-04-27 16:11:43 +0200

    updated: FB and Twitter links pointed back to pling
    (cherry picked from commit 926c13e77ab7f7514d35f73b7954dcf9dd0ab7d5)

---
+++ application/modules/default/views/helpers/TwitterLink.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ *  ocs-webserver
+ *
+ *  Copyright 2016 by pling GmbH.
+ *
+ *    This file is part of ocs-webserver.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+class Default_View_Helper_TwitterLink extends Zend_View_Helper_Abstract
+{
+
+    public function twitterLink($strLink)
+    {
+        $strLink = trim($strLink);
+
+        if ($this->checkHttpProtocol($strLink)) {
+            return $strLink;
+        }
+        if ($this->checkDomain($strLink)) {
+            return '//'.$strLink;
+        }
+        return '//twitter.com/'.$strLink;
+    }
+
+    private function checkHttpProtocol($strUrl)
+    {
+        if ((substr($strUrl, 0, 8) == 'https://')
+            OR (substr($strUrl, 0, 7) == 'http://')
+            OR (substr($strUrl, 0, 2) == '//'))
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function checkDomain($strUrl)
+    {
+        if ((strpos($strUrl, 'twitter') !== false)
+            OR (strpos($strUrl, '/') !== false))
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
