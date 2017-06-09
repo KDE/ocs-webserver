46ea8b7e91a9cabce29edc12e67a4e771f9ac92f
Alexander Schmidt <2f5ed1c5@opayq.com>
2017-06-09 14:08:04 +0200

    widget rendering updated
    (cherry picked from commit 73463ebf7c4a8aeb087b5032d3df5cccbbd6a09f)

--- application/modules/default/models/Project.php
+++ application/modules/default/models/Project.php
@@ -1760,4 +1760,15 @@ class Default_Model_Project extends Default_Model_DbTable_Project
 
     }
 
+    public function fetchProductDataFromMV($project_id)
+    {
+        $sql = "SELECT * FROM stat_projects WHERE project_id = :project_id";
+        $resultSet = $this->_db->query($sql, array('project_id'=>$project_id))->fetch();
+        if (false === $resultSet) {
+            return $this->generateRowClass(array());
+        }
+
+        return $this->generateRowClass($resultSet);
+    }
+
 }
