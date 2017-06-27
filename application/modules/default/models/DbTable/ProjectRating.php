a5e9fe8fb8b6bd0f0926881ee02b57452a1eace4
Ronald von Schlegell <rvs75@email.de>
2017-06-27 17:54:43 +0200

    rating

--- application/modules/default/models/DbTable/ProjectRating.php
+++ application/modules/default/models/DbTable/ProjectRating.php
@@ -159,7 +159,7 @@ class Default_Model_DbTable_ProjectRating extends Local_Model_Table
                     //$this->update(array('comment_id' =>$comment_id),'rating_id='.$alreadyExists->rating_id);    
                     //return;
                    $this->update(array('rating_active' =>0),'rating_id='.$alreadyExists->rating_id); 
-                   $flagFromLikeToDislike = true;
+                   $flagFromDislikeToLike  = true;
                 }else{
                 // else userRating ==2 dislike then deactive current rating add new line
                     $this->update(array('rating_active' =>0),'rating_id='.$alreadyExists->rating_id);
@@ -171,7 +171,7 @@ class Default_Model_DbTable_ProjectRating extends Local_Model_Table
                     //$this->update(array('comment_id' =>$comment_id),'rating_id='.$alreadyExists->rating_id);                   
                     //return;     
                     $this->update(array('rating_active' =>0),'rating_id='.$alreadyExists->rating_id);
-                    $flagFromDislikeToLike  = true;
+                    $flagFromLikeToDislike = true;
                 }else{
                     $this->update(array('rating_active' =>0),'rating_id='.$alreadyExists->rating_id);
                     $flagFromDislikeToLike  = true;
