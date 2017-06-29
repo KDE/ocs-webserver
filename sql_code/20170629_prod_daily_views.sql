c029c3ecf085c8587ed2134bfa9237d605da22c7
Alexander Schmidt <2f5ed1c5@opayq.com>
2017-06-29 14:52:22 +0200

    db table  stat_views_today updated

--- sql_code/20170629_prod_daily_views.sql
+++ sql_code/20170629_prod_daily_views.sql
@@ -3,40 +3,41 @@ DROP procedure IF EXISTS `generate_stat_views_today`;
 
 DELIMITER $$
 USE `pling`$$
-CREATE PROCEDURE `generate_stat_views_today`()
+CREATE DEFINER=`chibamaster`@`%` PROCEDURE `generate_stat_views_today`()
   BEGIN
 
-    CREATE TABLE `temp_stat_views_today` (
-      `id`            INT      NOT NULL AUTO_INCREMENT,
-      `project_id`    INT(11)  NOT NULL,
-      `count_views`   INT(11)  NULL     DEFAULT 0,
-      `count_visitor` INT(11)  NULL     DEFAULT 0,
-      `last_view`     DATETIME NULL     DEFAULT NULL,
-      PRIMARY KEY (`id`),
-      INDEX `idx_project` (`project_id` ASC)
-    )
-      ENGINE = MyISAM
-    AS
-      SELECT
-        project_id,
-        COUNT(*) AS count_views,
-        COUNT(DISTINCT `stat_page_views`.`ip`) AS `count_visitor`,
-        MAX(`stat_page_views`.`created_at`) AS `last_view`
-      FROM stat_page_views
-      WHERE (stat_page_views.`created_at`
-      BETWEEN DATE_FORMAT(NOW(), '%Y-%m-%d 00:00') AND DATE_FORMAT(NOW(), '%Y-%m-%d 23:59')
+      DROP TABLE IF EXISTS `temp_stat_views_today`;
+
+      CREATE TABLE `temp_stat_views_today` (
+        `id`            INT      NOT NULL AUTO_INCREMENT,
+        `project_id`    INT(11)  NOT NULL,
+        `count_views`   INT(11)  NULL     DEFAULT 0,
+        `count_visitor` INT(11)  NULL     DEFAULT 0,
+        `last_view`     DATETIME NULL     DEFAULT NULL,
+        PRIMARY KEY (`id`),
+        INDEX `idx_project` (`project_id` ASC)
       )
-      GROUP BY project_id;
+        ENGINE = MyISAM
+      AS
+        SELECT
+          project_id,
+          COUNT(*) AS count_views,
+          COUNT(DISTINCT `stat_page_views`.`ip`) AS `count_visitor`,
+          MAX(`stat_page_views`.`created_at`) AS `last_view`
+        FROM stat_page_views
+        WHERE (stat_page_views.`created_at`
+        BETWEEN DATE_FORMAT(NOW(), '%Y-%m-%d 00:00') AND DATE_FORMAT(NOW(), '%Y-%m-%d 23:59')
+        )
+        GROUP BY project_id;
 
-    ALTER TABLE `stat_views_today_mv`
-      RENAME TO  `old_stat_views_today_mv` ;
+      ALTER TABLE `stat_page_views_today_mv`
+        RENAME TO  `old_stat_views_today_mv` ;
 
-    ALTER TABLE `temp_stat_views_today`
-      RENAME TO  `stat_views_today_mv` ;
+      ALTER TABLE `temp_stat_views_today`
+        RENAME TO  `stat_page_views_today_mv` ;
 
-    DROP TABLE `old_stat_views_today_mv`;
+      DROP TABLE `old_stat_views_today_mv`;
 
   END$$
 
 DELIMITER ;
-
