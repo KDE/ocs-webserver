1e8d5afcd692607a9c8aea9a92883bdd7e722af9
Ronald von Schlegell <rvs75@email.de>
2017-10-06 15:52:17 +0200

    payout

--- application/modules/backend/controllers/MemberPayoutCliController.php
+++ application/modules/backend/controllers/MemberPayoutCliController.php
@@ -178,8 +178,16 @@ class Backend_MemberPayoutCliController extends Local_Controller_Action_CliAbstr
             
 	            echo "Result: " . print_r($result->getRawMessage());
 	            $payKey = $result->getPaymentId();
-	            //mark payout as requested
-	            $payoutTable->update(array("payment_reference_key" => $payKey, "status" => $this::$PAYOUT_STATUS_REQUESTED, "timestamp_masspay_start" => new Zend_Db_Expr('Now()')), "id = " . $payout['id']);
+                    
+                    $successful = $this->isSuccessful($result);
+                    
+                    if($successful) {
+                        //mark payout as requested
+                        $payoutTable->update(array("payment_reference_key" => $payKey, "status" => $this::$PAYOUT_STATUS_REQUESTED, "timestamp_masspay_start" => new Zend_Db_Expr('Now()')), "id = " . $payout['id']);
+                    } else {
+                        //mark payout as failed
+                        $payoutTable->update(array("payment_reference_key" => $payKey, "status" => $this::$PAYOUT_STATUS_PAYPAL_API_ERROR, "timestamp_masspay_start" => new Zend_Db_Expr('Now()'), "payment_raw_error" => print_r($result), "payment_status" => 'ERROR'), "id = " . $payout['id']);
+                    }
             
             } else {
             	
