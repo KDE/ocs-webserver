2cead8051a1fe0070697fbbed9340fd912b85de5
Siyuan Dong <dongsiyuan@gmail.com>
2018-04-23 18:57:32 +0200

    pling

--- sql_code/20180422_project_plings.sql
+++ sql_code/20180422_project_plings.sql
@@ -5,6 +5,5 @@ CREATE TABLE `project_plings` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `is_deleted` int(1) DEFAULT 0,
   `deleted_at` timestamp,
-  PRIMARY KEY (`project_plings_id`),
-  CONSTRAINT UC_Pling UNIQUE (`project_id`,`member_id`)
+  PRIMARY KEY (`project_plings_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
