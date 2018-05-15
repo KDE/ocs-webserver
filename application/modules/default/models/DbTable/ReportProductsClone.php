d17f50d6d7936c89cb03cb1fe09ce95e4b24fd34
siyuan dong <dongsiyuan@gmail.com>
2018-05-15 12:39:57 +0200

    reportclone

---
+++ application/modules/default/models/DbTable/ReportProductsClone.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ *  ocs-webserver
+ *
+ *  Copyright 2016 by pling GmbH.
+ *
+ *    This file is part of ocs-webserver.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+class Default_Model_DbTable_ReportProductsClone extends Local_Model_Table
+{
+
+    protected $_name = "reports_project_clone";
+
+    protected $_keyColumnsForRow = array('report_id');
+
+    protected $_key = 'report_id';
+
+    protected $_defaultValues = array(
+        'report_id'   => null,
+        'project_id'  => null,
+        'project_id_clone'  => null,
+        'external_link'  => null,
+        'reported_by' => null,
+        'text' => null,
+        'is_deleted'  => null,
+        'is_valid'   => null,
+        'created_at'  => null
+    );
+
+    public function setDelete($reportId)
+    {
+        $updateValues = array(
+            'is_deleted' => 1,
+        );
+
+        $this->update($updateValues, 'report_id=' . $reportId);
+    }
+
+    public function setDeleteByMember($member_id)
+    {
+        $updateValues = array(
+            'is_deleted' => 1,
+        );
+
+        $this->update($updateValues, 'reported_by=' . $member_id);
+    }
+
+}
