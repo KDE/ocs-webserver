9659e6a224786c5bdf86ed2c5a8d53e71021dad1
siyuan dong <dongsiyuan@gmail.com>
2018-05-17 16:28:26 +0200

    update

--- httpdocs/theme/flatui/js/script.js
+++ httpdocs/theme/flatui/js/script.js
@@ -1515,8 +1515,8 @@ var AppimagequestionOnClick = (function () {
                     var msgBox = $('#generic-dialog');                    
                     msgBox.modal('show');                    
                     msgBox.find('.modal-header-text').empty().append('AppImage Info');
-                    msgBox.find('.modal-body').empty().append("<div class='info' >To use appimage, install appimage launcher :"
-                        +"<p><a href='https://www.opendesktop.org/p/1228228'>www.opendesktop.org/p/1228228</a><p><a href='https://www.linuxuprising.com/2018/04/easily-run-and-integrate-appimage-files.html'> more info  </a>  </div>");
+                    msgBox.find('.modal-body').empty().append("<div class='info' >For easy appimage use, install appimage launcher :"
+                        +"<p><a href='https://www.opendesktop.org/p/1228228'>www.opendesktop.org/p/1228228</a><p>More info: <br/><a href='https://www.linuxuprising.com/2018/04/easily-run-and-integrate-appimage-files.html'> https://www.linuxuprising.com/2018/04/easily-run-and-integrate-appimage-files.html  </a>  </div>");
            });
 
         }
