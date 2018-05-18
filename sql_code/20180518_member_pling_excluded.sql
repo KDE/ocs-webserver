56c5e4f023aefb954bc5afe314d527281838df25
Ronald von Schlegell <rvs75@email.de>
2018-05-18 12:14:06 +0200

    member pling_excluded

--- sql_code/20180518_member_pling_excluded.sql
+++ sql_code/20180518_member_pling_excluded.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `member`
 	ADD COLUMN `pling_excluded` INT(1) NOT NULL DEFAULT '0' AFTER `primary_mail`;
 
-INSERT INTO `pling-import`.`activity_log_types` (`activity_log_type_id`, `type_text`) VALUES ('319', 'BackendUserPlingExcluded');
+INSERT INTO activity_log_types (`activity_log_type_id`, `type_text`) VALUES ('319', 'BackendUserPlingExcluded');
