6468833bd34179788365ea340e8d3b1c369350da
Ronald von Schlegell <rvs75@email.de>
2018-05-18 11:24:04 +0200

    member pling_excluded

--- application/modules/backend/controllers/MemberController.php
+++ application/modules/backend/controllers/MemberController.php
@@ -172,14 +172,20 @@ class Backend_MemberController extends Zend_Controller_Action
         $memberId = (int)$this->getParam('member_id', null);
         $member = $this->_model->find($memberId)->current();
         $exclude = (int)$this->getParam('pling_excluded', null);
+        $excludOrg = $member['pling_excluded'];
 
         $sql = "UPDATE member SET pling_excluded = :exclude WHERE member_id = :member_id";
         $this->_model->getAdapter()->query($sql, array('exclude' => $exclude, 'member_id' => $memberId));
 
         $auth = Zend_Auth::getInstance();
         $identity = $auth->getIdentity();
+        
+        $logArray = array();
+        $logArray['title'] = $member['username'];
+        $logArray['description'] = 'Change pling_excluded from '.$excludOrg.' to '.$exclude;
+        
         Default_Model_ActivityLog::logActivity($memberId, $memberId, $identity->member_id,
-            Default_Model_ActivityLog::BACKEND_USER_PLING_EXCLUDED, $member);
+            Default_Model_ActivityLog::BACKEND_USER_PLING_EXCLUDED, $logArray);
 
         $jTableResult = array();
         $jTableResult['Result'] = self::RESULT_OK;
