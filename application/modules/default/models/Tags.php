6b97fb76578be8aa873878f9130a3a71c9988bd7
Ronald von Schlegell <rvs75@email.de>
2018-06-14 16:02:39 +0200

    tags: bugfix

--- application/modules/default/models/Tags.php
+++ application/modules/default/models/Tags.php
@@ -606,7 +606,7 @@ class Default_Model_Tags
     public function deletePackageTypeOnProject($projectId, $fileId)
     {
         $sql = "UPDATE tag_object inner join tag ON tag.tag_id = tag_object.tag_id set tag_changed = NOW() , is_deleted = 1 
-                    WHERE tag_group_id = :tag_group_id and tag.tag_name = :name and tag_object.tag_object_id=:object_id and tag_object.tag_parent_object_id=:parent_object_id";
+                    WHERE tag_group_id = :tag_group_id and tag_object.tag_object_id=:object_id and tag_object.tag_parent_object_id=:parent_object_id";
 
         $this->getAdapter()->query($sql, array('tag_group_id' => $this::TAG_PACKAGETYPE_GROUPID, 'object_id' => $fileId, 'parent_object_id' => $projectId));
     }
