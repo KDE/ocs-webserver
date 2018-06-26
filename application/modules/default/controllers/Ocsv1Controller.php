86147be9fc4f3d3f23824657b3b8a3276c360403
Ronald von Schlegell <rvs75@email.de>
2018-06-26 10:36:25 +0200

    ocs

--- application/modules/default/controllers/Ocsv1Controller.php
+++ application/modules/default/controllers/Ocsv1Controller.php
@@ -890,7 +890,7 @@ class Ocsv1Controller extends Zend_Controller_Action
 
             $this->_sendResponse($response, $this->_format);
         }
-    }
+    } 
 
     /**
      * @param int        $contentId
