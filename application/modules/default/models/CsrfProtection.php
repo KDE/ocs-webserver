22b7244ac971ead33de5b43b81d881a9c807df4c
Alexander Schmidt <2f5ed1c5@opayq.com>
2018-07-20 13:40:23 +0200

    updated csrf protection for login page

--- application/modules/default/models/CsrfProtection.php
+++ application/modules/default/models/CsrfProtection.php
@@ -57,11 +57,31 @@ class Default_Model_CsrfProtection
     public static function validateCsrfToken($hash)
     {
         $session = new Zend_Session_Namespace();
-        if (hash_equals($session->crsf_token, $hash)) {
-            return true;
-        } else {
-            return false;
-        }
+
+//        if (false === function_exists("hash_equals")) {
+            $valid = self::hash_equals($session->crsf_token, $hash);
+
+            return $valid;
+//        }
+
+        $valid = hash_equals($session->crsf_token, $hash);
+
+        return $valid;
+    }
+
+    /**
+     * @param $a
+     * @param $b
+     *
+     * @return bool
+     * @author http://php.net/manual/en/function.hash-equals.php#usernotes  Cedric Van Bockhaven
+     */
+    private static function hash_equals($a, $b)
+    {
+        $ret = strlen($a) ^ strlen($b);
+        $ret |= array_sum(unpack("C*", $a ^ $b));
+
+        return !$ret;
     }
 
     /**
