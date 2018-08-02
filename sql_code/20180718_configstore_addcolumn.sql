0979e27907cfa4be67000bd76c5695b8a17715a1
Alexander Schmidt <2f5ed1c5@opayq.com>
2018-08-02 22:44:19 +0200

    cleanup sql files

--- sql_code/20180718_configstore_addcolumn.sql
+++ sql_code/20180718_configstore_addcolumn.sql
@@ -2,16 +2,16 @@ ALTER TABLE `config_store`
   ADD COLUMN `is_show_home` INT(1) DEFAULT 0
   AFTER `is_show_title`;
 
-  ALTER TABLE `config_store`
+ALTER TABLE `config_store`
   ADD COLUMN `layout_home` VARCHAR(45)
   AFTER `is_show_home`;
 
-  ALTER TABLE `config_store`
+ALTER TABLE `config_store`
   ADD COLUMN `layout` VARCHAR(45)
   AFTER `layout_home`;
 
 
-  ALTER TABLE `config_store`
+ALTER TABLE `config_store`
   ADD COLUMN `render_view_postfix` VARCHAR(45)
   AFTER `layout`;
  
--- sql_code/20180730_configstore_addcolumn.sql
+++ sql_code/20180730_configstore_addcolumn.sql
@@ -1,11 +1,8 @@
-
-  ALTER TABLE `config_store`
+ALTER TABLE `config_store`
   ADD COLUMN `layout_explore` VARCHAR(45)
   AFTER `layout_home`;
 
 
- ALTER TABLE `config_store`
+ALTER TABLE `config_store`
   ADD COLUMN `layout_pagedetail` VARCHAR(45)
   AFTER `layout_explore`;
-
- 
