f8de7ad8ea48a0c12c210e34157a316071096de8
david nelband <dnelband@gmail.com>
2018-09-04 18:32:31 +0200

    no message

--- httpdocs/theme/react/app-metaheader/app.js
+++ httpdocs/theme/react/app-metaheader/app.js
@@ -4,84 +4,47 @@ class MetaHeader extends React.Component {
   	this.state = {};
   }
 
+  componentDidMount() {
+    console.log('component did mount');
+  }
+
   render(){
     return (
-      <ul className="nav nav-pills meta-nav-top right">
-         <li><a href="/community" > Community </a> </li>
-         <li><a href="<?=$url_forum?>" target="_blank"> Forum</a> </li>
-         <li><a href="<?=$url_blog?>" target="_blank"> Blog</a> </li>
-         <li><a id="plingList" href="/plings" className="popuppanel"> What are Plings? </a></li>
-         <li><a id="ocsapiContent" href="/partials/ocsapicontent.phtml" className="popuppanel"> API </a> </li>
-         <li><a id="aboutContent" href="/partials/about.phtml" className="popuppanel">About </a></li>
-         <li id="user-context-dropdown-container" className="metaheader-menu-item">
-           <div id="user-dropdown">
-             <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-               <span className="glyphicon glyphicon-th"></span>
-             </button>
-             <ul className="dropdown-menu three-column dropdown-menu-right" aria-labelledby="dropdownMenu2">
-               <li id="opendesktop-link-item">
-                 <a href="http://www.opendesktop.org">
-                   <div className="icon"></div>
-                   <span>OpenDesktop</span>
-                 </a>
-               </li>
-               <li id="discourse-link-item">
-                 <a href="http://discourse.opendesktop.org/">
-                   <div className="icon"></div>
-                   <span>Discourse</span>
-                 </a>
-               </li>
-               <li id="gitlab-link-item">
-                 <a href="https://about.gitlab.com/">
-                   <div className="icon"></div>
-                   <span>Gitlab</span>
-                 </a>
-               </li>
-               <li id="opencode-link-item">
-                 <a href="https://www.opencode.net/">
-                   <div className="icon"></div>
-                   <span>OpenCode</span>
-                 </a>
-               </li>
-             </ul>
-           </div>
-         </li>
-         <li id="user-dropdown-container" className="metaheader-menu-item">
-           <div id="user-dropdown">
-             <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-               <img src="<?=$profile_image_url?>"/>
-             </button>
-             <ul className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
-               <li id="user-info-menu-item">
-                 <div id="user-info-section">
-                   <div className="user-avatar">
-                     <div className="no-avatar-user-letter">
-                       <img src="<?=$profile_image_url?>"/>
-                       <a className="change-profile-pic">
-                         Change
-                       </a>
-                     </div>
-                   </div>
-                   <div className="user-details">
-                     <ul>
-                       <li><b>username</b></li>
-                       <li>$loginMember->mail</li>
-                       <li></li>
-                       <li><a>Profile</a> - <a>Privacy</a></li>
-                       <li><button className="btn btn-default btn-blue">Account</button></li>
-                     </ul>
-                   </div>
-                 </div>
-               </li>
-               <li id="main-seperator" role="separator" className="divider"></li>
-               <li className="buttons">
-                 <button className="btn btn-default btn-metaheader">Add Account</button>
-                 <button className="btn btn-default pull-right btn-metaheader">Sign Up</button>
-               </li>
-             </ul>
-           </div>
-         </li>
-         <li id="user-signin-button" className="metaheader-menu-item"><button className="btn btn-default btn-blue"><a href="/login">Login</a></button></li>
+      <nav id="metaheader-nav">
+        <DomainsMenu/>
+        <UserMenu/>
+      </nav>
+    )
+  }
+}
+
+class DomainsMenu extends React.Component {
+  constructor(props){
+  	super(props);
+  	this.state = {};
+  }
+
+  render(){
+    return (
+      <ul className="metaheader-menu left" id="domains-menu">
+      </ul>
+    )
+  }
+}
+
+class UserMenu extends React.Component {
+  constructor(props){
+  	super(props);
+  	this.state = {};
+  }
+  render(){
+    return (
+      <ul className="metaheader-menu right" id="user-menu">
+        <li><a href="/community">Community</a></li>
+        <li><a href="<?=$url_blog?>" target="_blank">Blog</a></li>
+        <li><a id="plingList" className="popuppanel" href="/plings">What are Plings?</a></li>
+        <li><a id="ocsapiContent" className="popuppanel" href="/partials/ocsapicontent.phtml">API</a></li>
+        <li><a id="aboutContent" className="popuppanel" href="/partials/about.phtml" >About</a></li>
       </ul>
     )
   }
@@ -89,5 +52,5 @@ class MetaHeader extends React.Component {
 
 ReactDOM.render(
     <MetaHeader />,
-    document.getElementById('metaheader-menu')
+    document.getElementById('metaheader')
 );
--- httpdocs/theme/react/metaheader.js
+++ httpdocs/theme/react/metaheader.js
@@ -4,29 +4,48 @@ class MetaHeader extends React.Component {
     this.state = {};
   }
 
+  componentDidMount() {
+    console.log('component did mount');
+  }
+
+  render() {
+    return React.createElement(
+      "nav",
+      { id: "metaheader-nav" },
+      React.createElement(DomainsMenu, null),
+      React.createElement(UserMenu, null)
+    );
+  }
+}
+
+class DomainsMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return React.createElement("ul", { className: "metaheader-menu left", id: "domains-menu" });
+  }
+}
+
+class UserMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return React.createElement(
       "ul",
-      { className: "nav nav-pills meta-nav-top right" },
+      { className: "metaheader-menu right", id: "user-menu" },
       React.createElement(
         "li",
         null,
         React.createElement(
           "a",
           { href: "/community" },
-          " Community "
-        ),
-        " "
-      ),
-      React.createElement(
-        "li",
-        null,
-        React.createElement(
-          "a",
-          { href: "<?=$url_forum?>", target: "_blank" },
-          " Forum"
-        ),
-        " "
+          "Community"
+        )
       ),
       React.createElement(
         "li",
@@ -34,17 +53,7 @@ class MetaHeader extends React.Component {
         React.createElement(
           "a",
           { href: "<?=$url_blog?>", target: "_blank" },
-          " Blog"
-        ),
-        " "
-      ),
-      React.createElement(
-        "li",
-        null,
-        React.createElement(
-          "a",
-          { id: "plingList", href: "/plings", className: "popuppanel" },
-          " What are Plings? "
+          "Blog"
         )
       ),
       React.createElement(
@@ -52,209 +61,30 @@ class MetaHeader extends React.Component {
         null,
         React.createElement(
           "a",
-          { id: "ocsapiContent", href: "/partials/ocsapicontent.phtml", className: "popuppanel" },
-          " API "
-        ),
-        " "
+          { id: "plingList", className: "popuppanel", href: "/plings" },
+          "What are Plings?"
+        )
       ),
       React.createElement(
         "li",
         null,
         React.createElement(
           "a",
-          { id: "aboutContent", href: "/partials/about.phtml", className: "popuppanel" },
-          "About "
+          { id: "ocsapiContent", className: "popuppanel", href: "/partials/ocsapicontent.phtml" },
+          "API"
         )
       ),
       React.createElement(
         "li",
-        { id: "user-context-dropdown-container", className: "metaheader-menu-item" },
+        null,
         React.createElement(
-          "div",
-          { id: "user-dropdown" },
-          React.createElement(
-            "button",
-            { className: "btn btn-default dropdown-toggle", type: "button", id: "dropdownMenu2", "data-toggle": "dropdown", "aria-haspopup": "true", "aria-expanded": "true" },
-            React.createElement("span", { className: "glyphicon glyphicon-th" })
-          ),
-          React.createElement(
-            "ul",
-            { className: "dropdown-menu three-column dropdown-menu-right", "aria-labelledby": "dropdownMenu2" },
-            React.createElement(
-              "li",
-              { id: "opendesktop-link-item" },
-              React.createElement(
-                "a",
-                { href: "http://www.opendesktop.org" },
-                React.createElement("div", { className: "icon" }),
-                React.createElement(
-                  "span",
-                  null,
-                  "OpenDesktop"
-                )
-              )
-            ),
-            React.createElement(
-              "li",
-              { id: "discourse-link-item" },
-              React.createElement(
-                "a",
-                { href: "http://discourse.opendesktop.org/" },
-                React.createElement("div", { className: "icon" }),
-                React.createElement(
-                  "span",
-                  null,
-                  "Discourse"
-                )
-              )
-            ),
-            React.createElement(
-              "li",
-              { id: "gitlab-link-item" },
-              React.createElement(
-                "a",
-                { href: "https://about.gitlab.com/" },
-                React.createElement("div", { className: "icon" }),
-                React.createElement(
-                  "span",
-                  null,
-                  "Gitlab"
-                )
-              )
-            ),
-            React.createElement(
-              "li",
-              { id: "opencode-link-item" },
-              React.createElement(
-                "a",
-                { href: "https://www.opencode.net/" },
-                React.createElement("div", { className: "icon" }),
-                React.createElement(
-                  "span",
-                  null,
-                  "OpenCode"
-                )
-              )
-            )
-          )
-        )
-      ),
-      React.createElement(
-        "li",
-        { id: "user-dropdown-container", className: "metaheader-menu-item" },
-        React.createElement(
-          "div",
-          { id: "user-dropdown" },
-          React.createElement(
-            "button",
-            { className: "btn btn-default dropdown-toggle", type: "button", id: "dropdownMenu1", "data-toggle": "dropdown", "aria-haspopup": "true", "aria-expanded": "true" },
-            React.createElement("img", { src: "<?=$profile_image_url?>" })
-          ),
-          React.createElement(
-            "ul",
-            { className: "dropdown-menu dropdown-menu-right", "aria-labelledby": "dropdownMenu1" },
-            React.createElement(
-              "li",
-              { id: "user-info-menu-item" },
-              React.createElement(
-                "div",
-                { id: "user-info-section" },
-                React.createElement(
-                  "div",
-                  { className: "user-avatar" },
-                  React.createElement(
-                    "div",
-                    { className: "no-avatar-user-letter" },
-                    React.createElement("img", { src: "<?=$profile_image_url?>" }),
-                    React.createElement(
-                      "a",
-                      { className: "change-profile-pic" },
-                      "Change"
-                    )
-                  )
-                ),
-                React.createElement(
-                  "div",
-                  { className: "user-details" },
-                  React.createElement(
-                    "ul",
-                    null,
-                    React.createElement(
-                      "li",
-                      null,
-                      React.createElement(
-                        "b",
-                        null,
-                        "username"
-                      )
-                    ),
-                    React.createElement(
-                      "li",
-                      null,
-                      "$loginMember->mail"
-                    ),
-                    React.createElement("li", null),
-                    React.createElement(
-                      "li",
-                      null,
-                      React.createElement(
-                        "a",
-                        null,
-                        "Profile"
-                      ),
-                      " - ",
-                      React.createElement(
-                        "a",
-                        null,
-                        "Privacy"
-                      )
-                    ),
-                    React.createElement(
-                      "li",
-                      null,
-                      React.createElement(
-                        "button",
-                        { className: "btn btn-default btn-blue" },
-                        "Account"
-                      )
-                    )
-                  )
-                )
-              )
-            ),
-            React.createElement("li", { id: "main-seperator", role: "separator", className: "divider" }),
-            React.createElement(
-              "li",
-              { className: "buttons" },
-              React.createElement(
-                "button",
-                { className: "btn btn-default btn-metaheader" },
-                "Add Account"
-              ),
-              React.createElement(
-                "button",
-                { className: "btn btn-default pull-right btn-metaheader" },
-                "Sign Up"
-              )
-            )
-          )
-        )
-      ),
-      React.createElement(
-        "li",
-        { id: "user-signin-button", className: "metaheader-menu-item" },
-        React.createElement(
-          "button",
-          { className: "btn btn-default btn-blue" },
-          React.createElement(
-            "a",
-            { href: "/login" },
-            "Login"
-          )
+          "a",
+          { id: "aboutContent", className: "popuppanel", href: "/partials/about.phtml" },
+          "About"
         )
       )
     );
   }
 }
 
-ReactDOM.render(React.createElement(MetaHeader, null), document.getElementById('metaheader-menu'));
+ReactDOM.render(React.createElement(MetaHeader, null), document.getElementById('metaheader'));
