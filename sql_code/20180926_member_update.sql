3b9e00e8cf6dc210a26d2e7625d86a01a9d0b673
siyuan dong <dongsiyuan@gmail.com>
2018-09-26 14:23:47 +0200

    letter

--- sql_code/20180926_member_update.sql
+++ sql_code/20180926_member_update.sql
@@ -4,7 +4,13 @@ CREATE TABLE  `member_avatar_type` (
   PRIMARY KEY (`member_avatar_type_id`));
 
 
+INSERT INTO `member_avatar_type` (`member_avatar_type_id`, `title`) VALUES (0, 'unknow');
+INSERT INTO `member_avatar_type` (`member_avatar_type_id`, `title`) VALUES (1, 'auto generated letter avatar');
+INSERT INTO `member_avatar_type` (`member_avatar_type_id`, `title`) VALUES (2, 'user uploaded');
+
+
 ALTER TABLE `member` 
 ADD COLUMN `avatar_type_id` INT(11) NOT NULL DEFAULT 1 after avatar;
+
 
 update member set avatar_type_id = 0;
