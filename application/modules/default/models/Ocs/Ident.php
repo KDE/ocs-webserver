7fc8cc990f6a99a56803984854a27427b079fd5c
Ronald von Schlegell <rvs75@email.de>
2018-10-05 15:17:52 +0200

    avatar

--- application/modules/default/models/Ocs/Ident.php
+++ application/modules/default/models/Ocs/Ident.php
@@ -101,7 +101,7 @@ class Default_Model_Ocs_Ident
         $member_data = $this->getMemberData($member_id);
         $imgTempPath = 'img/data/'.$member_id."_avatar.jpg";
         $im = new imagick($member_data['profile_image_url']);
-        $im = $im->flattenImages();
+        $im->flattenImages();
         // convert to jpeg
         $im->setImageFormat('jpeg');
         //write image on server
