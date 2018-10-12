64e4d2091f40894cb1826ae88dc3ca09bd7723a2
Alexander Schmidt <2f5ed1c5@opayq.com>
2018-10-12 16:50:38 +0200

    group update from gitlab

--- application/modules/backend/models/Group.php
+++ application/modules/backend/models/Group.php
@@ -33,7 +33,7 @@ class Backend_Model_Group
                 $this->addGroup2Ldap($data['name'], $data['group_id'], $data['full_path']);
                 break;
             case 'user_add_to_group':
-                $this->addUser($data['group_name'], $data['group_id'], $data['user_id'], $data['user_name'], $data['user_email'], $data['group_access']);
+                $this->addUser($data['group_id'], $data['user_id'], $data['user_name'], $data['user_email'], $data['group_access']);
                 break;
             default: Zend_Registry::get('logger')->info(__METHOD__ . ' - unhandled git event: ' . json_encode($data));
         }
