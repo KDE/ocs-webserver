a04a508ef3f3949231c7edc15b066960db9a4dc7
david nelband <dnelband@gmail.com>
2018-10-16 12:20:35 +0200

    no message

--- httpdocs/theme/react/metaheader.js
+++ httpdocs/theme/react/metaheader.js
@@ -516,8 +516,6 @@ class UserContextMenuContainer extends React.Component {
   }
 
   componentDidMount() {
-    const elm = $('a[href$="/users/sign_in?redirect_to_referer=yes"]');
-    console.log(elm);
     const self = this;
     $.ajax({ url: window.gitlabUrl + "/api/v4/users?username=" + this.props.user.username, cache: false }).done(function (response) {
       const gitlabLink = self.state.gitlabLink + response[0].id;
