7b6d5d0552f814af592414e0742551eb80b40041
david nelband <dnelband@gmail.com>
2018-11-06 18:03:52 +0100

    hotfix metaheader discussions menu

--- httpdocs/theme/react/app-metaheader/app.js
+++ httpdocs/theme/react/app-metaheader/app.js
@@ -263,6 +263,7 @@ class DiscussionBoardsDropDownMenu extends React.Component {
   render(){
     return (
       <li ref={node => this.node = node}  id="discussion-boards" className={this.state.dropdownClass}>
+        
         <a className="discussion-menu-link-item">Discussion Boards</a>
         <ul className="discussion-menu dropdown-menu dropdown-menu-right">
           <li><a href={this.props.forumUrl + "/c/general"}>General</a></li>
