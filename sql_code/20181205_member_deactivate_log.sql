1c4c7adb064188b81f01062c872fe8e577b031d3
Ronald von Schlegell <rvs75@email.de>
2018-12-05 15:29:15 +0100

    member log

--- sql_code/20181205_member_deactivate_log.sql
+++ sql_code/20181205_member_deactivate_log.sql
@@ -1,6 +1,7 @@
-CREATE TABLE `member_deactivate_log` (
+DROP TABLE IF EXISTS `member_deactivation_log`;
+CREATE TABLE `member_deactivation_log` (
 	`log_id` INT NOT NULL AUTO_INCREMENT,
-	`action_id` INT NOT NULL DEFAULT '0' COMMENT 'Id of the deactivation',
+	`deactivation_id` INT NOT NULL DEFAULT '0' COMMENT 'Id of the deactivation',
 	`object_type_id` INT NOT NULL DEFAULT '0',
 	`object_id` INT NOT NULL DEFAULT '0',
 	`member_id` INT NOT NULL DEFAULT '0' COMMENT 'Member was deactivated from this user',
@@ -10,8 +11,8 @@ CREATE TABLE `member_deactivate_log` (
 COLLATE='utf8_general_ci'
 ;
 
-DROP TABLE `member_deactivate_object_types`;
-CREATE TABLE `member_deactivate_object_types` (
+DROP TABLE IF EXISTS `member_deactivation_object_types`;
+CREATE TABLE `member_deactivation_object_types` (
 	`object_type_id` INT NULL,
 	`object_system` VARCHAR(50) NULL,
 	`object_name` VARCHAR(50) NULL,
@@ -20,14 +21,13 @@ CREATE TABLE `member_deactivate_object_types` (
 COLLATE='utf8_general_ci'
 ;
 
-INSERT INTO member_deactivate_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (1,'opendesktop','member');
-INSERT INTO member_deactivate_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (2,'opendesktop','member_email');
-INSERT INTO member_deactivate_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (3,'opendesktop','project');
-INSERT INTO member_deactivate_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (4,'opendesktop','comments');
+INSERT INTO member_deactivation_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (1,'opendesktop','member');
+INSERT INTO member_deactivation_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (2,'opendesktop','member_email');
+INSERT INTO member_deactivation_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (3,'opendesktop','project');
+INSERT INTO member_deactivation_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (4,'opendesktop','comments');
 
-INSERT INTO member_deactivate_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (20,'gitlab','user');
-INSERT INTO member_deactivate_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (21,'gitlab','project');
+INSERT INTO member_deactivation_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (20,'gitlab','user');
+INSERT INTO member_deactivation_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (21,'gitlab','project');
 
-INSERT INTO member_deactivate_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (30,'discourse','user');
-INSERT INTO member_deactivate_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (31,'discourse','topic');
-
+INSERT INTO member_deactivation_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (30,'discourse','user');
+INSERT INTO member_deactivation_object_types (`object_type_id`,`object_system`,`object_name`) VALUES (31,'discourse','topic');
