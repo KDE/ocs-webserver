3eb69cfc3ca4fba7191cb18582e5a4e6ad28cc9b
david nelband <dnelband@gmail.com>
2018-12-11 11:12:07 +0100

    no message

---
+++ httpdocs/theme/react/opendesktop-home.js
@@ -0,0 +1,25 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log('opendesktop app homepage');
+    console.log(window.data);
+  }
+
+  render() {
+    return React.createElement(
+      'main',
+      { id: 'opendesktop-homepage' },
+      React.createElement(
+        'p',
+        null,
+        'test homepage'
+      )
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('explore-content'));
