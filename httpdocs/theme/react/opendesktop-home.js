68edebd7cf066002d051807d239239f923b6e079
david nelband <dnelband@gmail.com>
2018-12-13 13:26:08 +0100

    no message

--- httpdocs/theme/react/opendesktop-home.js
+++ httpdocs/theme/react/opendesktop-home.js
@@ -364,7 +364,7 @@ class ProductCarouselItem extends React.Component {
   render() {
 
     let imageUrl = this.props.product.image_small;
-    if (this.props.product.image_small.indexOf('https://') > -1 && this.props.product.image_small.indexOf('http://') > -1) {
+    if (this.props.product.image_small.indexOf('https://') === -1 && this.props.product.image_small.indexOf('http://') === -1) {
       let imageBaseUrl;
       if (this.props.env === 'live') {
         imageBaseUrl = 'cn.opendesktop.org';
