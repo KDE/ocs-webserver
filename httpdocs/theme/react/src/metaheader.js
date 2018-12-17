e10ed0051054e39a994f360687386bc2918db3ba
david nelband <dnelband@gmail.com>
2018-12-17 20:07:01 +0100

    no message

--- httpdocs/theme/react/src/metaheader.js
+++ httpdocs/theme/react/src/metaheader.js
@@ -765,7 +765,6 @@ class UserContextMenuContainer extends React.Component {
                 <span>Talk</span>
               </a>
             </li>
-
           </ul>
         </div>
       </li>
@@ -773,7 +772,7 @@ class UserContextMenuContainer extends React.Component {
   }
 }
 
-class UserLoginMenuContainer extends React.Component {
+class UserLoginMenuContainerVersionTwo extends React.Component {
   constructor(props){
     super(props);
     this.state = {};
@@ -852,6 +851,74 @@ class UserLoginMenuContainer extends React.Component {
   }
 }
 
+class UserLoginMenuContainer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentWillMount() {
+    document.addEventListener('mousedown',this.handleClick, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown',this.handleClick, false);
+  }
+
+  handleClick(e){
+    let dropdownClass = "";
+    if (this.node.contains(e.target)){
+      if (this.state.dropdownClass === "open"){
+        if (e.target.className === "th-icon" || e.target.className === "btn btn-default dropdown-toggle"){
+          dropdownClass = "";
+        } else {
+          dropdownClass = "open";
+        }
+      } else {
+        dropdownClass = "open";
+      }
+    }
+    this.setState({dropdownClass:dropdownClass});
+  }
+
+
+  render(){
+    return (
+      <li id="user-login-menu-container" ref={node => this.node = node}>
+        <div className={"user-dropdown " + this.state.dropdownClass}>
+          <button
+            className="btn btn-default dropdown-toggle"
+            type="button"
+            id="userLoginDropdown">
+            <img className="th-icon" src={this.props.user.avatar}/>
+          </button>
+          <ul className="dropdown-menu dropdown-menu-right">
+            <li id="user-info-menu-item">
+              <div id="user-info-section">
+                <div className="user-avatar">
+                  <div className="no-avatar-user-letter">
+                    <img src={this.props.user.avatar}/>
+                  </div>
+                </div>
+                <div className="user-details">
+                  <ul>
+                    <li id="user-details-username"><b>{this.props.user.username}</b></li>
+                    <li id="user-details-email">{this.props.user.mail}</li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li className="buttons">
+              <a href={this.props.baseUrl + "/settings/"} className="btn btn-default btn-metaheader"><span>Settings</span></a>
+              <a href={this.props.logoutUrl} className="btn btn-default pull-right btn-metaheader"><span>Logout</span></a>
+            </li>
+          </ul>
+        </div>
+      </li>
+    )
+  }
+}
 
 class UserTabs extends React.Component {
   constructor(props){
