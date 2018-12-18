de9629e3a5305027399510bc4e021af43d0e140e
Alexander Schmidt <2f5ed1c5@opayq.com>
2018-12-18 15:59:44 +0100

    updated remove/reactivate member

--- application/modules/default/models/Member.php
+++ application/modules/default/models/Member.php
@@ -1230,7 +1230,7 @@ class Default_Model_Member extends Default_Model_DbTable_Member
     {
         try {
             $id_server = new Default_Model_Ocs_OAuth();
-            $id_server->createUser($member_id);
+            $id_server->updateUser($member_id);
             //Zend_Registry::get('logger')->debug(__METHOD__ . ' - : ' . implode(PHP_EOL." - ", $id_server->getMessages()));
         } catch (Exception $e) {
             Zend_Registry::get('logger')->err($e->getMessage() . PHP_EOL . $e->getTraceAsString());
