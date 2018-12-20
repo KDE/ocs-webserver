b5342fd6d8480d822a6852e15be39f78dee22209
david nelband <dnelband@gmail.com>
2018-12-20 20:52:50 +0100

    no message

--- httpdocs/theme/react/src/metaheader.js
+++ httpdocs/theme/react/src/metaheader.js
@@ -923,31 +923,32 @@ class UserLoginMenuContainerVersionTwo extends React.Component {
             id="userLoginDropdown">
             <img className="th-icon" src={this.props.user.avatar}/>
           </button>
-          <div id="background-overlay"></div>
-          <ul id="right-panel" className="dropdown-menu dropdown-menu-right">
-            <li id="user-info-menu-item">
-              <div id="user-info-section">
-                <div className="user-avatar">
-                  <div className="no-avatar-user-letter">
-                    <img src={this.props.user.avatar}/>
+          <div id="background-overlay">
+            <ul id="right-panel" className="dropdown-menu dropdown-menu-right">
+              <li id="user-info-menu-item">
+                <div id="user-info-section">
+                  <div className="user-avatar">
+                    <div className="no-avatar-user-letter">
+                      <img src={this.props.user.avatar}/>
+                    </div>
+                  </div>
+                  <div className="user-details">
+                    <ul>
+                      <li id="user-details-username"><h2>{this.props.user.username}</h2></li>
+                      <li id="user-details-email">{this.props.user.mail}</li>
+                      <li className="buttons">
+                        <a href={this.props.baseUrl + "/settings/"} className="btn btn-default btn-metaheader"><span>Settings</span></a>
+                        <a href={this.props.logoutUrl} className="btn btn-default pull-right btn-metaheader"><span>Logout</span></a>
+                      </li>
+                    </ul>
                   </div>
                 </div>
-                <div className="user-details">
-                  <ul>
-                    <li id="user-details-username"><h2>{this.props.user.username}</h2></li>
-                    <li id="user-details-email">{this.props.user.mail}</li>
-                    <li className="buttons">
-                      <a href={this.props.baseUrl + "/settings/"} className="btn btn-default btn-metaheader"><span>Settings</span></a>
-                      <a href={this.props.logoutUrl} className="btn btn-default pull-right btn-metaheader"><span>Logout</span></a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </li>
-            <li id="user-tabs-menu-item">
-              <UserTabs />
-            </li>
-          </ul>
+              </li>
+              <li id="user-tabs-menu-item">
+                <UserTabs />
+              </li>
+            </ul>
+          </div>
         </div>
       </li>
     )
@@ -1466,17 +1467,6 @@ class UserCommentsTabThreadCommentItem extends React.Component {
         </div>
         <div className="comment-item-content">
           <div dangerouslySetInnerHTML={{__html:c.comment_text}}></div>
-        </div>
-        <div className="comment-item-votes-container">
-          <div className="comment-item-votes up-votes">
-            <span className="glyphicon glyphicon-arrow-up"></span>
-            <span>{c.votes_up}</span>
-          </div>
-          <div className="comment-item-votes down-votes">
-            <span className="glyphicon glyphicon-arrow-down"></span>
-            <span>{c.votes_down}</span>
-          </div>
-          <p>View in discussion</p>
         </div>
       </div>
     )
