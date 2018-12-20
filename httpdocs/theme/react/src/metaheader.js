daedf34f0cecd87ac98774b73d0be3813680f314
david nelband <dnelband@gmail.com>
2018-12-20 19:09:37 +0100

    no message

--- httpdocs/theme/react/src/metaheader.js
+++ httpdocs/theme/react/src/metaheader.js
@@ -1442,7 +1442,7 @@ class UserCommentsTabThreadCommentItem extends React.Component {
     const user = this.props.user;
     let repliedUsernameDisplay;
     if (c.p_comment_member_id){
-      repliedUsernameDisplay = ( <p className="replied-user"><span className="glyphicon glyphicon-share-alt"></span><a href="#">{c.p_username}</a></p> )
+      repliedUsernameDisplay = ( <p className="replied-user"><span className="glyphicon glyphicon-share-alt"></span><a href={"https://forum.opendesktop.cc/u/"+c.p_username+"/messages"}>{c.p_username}</a></p> )
     }
 
     let userImage = user.avatar;
