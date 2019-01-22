9239706811432f0cc2884a5c246f79ed9dcde5b6
Alexander Schmidt <2f5ed1c5@opayq.com>
2019-01-22 13:38:20 +0100

    spam filter - simple plural detection added

--- application/modules/default/models/Spam.php
+++ application/modules/default/models/Spam.php
@@ -44,7 +44,7 @@ class Default_Model_Spam
 
         $haystack = implode(" ", array($project_data['title'], $project_data['description']));
 
-        if(preg_match("/\b({$needles})\b/i", $haystack)){
+        if(preg_match("/\b({$needles})s?\b/i", $haystack)){
             return true;
         }
 
