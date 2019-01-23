4b41e6d30c3dc31a06ba8ef6d1b3004ebb42ab2c
Ronald von Schlegell <rvs75@email.de>
2019-01-23 11:14:09 +0100

    store-tags

--- sql_code/20190123_add_table_config_store_tag.sql
+++ sql_code/20190123_add_table_config_store_tag.sql
@@ -1,14 +1,15 @@
 CREATE TABLE `config_store_tag` (
-	`config_store_tag_id` INT NOT NULL AUTO_INCREMENT,
-	`store_id` INT NOT NULL,
-	`tag_id` INT NOT NULL,
+	`config_store_tag_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`store_id` INT(11) NOT NULL,
+	`tag_id` INT(11) NOT NULL,
 	`is_active` INT(1) UNSIGNED NOT NULL DEFAULT '1',
-	`created_at` DATETIME NOT NULL,
-	`changed_at` DATETIME NOT NULL,
-	`deleted_at` DATETIME NOT NULL,
+	`created_at` DATETIME NULL DEFAULT NULL,
+	`changed_at` DATETIME NULL DEFAULT NULL,
+	`deleted_at` DATETIME NULL DEFAULT NULL,
 	PRIMARY KEY (`config_store_tag_id`)
 )
 ;
+
 
 CREATE TRIGGER `config_store_tag_before_insert` BEFORE INSERT ON `config_store_tag` FOR EACH ROW BEGIN
 	 IF NEW.created_at IS NULL THEN
