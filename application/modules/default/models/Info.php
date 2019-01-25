c85d6d5f3646064bc6e7473586c7a80bd83e48e4
siyuan dong <dongsiyuan@gmail.com>
2019-01-25 20:35:03 +0100

    community

--- application/modules/default/models/Info.php
+++ application/modules/default/models/Info.php
@@ -1132,6 +1132,79 @@ class Default_Model_Info
         return $result;
     }
 
+    public function getMostPlingedProducts($limit = 20)
+    {
+        /** @var Zend_Cache_Core $cache */
+        $cache = Zend_Registry::get('cache');
+        $cacheName = __FUNCTION__ . '_' . md5((int)$limit);
+
+        if (false !== ($newSupporters = $cache->load($cacheName))) {
+            return $newSupporters;
+        }
+
+        $sql = '  
+                        select pl.project_id
+                        ,count(1) as sum_plings 
+                        ,p.title
+                        ,p.image_small
+                        ,p.laplace_score
+                        ,p.count_likes
+                        ,p.count_dislikes   
+                        ,p.member_id 
+                        ,p.profile_image_url
+                        ,p.username
+                        ,p.cat_title as catTitle
+                        from project_plings pl
+                        inner join stat_projects p on pl.project_id = p.project_id and p.status = 100
+                        where pl.is_deleted = 0 and pl.is_active = 1 
+                        group by pl.project_id
+                        order by sum_plings desc 
+                                                              
+        ';
+        if (isset($limit)) {
+            $sql .= ' limit ' . (int)$limit;
+        }
+        $result = Zend_Db_Table::getDefaultAdapter()->query($sql, array())->fetchAll();
+
+        $cache->save($result, $cacheName, array(), 300);
+
+        return $result;
+    }
+
+    public function getMostPlingedCreators($limit = 20)
+    {
+        /** @var Zend_Cache_Core $cache */
+        $cache = Zend_Registry::get('cache');
+        $cacheName = __FUNCTION__ . '_' . md5((int)$limit);
+
+        if (false !== ($newSupporters = $cache->load($cacheName))) {
+            return $newSupporters;
+        }
+
+        $sql = '  
+                       select p.member_id,
+                        count(1) as cnt,
+                        m.username,
+                        m.profile_image_url,
+                        m.created_at
+                        from stat_projects p
+                        join project_plings pl on p.project_id = pl.project_id
+                        join member m on p.member_id = m.member_id
+                        where p.status = 100
+                        group by p.member_id
+                        order by cnt desc                        
+                                                              
+        ';
+        if (isset($limit)) {
+            $sql .= ' limit ' . (int)$limit;
+        }
+        $result = Zend_Db_Table::getDefaultAdapter()->query($sql, array())->fetchAll();
+
+        $cache->save($result, $cacheName, array(), 300);
+
+        return $result;
+    }
+
     public function getCountActiveSupporters()
     {
         /** @var Zend_Cache_Core $cache */
