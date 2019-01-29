8d900273eeb62dccf38a82dc76ad7633911c5c2a
Ronald von Schlegell <rvs75@email.de>
2019-01-29 11:37:06 +0100

    insert store tags

--- sql_code/20190123_add_table_config_store_tag.sql
+++ sql_code/20190123_add_table_config_store_tag.sql
@@ -17,3 +17,19 @@ CREATE TRIGGER `config_store_tag_before_insert` BEFORE INSERT ON `config_store_t
     END IF;
 END;
 
+
+insert into config_store_tag
+
+select 
+null as `config_store_tag_id`,
+	s.store_id as `store_id`,
+	s.package_type as `tag_id`,
+	1 as `is_active`,
+	null as `created_at`,
+	null as `changed_at`,
+	null as `deleted_at`
+from config_store s
+where s.package_type is not null;
+
+
+
