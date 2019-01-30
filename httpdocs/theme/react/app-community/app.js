2f41355032be27bbde6e3b07e17617d8e843bea4
david nelband <dnelband@gmail.com>
2019-01-30 14:48:12 +0100

    a

--- httpdocs/theme/react/app-community/app.js
+++ httpdocs/theme/react/app-community/app.js
@@ -2,18 +2,83 @@ class CommunityPage extends React.Component {
   constructor(props){
   	super(props);
   	this.state = {
-      jsonData:window.json_data
+      headerData:window.json_data.data
     };
   }
 
   componentDidMount() {
     console.log(this.state);
+    /*
+      var json_data = <?=json_encode($this->json_data)?>; hier bekommest du die oberteil info
+      tabs info top members https://www.opendesktop.cc/community/getjson?e=topmembers
+      hier ist supporters https://www.opendesktop.cc/community/getjson?e=supporters
+      alle tab events sind hier. /var/www/ocs-webserver/application/modules/default/controllers/CommunityController.php
+      getjsonAction
+    */
   }
 
   render(){
     return(
       <div id="community-page">
-        react community
+        <div className="container">
+          <CommunityPageHeader
+            headerData={this.state.headerData}
+          />
+          <CommunityPageTabsContainer />
+        </div>
+      </div>
+    );
+  }
+}
+
+class CommunityPageHeader extends React.Component {
+  constructor(props){
+  	super(props);
+  	this.state = {};
+  }
+
+  render(){
+    return(
+      <div id="community-page-header">
+        <h1>Community</h1>
+        <div id="community-page-header-banner">
+          <div className="header-banner-row">
+            <p>{this.props.headerData.countActiveMembers}</p>
+            <span>contributors added</span>
+          </div>
+          <div className="header-banner-row">
+            <p>{this.props.headerData.countProjects}</p>
+            <span>products</span>
+          </div>
+          <div id="header-banner-bottom">
+            <div className="center">
+              <a href="/register">
+                Register
+              </a>
+              <span>to join the community</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class CommunityPageTabsContainer extends React.Component {
+  constructor(props){
+  	super(props);
+  	this.state = {};
+  }
+
+  render(){
+    return(
+      <div id="community-page-tabs-container">
+        <div id="tabs-menu">
+          tabs
+        </div>
+        <div id="tabs-content">
+          content
+        </div>
       </div>
     );
   }
--- httpdocs/theme/react/community.js
+++ httpdocs/theme/react/community.js
@@ -2,19 +2,126 @@ class CommunityPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      jsonData: window.json_data
+      headerData: window.json_data.data
     };
   }
 
   componentDidMount() {
     console.log(this.state);
+    /*
+      var json_data = <?=json_encode($this->json_data)?>; hier bekommest du die oberteil info
+      tabs info top members https://www.opendesktop.cc/community/getjson?e=topmembers
+      hier ist supporters https://www.opendesktop.cc/community/getjson?e=supporters
+      alle tab events sind hier. /var/www/ocs-webserver/application/modules/default/controllers/CommunityController.php
+      getjsonAction
+    */
   }
 
   render() {
     return React.createElement(
       "div",
       { id: "community-page" },
-      "react community"
+      React.createElement(
+        "div",
+        { className: "container" },
+        React.createElement(CommunityPageHeader, {
+          headerData: this.state.headerData
+        }),
+        React.createElement(CommunityPageTabsContainer, null)
+      )
+    );
+  }
+}
+
+class CommunityPageHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      { id: "community-page-header" },
+      React.createElement(
+        "h1",
+        null,
+        "Community"
+      ),
+      React.createElement(
+        "div",
+        { id: "community-page-header-banner" },
+        React.createElement(
+          "div",
+          { className: "header-banner-row" },
+          React.createElement(
+            "p",
+            null,
+            this.props.headerData.countActiveMembers
+          ),
+          React.createElement(
+            "span",
+            null,
+            "contributors added"
+          )
+        ),
+        React.createElement(
+          "div",
+          { className: "header-banner-row" },
+          React.createElement(
+            "p",
+            null,
+            this.props.headerData.countProjects
+          ),
+          React.createElement(
+            "span",
+            null,
+            "products"
+          )
+        ),
+        React.createElement(
+          "div",
+          { id: "header-banner-bottom" },
+          React.createElement(
+            "div",
+            { className: "center" },
+            React.createElement(
+              "a",
+              { href: "/register" },
+              "Register"
+            ),
+            React.createElement(
+              "span",
+              null,
+              "to join the community"
+            )
+          )
+        )
+      )
+    );
+  }
+}
+
+class CommunityPageTabsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      { id: "community-page-tabs-container" },
+      React.createElement(
+        "div",
+        { id: "tabs-menu" },
+        "tabs"
+      ),
+      React.createElement(
+        "div",
+        { id: "tabs-content" },
+        "content"
+      )
     );
   }
 }
