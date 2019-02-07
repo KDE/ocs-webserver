86ea18dfc62ec407c1fafc755345638a469a01fd
david nelband <dnelband@gmail.com>
2019-02-07 16:49:14 +0100

    a

--- httpdocs/theme/react/app-opendesktop-home/app.js
+++ httpdocs/theme/react/app-opendesktop-home/app.js
@@ -290,12 +290,19 @@ class SpotlightProduct extends React.Component {
     const createdDate = jQuery.timeago(cDate);
     const productScoreColor = window.hpHelpers.calculateScoreColor(this.props.featuredProduct.laplace_score);
 
+    let imageBaseUrl;
+    if (store.getState().env === 'live') {
+      imageBaseUrl = 'cn.opendesktop.org';
+    } else {
+      imageBaseUrl = 'cn.pling.it';
+    }
+
     return(
       <div id="spotlight-product">
         <h2>In the Spotlight</h2>
         <div className="container">
           <div className="spotlight-image">
-            <img src={this.state.featuredProduct.image_small}/>
+            <img src={"https://" + imageBaseUrl + "/cache/300x230-1/img/" + this.state.featuredProduct.image_small}/>
           </div>
           <div className="spotlight-info">
             <div className="spotlight-info-wrapper">
