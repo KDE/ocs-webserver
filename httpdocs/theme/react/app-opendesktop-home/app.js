1f06b2a281b65d9fcfceb6afe1e9e2ed8677bfa3
david nelband <dnelband@gmail.com>
2019-02-07 11:52:44 +0100

    a

--- httpdocs/theme/react/app-opendesktop-home/app.js
+++ httpdocs/theme/react/app-opendesktop-home/app.js
@@ -165,7 +165,6 @@ class SpotlightUser extends React.Component {
   getSpotlightUser(){
     const self = this;
     $.ajax({url: "/home/showspotlightjson?page="+this.state.page,cache: false}).done(function(response){
-      console.log(response);
       self.setState({user:response,loading:false});
     });
   }
