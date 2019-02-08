48d74d95ec26ddd0c299c9ad796e00173591dde6
david nelband <dnelband@gmail.com>
2019-02-08 12:34:01 +0100

    a

--- httpdocs/theme/react/all.js
+++ httpdocs/theme/react/all.js
@@ -3047,7 +3047,6 @@ class ProductViewHeaderLikes extends React.Component {
       const url = "/p/" + this.props.product.project_id + "/followproject/";
       const self = this;
       $.ajax({ url: url, cache: false }).done(function (response) {
-        console.log(response);
         // error
         if (response.status === "error") {
           self.setState({ msg: response.msg });
@@ -4095,7 +4094,6 @@ class CommentItem extends React.Component {
   }
 
   onConfirmReportClick(commentId, productId) {
-    console.log(commentId, productId);
     jQuery.ajax({
       data: {
         i: commentId,
@@ -4131,7 +4129,6 @@ class CommentItem extends React.Component {
   }
 
   render() {
-    console.log(this.props.comment);
     let commentRepliesContainer;
     const filteredComments = categoryHelpers.convertCatChildrenObjectToArray(this.props.product.r_comments).filter(this.filterByCommentLevel);
     if (filteredComments.length > 0) {
