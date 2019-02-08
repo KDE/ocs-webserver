75220c166c132eed1f1ec5443098ba9d98c2cd57
david nelband <dnelband@gmail.com>
2019-02-08 15:21:34 +0100

    a

--- httpdocs/theme/react/carousel.js
+++ httpdocs/theme/react/carousel.js
@@ -195,7 +195,7 @@ class Carousel extends React.Component {
     }
 
     if (window.page === "appimages") {
-      $('#carousel-module-container').width(containerWidth);
+      $('#carousel-module-container').width(containerWidth + 20);
     }
 
     this.setState({
