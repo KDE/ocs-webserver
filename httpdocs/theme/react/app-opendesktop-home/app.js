7fddfc16f14f9eaa4140bc61eefd83578fe3a56d
david nelband <dnelband@gmail.com>
2019-02-12 10:11:01 +0100

    a

--- httpdocs/theme/react/app-opendesktop-home/app.js
+++ httpdocs/theme/react/app-opendesktop-home/app.js
@@ -149,10 +149,10 @@ class SpotlightProduct extends React.Component {
       featuredLabelDisplay = "featured"
     }
 
-    let cDate = new Date(this.props.featuredProduct.created_at);
+    let cDate = new Date(this.state.featuredProduct.created_at);
     cDate = cDate.toString();
     const createdDate = cDate.split(' ')[1] + " " + cDate.split(' ')[2] + " " + cDate.split(' ')[3];
-    const productScoreColor = window.hpHelpers.calculateScoreColor(this.props.featuredProduct.laplace_score);
+    const productScoreColor = window.hpHelpers.calculateScoreColor(this.state.featuredProduct.laplace_score);
 
     let loadingContainerDisplay;
     if (this.state.loading){
