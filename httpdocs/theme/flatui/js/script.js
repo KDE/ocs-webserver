85dcb27f2262d5f95cbc905e9c3446332d9e72f8
Ronald von Schlegell <rvs75@email.de>
2019-03-01 12:47:58 +0100

    dl popup

--- httpdocs/theme/flatui/js/script.js
+++ httpdocs/theme/flatui/js/script.js
@@ -655,7 +655,7 @@ var Opendownloadfile= (function () {
                     'transitionOut': 'elastic',
                     'type': 'iframe',
                     'width':'600',
-                    'height':'auto',
+                    'height':'400',
                     'iframe': {'scrolling': 'no'},
                     'autoSize':false,
                     'id': 'opendownloadfilepopup',
