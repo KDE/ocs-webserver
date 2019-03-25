43ea729f3087dfc6c6244d49051970da22ba97d6
siyuan dong <dongsiyuan@gmail.com>
2019-03-25 12:52:55 +0100

    metaheader

--- httpdocs/theme/react/src/metaheader/UserInfo.js
+++ httpdocs/theme/react/src/metaheader/UserInfo.js
@@ -4,10 +4,10 @@ function UserInfo(props){
     <div className="userinfo">
       <div className="header">{props.userinfo.username} {props.userinfo.countrycity}</div>
       <div className="statistic">
-        <div className="statisticRow"><span className="title">{props.userinfo.cntProjects} </span> products </div>
-        <div className="statisticRow"><span className="title">{props.userinfo.totalComments} </span> comments </div>
-        <div className="statisticRow">Likes <span className="title">{props.userinfo.cntLikesGave} </span> products </div>
-        <div className="statisticRow">Got <span className="title">{props.userinfo.cntLikesGot} </span>Likes </div>
+        <div className="statisticRow"><span className="statisticRowTitle">{props.userinfo.cntProjects} </span> products </div>
+        <div className="statisticRow"><span className="statisticRowTitle">{props.userinfo.totalComments} </span> comments </div>
+        <div className="statisticRow">Likes <span className="statisticRowTitle">{props.userinfo.cntLikesGave} </span> products </div>
+        <div className="statisticRow">Got <span className="statisticRowTitle">{props.userinfo.cntLikesGot} </span>Likes </div>
         <div className="statisticRow">Last time active :{props.userinfo.lastactive_at}  </div>
         <div className="statisticRow">Member since : {props.userinfo.created_at}  </div>
       </div>
