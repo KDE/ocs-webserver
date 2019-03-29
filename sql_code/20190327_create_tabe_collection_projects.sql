f16601a0bbd659443359e175df7286230aee39be
Ronald von Schlegell <rvs75@email.de>
2019-03-29 13:32:53 +0100

    table collection projects

--- sql_code/20190327_create_tabe_collection_projects.sql
+++ sql_code/20190327_create_tabe_collection_projects.sql
@@ -1,13 +1,14 @@
 CREATE TABLE `collection_projects` (
-	`collection_project_id` INT(11) NOT NULL,
+	`collection_project_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`collection_id` INT(11) NOT NULL,
 	`project_id` INT(11) NOT NULL,
-	`order` INT(11) NULL,
+	`order` INT(11) NULL DEFAULT NULL,
 	`active` INT(1) UNSIGNED NULL DEFAULT '1',
-	`created_at` DATETIME NULL,
-	`changed_at` DATETIME NULL,
-	`deleted_at` DATETIME NULL,
-	PRIMARY KEY (`collection_project_id`, `project_id`)
+	`created_at` DATETIME NULL DEFAULT NULL,
+	`changed_at` DATETIME NULL DEFAULT NULL,
+	`deleted_at` DATETIME NULL DEFAULT NULL,
+	PRIMARY KEY (`collection_project_id`)
 )
 COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
 ;
-
