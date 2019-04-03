8537708cb5b44c86a758c7580de673810e7fd476
Ronald von Schlegell <rvs75@email.de>
2019-04-03 09:32:34 +0200

    fix missing project

--- application/modules/default/controllers/ProductController.php
+++ application/modules/default/controllers/ProductController.php
@@ -365,13 +365,14 @@ class ProductController extends Local_Controller_Action_DomainSwitch
         
         $modelProduct = new Default_Model_Project();
         $productInfo = $modelProduct->fetchProductInfo($this->_projectId);
+        if (empty($productInfo)) {
+            throw new Zend_Controller_Action_Exception('This page does not exist', 404);
+        }
         $this->view->product = $productInfo;
         $this->view->headTitle($productInfo->title . ' - ' . $this->getHeadTitle(), 'SET');
         $this->view->cat_id = $this->view->product->project_category_id;
 
-        if (empty($this->view->product)) {
-            throw new Zend_Controller_Action_Exception('This page does not exist', 404);
-        }
+        
         
         //create ppload download hash: secret + collection_id + expire-timestamp
         $salt = PPLOAD_DOWNLOAD_SECRET;
