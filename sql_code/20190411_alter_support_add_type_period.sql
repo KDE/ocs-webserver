6edd9ed1e59992eaec7fc4bd68533d5450a4299f
Ronald von Schlegell <rvs75@email.de>
2019-04-11 14:34:29 +0200

    subscr

--- sql_code/20190411_alter_support_add_type_period.sql
+++ sql_code/20190411_alter_support_add_type_period.sql
@@ -1,11 +1,10 @@
 ALTER TABLE `support`
-	ADD COLUMN `type_id` INT(1) UNSIGNED NULL DEFAULT '1' COMMENT '1 = signup, 2 = payment' AFTER `create_time`,
+	ADD COLUMN `type_id` INT(1) UNSIGNED NULL DEFAULT '1' COMMENT '0 = onetime payment, 1 = subsscription signup, 2 = subsscription payment' AFTER `create_time`,
 	ADD COLUMN `period` VARCHAR(50) NULL AFTER `amount`;
 
 
 UPDATE support p
-SET p.type_id = 2
-WHERE p.type_id = 1;
+SET p.type_id = 0;
 
 ALTER TABLE `support`
 	ADD COLUMN `subscription_id` VARCHAR(255) NULL AFTER `type_id`;
