c3341f50604f873492e6ad08c477ad2b84e879df
Ronald von Schlegell <rvs75@email.de>
2019-04-11 11:33:06 +0200

    subsrc

--- library/Local/Payment/PayPal/Response.php
+++ library/Local/Payment/PayPal/Response.php
@@ -34,10 +34,10 @@ class Local_Payment_PayPal_Response
             return new Local_Payment_PayPal_Masspay_ResponseMasspay($rawResponse);
         } else    
         if (isset($rawResponse['txn_type']) AND ($rawResponse['txn_type'] == 'subscr_payment')) {
-            return new Local_Payment_PayPal_SubscriptionPayment_ResponseSupport($rawResponse);
+            return new Local_Payment_PayPal_SubscriptionPayment_Response($rawResponse);
         } else    
         if (isset($rawResponse['txn_type']) AND ($rawResponse['txn_type'] == 'subscr_signup')) {
-            return new Local_Payment_PayPal_SubscriptionSignup_ResponseSupport($rawResponse);
+            return new Local_Payment_PayPal_SubscriptionSignup_Response($rawResponse);
         } else    
         if (isset($rawResponse['responseEnvelope_ack'])) {
             return new Local_Payment_PayPal_AdaptivePayment_ResponsePayRequest($rawResponse);
