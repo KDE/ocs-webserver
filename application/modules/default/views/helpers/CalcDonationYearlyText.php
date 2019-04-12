c850686204f126d8ac7708bfdef0dc2ecd2373e6
siyuan dong <dongsiyuan@gmail.com>
2019-04-12 12:36:31 +0200

    sub

--- application/modules/default/views/helpers/CalcDonationYearlyText.php
+++ application/modules/default/views/helpers/CalcDonationYearlyText.php
@@ -36,7 +36,7 @@ class Default_View_Helper_CalcDonationYearlyText extends Zend_View_Helper_Abstra
 		$t=$total*self::tax;
 		$g = $total+$t;
         $r = '($'.$tier.'*12 = '.number_format($tier*12,2).' + $'.number_format($paypal, 2).' paypal + $'.number_format($t, 2).' tax = $'
-        		.number_format($g,2).' monthly)';
+        		.number_format($g,2).' yearly)';
         return $r;
     }
 
