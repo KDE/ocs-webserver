4b040af4915d4e5beca09ad38ab866e8efa45bd5
Ronald von Schlegell <rvs75@email.de>
2019-04-15 15:09:49 +0200

    coll

--- application/modules/default/controllers/UserController.php
+++ application/modules/default/controllers/UserController.php
@@ -128,6 +128,7 @@ class UserController extends Local_Controller_Action_DomainSwitch
                 $tableProject->getUserActiveProjects($this->_memberId, $pageLimit, ($projectpage - 1) * $pageLimit);
        
             $this->view->userFeaturedProducts = $tableProject->fetchAllFeaturedProjectsForMember($this->_memberId);
+            $this->view->userCollections = $tableProject->fetchAllCollectionsForMember($this->_memberId);
 
             $paginationComments = $tableMember->fetchComments($this->_memberId);
             if ($paginationComments) {
