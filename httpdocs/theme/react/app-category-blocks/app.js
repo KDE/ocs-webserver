950fac4d855157d4c9bd9999ad21fd91b451ccfd
David Nelband <dnelband@gmail.com>
2019-04-25 12:43:36 +0200

    aih

--- httpdocs/theme/react/app-category-blocks/app.js
+++ httpdocs/theme/react/app-category-blocks/app.js
@@ -1,18 +1,5 @@
 function CategoryBlocks(){
-    console.log(window.catTree)
-    const catList = [
-        {title:'All',product_count:'427'},
-        {title:'Audio',product_count:'47'},
-        {title:'Education',product_count:'27'},
-        {title:'Games',product_count:'17'},
-        {title:'Graphics',product_count:'42'},
-        {title:'Internet',product_count:'427'},
-        {title:'Office',product_count:'427'},
-        {title:'Programming',product_count:'427'},
-        {title:'Systools',product_count:'427'},
-        {title:'Video',product_count:'427'}
-    ]
-    const [ categories, setCategories ] = React.useState(catList)
+    const [ categories, setCategories ] = React.useState(windows.catTree.children)
     let categoriesDisplay;
     if (categories) categoriesDisplay = categories.map((c,index) => (<CategoryBlockItem category={c}/> ))
     return (
@@ -30,15 +17,14 @@ function CategoryBlockItem(props){
     const c = props.category;
     const imgUrl = "/theme/react/assets/img/aih-"+c.title.toLowerCase()+".png";
     const ribbonCssClass = "aih-ribbon aih-"+c.title.toLowerCase();
-
     return (
-        <a href={"/browse/cat/" + props.category.cat_id}>
+        <a href={"/browse/cat/" + c.id}>
             <div className="aih-card">
                 <div className={ribbonCssClass}></div>
                 <img className="aih-thumb" src={imgUrl}/>
                 <div className="aih-content">
                     <h3 className="aih-title">{c.title}</h3>
-                    <p className="aih-counter">427 <span>products</span></p>
+                    <p className="aih-counter">{c.product_count} <span>products</span></p>
                 </div>
             </div>
         </a>        
--- httpdocs/theme/react/category-blocks.js
+++ httpdocs/theme/react/category-blocks.js
@@ -1,12 +1,9 @@
-'use strict';
+"use strict";
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 function CategoryBlocks() {
-    console.log(window.catTree);
-    var catList = [{ title: 'All', product_count: '427' }, { title: 'Audio', product_count: '47' }, { title: 'Education', product_count: '27' }, { title: 'Games', product_count: '17' }, { title: 'Graphics', product_count: '42' }, { title: 'Internet', product_count: '427' }, { title: 'Office', product_count: '427' }, { title: 'Programming', product_count: '427' }, { title: 'Systools', product_count: '427' }, { title: 'Video', product_count: '427' }];
-
-    var _React$useState = React.useState(catList),
+    var _React$useState = React.useState(windows.catTree.children),
         _React$useState2 = _slicedToArray(_React$useState, 2),
         categories = _React$useState2[0],
         setCategories = _React$useState2[1];
@@ -16,14 +13,14 @@ function CategoryBlocks() {
         return React.createElement(CategoryBlockItem, { category: c });
     });
     return React.createElement(
-        'div',
-        { id: 'category-blocks' },
+        "div",
+        { id: "category-blocks" },
         React.createElement(
-            'div',
-            { className: 'container aih-container aih-section' },
+            "div",
+            { className: "container aih-container aih-section" },
             React.createElement(
-                'div',
-                { className: 'aih-row' },
+                "div",
+                { className: "aih-row" },
                 categoriesDisplay
             )
         )
@@ -34,31 +31,31 @@ function CategoryBlockItem(props) {
     var c = props.category;
     var imgUrl = "/theme/react/assets/img/aih-" + c.title.toLowerCase() + ".png";
     var ribbonCssClass = "aih-ribbon aih-" + c.title.toLowerCase();
-
     return React.createElement(
-        'a',
-        { href: "/browse/cat/" + props.category.cat_id },
+        "a",
+        { href: "/browse/cat/" + c.id },
         React.createElement(
-            'div',
-            { className: 'aih-card' },
-            React.createElement('div', { className: ribbonCssClass }),
-            React.createElement('img', { className: 'aih-thumb', src: imgUrl }),
+            "div",
+            { className: "aih-card" },
+            React.createElement("div", { className: ribbonCssClass }),
+            React.createElement("img", { className: "aih-thumb", src: imgUrl }),
             React.createElement(
-                'div',
-                { className: 'aih-content' },
+                "div",
+                { className: "aih-content" },
                 React.createElement(
-                    'h3',
-                    { className: 'aih-title' },
+                    "h3",
+                    { className: "aih-title" },
                     c.title
                 ),
                 React.createElement(
-                    'p',
-                    { className: 'aih-counter' },
-                    '427 ',
+                    "p",
+                    { className: "aih-counter" },
+                    c.product_count,
+                    " ",
                     React.createElement(
-                        'span',
+                        "span",
                         null,
-                        'products'
+                        "products"
                     )
                 )
             )
