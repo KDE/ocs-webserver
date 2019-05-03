f4cd84151f079ab90696b49e24ea36dc800e947e
Ronald von Schlegell <rvs75@email.de>
2019-05-03 16:07:58 +0200

    dl

---
+++ sql_code/20190503_create_files_downloaded_all.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ppload.ppload_files_downloaded_all LIKE ppload.ppload_files_downloaded
+;
+
+
+ALTER TABLE `ppload_files_downloaded_all`
+	ADD COLUMN `source` VARCHAR(39) NULL DEFAULT NULL AFTER `downloaded_ip`;
+ALTER TABLE `ppload_files_downloaded_all`
+	ADD COLUMN `link_type` VARCHAR(39) NULL DEFAULT NULL AFTER `source`;
