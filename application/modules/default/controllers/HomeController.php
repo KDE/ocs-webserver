4bf7447151c2ecb7dc811422d93a0d215195c0eb
siyuan dong <dongsiyuan@gmail.com>
2019-05-06 12:15:32 +0200

    od

--- application/modules/default/controllers/HomeController.php
+++ application/modules/default/controllers/HomeController.php
@@ -32,9 +32,9 @@ class HomeController extends Local_Controller_Action_DomainSwitch
 
             if($storeConfig->config_id_name=='plingcom')
             {
-                $this->view->domainReferText='<div>Welcome to opendesktop.org.<span class="refertextsmall"> go to <a href="www.opendesktop.org">opendesktop.org</a> for Desktop related stuff like themes and Apps. </span> </div>';
+                $this->view->domainReferText='<div>Welcome to pling.com.<span class="refertextsmall"> go to <a href="www.opendesktop.org">opendesktop.org</a> for Desktop related stuff like themes and Apps. </span> </div>';
             }else if($storeConfig->config_id_name=='opendesktop'){
-                $this->view->domainReferText='<div>Welcome to opendesktop.org.<span class="refertextsmall"> go to <a href="www.pling.com">pling.com</a> for more stuff. </span> </div>';
+                $this->view->domainReferText='<div>Welcome to opendesktop.org.<span class="refertextsmall"> For more stuff go to <a href="www.pling.com">pling.com</a>  </span> </div>';
             }
         
             if($storeConfig->isShowHomepage())
