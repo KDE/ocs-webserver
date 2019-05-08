c2c32368b1d41372c426fca8062a549a861e050e
David Nelband <dnelband@gmail.com>
2019-05-08 16:41:35 +0200

    product media slider

--- httpdocs/theme/react/product-media-slider/app/index.js
+++ httpdocs/theme/react/product-media-slider/app/index.js
@@ -22,7 +22,7 @@ function ProductMediaSlider(){
   React.useEffect(() => { 
     window.addEventListener("resize", updateDimensions);
     window.addEventListener("orientationchange", updateDimensions);
-    // setLoading(false);
+    setLoading(false);
   },[])
 
   // update dimensions
