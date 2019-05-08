eb94caa75a5fd689f98521a973a1255bb81e8d6a
David Nelband <dnelband@gmail.com>
2019-05-08 18:20:20 +0200

    pms

--- httpdocs/theme/react/product-media-slider/app/index.js
+++ httpdocs/theme/react/product-media-slider/app/index.js
@@ -65,11 +65,11 @@ function ProductMediaSlider(){
   )
 }
 
-function SlideItem(props){0
+function SlideItem(props){
   const [ mediaType, setMediaType ] = useState(props.slideUrl.indexOf('<iframe') > -1 ? "embed" : "image");
   let slideContentDisplay;
   if (mediaType === "embed"){
-    slideContentDisplay = props.slideUrl;
+    slideContentDisplay = <div dangerouslySetInnerHTML __html={{props.slideUrl}}></div>
   } else if (mediaType === "image"){
     slideContentDisplay = <img src={props.slideUrl}/>
   } else { 
