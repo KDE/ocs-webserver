e7a7a5d01cf722bf0d0e4d832b0abd06beb88c9a
David Nelband <dnelband@gmail.com>
2019-05-09 17:43:30 +0200

    pms

--- httpdocs/theme/react/product-media-slider/app/product-media-slider.js
+++ httpdocs/theme/react/product-media-slider/app/product-media-slider.js
@@ -8,9 +8,11 @@ function ProductMediaSlider(){
 
   const [ product, setProduct ] = useState(window.product);
   let galleryArray;
-  if (product.embed_code !== null && product.embed_code !== "" || product.image_small !== null && product.image_small !== "" ){
-    const productMainSlide = product.embed_code !== null ? product.embed_code : product.image_small;
-    galleryArray = [ productMainSlide, ... window.galleryPicturesJson ];
+  if (product.embed_code !== null){
+    galleryArray = [  product.embed_code, ... window.galleryPicturesJson ];
+  } 
+  else {
+    if (!window.galleryPicturesJson) galleryArray = [ product.image_small ]
   }
   const [ gallery, setGallery ] = useState(galleryArray);
   const parentContainerElement = document.getElementById('product-title-div');
@@ -43,7 +45,7 @@ function ProductMediaSlider(){
       if (f.type.indexOf('video') > -1 || f.type.indexOf('audio') > -1) mediaGalleryItems.push(f.url)
     })
     if (mediaGalleryItems.length > 0) {
-      const newGallery = [...gallery, mediaGalleryItems]
+      const newGallery = gallery.concat(mediaGalleryItems);
       setGallery(newGallery);
     }
     setLoading(false);
