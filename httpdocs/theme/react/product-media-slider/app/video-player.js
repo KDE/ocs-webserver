ec841153b5fea38357516e453ec0a43c5fc75b1d
David Nelband <dnelband@gmail.com>
2019-05-09 19:49:36 +0200

    pms

--- httpdocs/theme/react/product-media-slider/app/video-player.js
+++ httpdocs/theme/react/product-media-slider/app/video-player.js
@@ -2,17 +2,12 @@ import React, { useState } from 'react';
 import ReactPlayer from 'react-player'
 
 function VideoPlayerWrapper(props){
-
     const [ source, setSource ] = useState();
-    console.log(source);
-
     React.useEffect(() => { convertStringToUrl(); }, [props.source])
-
     function convertStringToUrl(){
         let newSource = props.source.replace(/%2F/g,'/').replace(/%3A/g,':');
         setSource(newSource);
     }
-
     let videoPlayerDisplay;
     if (source) videoPlayerDisplay = <ReactPlayer url={source} playing={false} controls={true} /> 
     return (
