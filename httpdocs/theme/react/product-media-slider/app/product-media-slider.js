0dee35c2607e51f5e9a4f4c0797cbc8c1563f113
David Nelband <dnelband@gmail.com>
2019-05-10 15:34:25 +0200

    pms fix

--- httpdocs/theme/react/product-media-slider/app/product-media-slider.js
+++ httpdocs/theme/react/product-media-slider/app/product-media-slider.js
@@ -7,43 +7,34 @@ function ProductMediaSlider(){
   /* Component */
 
   const [ product, setProduct ] = useState(window.product);
-  const [ gallery, setGallery ] = useState();
+
+  let galleryArray = []
+  if (window.galleryPicturesJson) window.galleryPicturesJson.forEach(function(gp,index){ galleryArray.push({url:gp,type:'image'}); });
+  else galleryArray = [{url:product.image_small,type:'image'} ];
+  if (product.embed_code !== null && product.embed_code.length > 0) galleryArray = [{url:product.embed_code,type:'embed'}, ... galleryArray ];
+  if (window.filesJson) {
+    window.filesJson.forEach(function(f,index){  
+      if (f.type.indexOf('video') > -1 || f.type.indexOf('audio') > -1) galleryArray = [ {url:f.url,type:f.type.split('/')[0]}, ... galleryArray] 
+    })
+  }
+
+  const [ gallery, setGallery ] = useState(galleryArray);
   const parentContainerElement = document.getElementById('product-title-div');
   const [ containerWidth, setContainerWidth ] = useState(parentContainerElement.offsetWidth);
   const [ currentSlide, setCurrentSlide ] = useState(0)
-  const [ sliderWidth, setSliderWidth ] = useState();
+  const [ sliderWidth, setSliderWidth ] = useState(containerWidth * gallery.length);
   const [ sliderHeight, setSliderHeight ] = useState(315);
   const [ sliderPosition, setSliderPosition ] = useState(containerWidth * currentSlide);
   const [ cinemaMode, setCinemaMode ] = useState(false);
   const [ loading, setLoading ] = useState(true);
 
-  console.log('cinema mode:' + cinemaMode);
-  console.log('gallery width:' + sliderWidth);
-  console.log('gallery height:' + sliderHeight);
-
   React.useEffect(() => { initProductMediaSlider() },[])
-  React.useEffect(() => { if (gallery) updateDimensions() },[currentSlide, cinemaMode])
+  React.useEffect(() => { updateDimensions() },[currentSlide, cinemaMode])
 
   // init product media slider
   function initProductMediaSlider(){
     window.addEventListener("resize", updateDimensions);
     window.addEventListener("orientationchange", updateDimensions);
-    generateProductGallery();
-  }
-
-  // generate product gallery
-  function generateProductGallery(){
-    let galleryArray = []
-    if (window.galleryPicturesJson) window.galleryPicturesJson.forEach(function(gp,index){ galleryArray.push({url:gp,type:'image'}); });
-    else galleryArray = [{url:product.image_small,type:'image'} ];
-    if (product.embed_code !== null && product.embed_code.length > 0) galleryArray = [{url:product.embed_code,type:'embed'}, ... galleryArray ];
-    if (window.filesJson) {
-      window.filesJson.forEach(function(f,index){  
-        if (f.type.indexOf('video') > -1 || f.type.indexOf('audio') > -1) galleryArray = [ {url:f.url,type:f.type.split('/')[0]}, ... galleryArray] 
-      })
-    }
-    setGallery(galleryArray);
-    setSliderWidth(containerWidth * galleryArray.length);
     setLoading(false);
   }
 
@@ -64,9 +55,7 @@ function ProductMediaSlider(){
     $(targetChildPrependedElement).prependTo('#product-main-img-container');
     $("#product-media-slider-container").toggleClass("imgsmall");
     $("#product-media-slider-container").toggleClass("imgfull");
-    setTimeout(() => {
-      setCinemaMode(newCinemaMode);      
-    }, 1000);
+    setCinemaMode(newCinemaMode);      
   }
 
   /* Render */
