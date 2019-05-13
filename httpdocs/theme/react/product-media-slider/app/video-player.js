c695c9a0d8bfa6c3b26c5c34e2743d713558ea05
David Nelband <dnelband@gmail.com>
2019-05-13 12:10:11 +0200

    pms fix

--- httpdocs/theme/react/product-media-slider/app/video-player.js
+++ httpdocs/theme/react/product-media-slider/app/video-player.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Player } from 'video-react';
+import { play } from 'video-react/lib/actions/player';
 
 function VideoPlayerWrapper(props){
 
+    const playerEl = useRef(null)
     const [ source, setSource ] = useState();
+
+    console.log(playerEl.getState())
     
     React.useEffect(() => { convertStringToUrl(); }, [props.source])
+    React.useEffect(() => { console.log(playerEl)},[playerEl])
     
     function convertStringToUrl(){
         let newSource = props.source.replace(/%2F/g,'/').replace(/%3A/g,':');
@@ -16,6 +21,7 @@ function VideoPlayerWrapper(props){
     if (source){
         videoPlayerDisplay = (
             <Player
+                ref={playerEl}
                 height={props.height}
                 width={props.width}
                 playsInline
@@ -23,6 +29,7 @@ function VideoPlayerWrapper(props){
             />            
         )
     }
+
     return (
         <div className="react-player-container">
             {videoPlayerDisplay}
