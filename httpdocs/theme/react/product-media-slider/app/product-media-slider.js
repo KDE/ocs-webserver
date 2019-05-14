27e85b5026ecedd140592bf304f42274d54c9555
David Nelband <dnelband@gmail.com>
2019-05-14 17:39:47 +0200

    pms imageonload

--- httpdocs/theme/react/product-media-slider/app/product-media-slider.js
+++ httpdocs/theme/react/product-media-slider/app/product-media-slider.js
@@ -144,11 +144,16 @@ function ProductMediaSlider(){
 }
 
 function SlideItem(props){
+
+  function imageOnLoad(e){
+    console.log(e)
+    console.log('image one load');
+  }
   
   let slideContentDisplay, slideMediaItemMenu;
     if (props.slide.type === "embed") slideContentDisplay = <div dangerouslySetInnerHTML={{__html: props.slide.url}} />;
     else if (props.slide.type === "image") {
-      slideContentDisplay = <img onClick={props.onCinemaModeClick} id={"slide-img-"+props.slideIndex} src={props.slide.url}/>
+      slideContentDisplay = <img onClick={props.onCinemaModeClick} onLoad={e => imageOnLoad(e)} id={"slide-img-"+props.slideIndex} src={props.slide.url}/>
       /*slideMediaItemMenu = (
         <ul className="slide-media-item-menu">
           <li><a onClick={props.onCinemaModeClick} className="cinema-mode">cinema</a></li>
