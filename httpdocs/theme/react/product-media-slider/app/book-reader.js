4fffff4eb17f39df99e21f463bd9f3ba702db62c
David Nelband <dnelband@gmail.com>
2019-06-05 16:47:16 +0200

    pms book reader pager

--- httpdocs/theme/react/product-media-slider/app/book-reader.js
+++ httpdocs/theme/react/product-media-slider/app/book-reader.js
@@ -3,30 +3,52 @@ import React, { useState } from 'react';
 function BookReaderWrapper(props){
   
   const [ renditionState , setRenditionState ] = useState()
+  const [ progress, setProgress ] = useState()
+  const [ currentPage, setCurrentPage ] = useState();
+  const [ totalPages, setTotalPages ] = useState();
 
-  React.useEffect(() => {
-    var book = ePub(props.slide.url);
-    var rendition = book.renderTo("book-container", { flow: "paginated", width: props.width - 40, height: props.height - 40});
+  React.useEffect(() => {initBookReader()},[])
+  React.useEffect(() => {initBookReader()},[props.cinemaMode])
+
+  function initBookReader(){
+    // Initialize the book
+    let book = ePub(props.slide.url, {});
+    let rendition = book.renderTo('book-container', {
+        flow: 'paginated',
+        manager: 'continuous',
+        spread: 'always',
+        width: "100% - 106px",
+        height: (props.height - 40)
+    });
     setRenditionState(rendition)
-    var displayed = rendition.display();
-    book.ready.then((book) => {
-      console.log(book);
-      /*let meta = book.package.metadata; // Metadata from the package json
-      let toc = book.navigation.toc; // Table of Contents
-      let landmarks = book.navigation.landmarks; // landmarks
-      let spine = book.spine; // landmarks
-      let cover = book.cover; // landmarks
-      let resources = book.resources; // landmarks
-      let pageList = book.pageList; // page list (if present)
-      console.log(meta);
-      console.log(toc);
-      console.log(landmarks);
-      console.log(spine);
-      console.log(cover);
-      console.log(resources);
-      console.log(pageList);*/
+
+    // Display the book
+    let displayed = rendition.display(window.location.hash.substr(1) || undefined);
+    displayed.then(function() {
+        // console.log('rendition.currentLocation():', rendition.currentLocation());
+    });
+
+    // Generate location and pagination
+    book.ready.then(function() {
+        const stored = localStorage.getItem(book.key() + '-locations');
+        // console.log('metadata:', book.package.metadata);
+        if (stored) {
+            return book.locations.load(stored);
+        } else {
+            return book.locations.generate(1024); // Generates CFI for every X characters (Characters per/page)
+        }
+    }).then(function(location) { // This promise will take a little while to return (About 20 seconds or so for Moby Dick)
+        localStorage.setItem(book.key() + '-locations', book.locations.save());
+    });
+
+    // When navigating to the next/previous page
+    rendition.on('relocated', function(locations) {
+        const newProgress = book.locations.percentageFromCfi(locations.start.cfi);
+        setProgress(newProgress);
+        setCurrentPage(book.locations.locationFromCfi(locations.start.cfi));
+        setTotalPages(book.locations.total)
     })
-  },[])
+  }
 
   function goPrev(){
     renditionState.prev();
@@ -36,12 +58,18 @@ function BookReaderWrapper(props){
     renditionState.next();
   }
 
+  let pageCountDisplay;
+  if (totalPages) pageCountDisplay = <span>{currentPage + "/" + totalPages}</span>
+
   return (
     <div id="book-reader-wrapper">
       <div id="prev" className="arrow" onClick={goPrev}>
         <span className="glyphicon glyphicon-chevron-left"></span>  
       </div>
       <div id="book-container"></div>
+      <div id="book-pager">
+        {pageCountDisplay}
+      </div>
       <div id="next" className="arrow" onClick={goNext}>
         <span className="glyphicon glyphicon-chevron-right"></span>  
       </div>
