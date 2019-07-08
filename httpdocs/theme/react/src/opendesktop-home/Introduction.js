96919b69d364f42d9a50bba626334c20ff0e37b1
siyuan dong <dongsiyuan@gmail.com>
2019-07-08 15:18:59 +0200

    homepage

--- httpdocs/theme/react/src/opendesktop-home/Introduction.js
+++ httpdocs/theme/react/src/opendesktop-home/Introduction.js
@@ -5,16 +5,25 @@ function Introduction(props) {
     <div className="introduction">
       <h1> Welcome to opendesktop</h1>
       <h4> Opendesktop is community based portal, dedicated to libre content, opencode, free software and operating system.</h4>
-      <ul>
-        <li><a href="https://opendesktop.org">Pling.com</a></li>
-        <li><a href="https://opencode.net">Opencode</a></li>
-        <li><a href="https://forum.opendesktop.org">Discourse</a></li>
-        <li><a href="https://chat.opendesktop.org">Riot</a></li>
-        <li>Mastodon</li>
-        <li><a href="https://my.opendesktop.org">Nextcloud</a></li>
-      </ul>
+      <div className="intro-container">
+        <div className="code-container sub-container"><a href="https://opencode.net">Code</a></div>
+        <div className="pub-container sub-container"><a href="https://opendesktop.org">Publish</a></div>
+        <div className="comm-container sub-container"><a href="https://forum.opendesktop.org">Community</a></div>
+        <div className="personal-container sub-container"><a href="https://my.opendesktop.org">Personal</a></div>
+      </div>
+
     </div>
   )
 }
 
+/*
+<ul>
+  <li><a href="https://opendesktop.org">Pling.com</a></li>
+  <li><a href="https://opencode.net">Opencode</a></li>
+  <li><a href="https://forum.opendesktop.org">Discourse</a></li>
+  <li><a href="https://chat.opendesktop.org">Riot</a></li>
+  <li>Mastodon</li>
+  <li><a href="https://my.opendesktop.org">Nextcloud</a></li>
+</ul>
+*/
 export default Introduction;
