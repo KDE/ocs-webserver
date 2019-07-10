f36cf3cbfb373f2927dfb3700a18c1ac82374a21
siyuan dong <dongsiyuan@gmail.com>
2019-07-10 14:13:21 +0200

    startpage pling

--- httpdocs/theme/react/src/opendesktop-home/Introduction.js
+++ httpdocs/theme/react/src/opendesktop-home/Introduction.js
@@ -3,15 +3,35 @@ import React from 'react';
 function Introduction(props) {
   return (
     <div className="introduction">
-      <h1> Welcome to opendesktop.org</h1>
-      <h4>
-    OpenDesktop is a portal providing 100% libre services for coding, publishing, messenging and personal data storage.
-  </h4>
+      <div className="intro-desc">
+        <h1> Welcome to opendesktop.org</h1>
+        <h4>
+          OpenDesktop is a portal providing 100% libre services for coding, publishing, messenging and personal data storage.
+        </h4>
+      </div>
       <ul>
-        <li><a href="https://opencode.net">Code</a></li>
-        <li><a href="https://pling.com">Publish</a></li>
-        <li><a href="https://forum.opendesktop.org">Community</a></li>
-        <li><a href="https://my.opendesktop.org">Personal</a></li>
+        <li>
+            <a href="https://opencode.net"><span className="link-code link-image"></span></a>
+            <span><a className="link-code-showmore">Show more</a></span>
+        </li>
+        <li>
+
+            <a href="https://pling.com"><span className="link-publish link-image"></span></a>
+            <span><a className="link-publish-showmore">Show more</a></span>
+
+        </li>
+        <li>
+
+            <a href="https://forum.opendesktop.org"><span className="link-community link-image"></span></a>
+            <span>Show more</span>
+
+        </li>
+        <li>
+
+            <a href="https://my.opendesktop.org"><span className="link-personal link-image"></span></a>
+            <span>Show more</span>
+
+        </li>
       </ul>
     </div>
   )
