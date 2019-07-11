165f45fcc8c4658166bd6f23c54d1c9e347032fc
David Nelband <dnelband@gmail.com>
2019-07-11 14:11:24 +0200

    ctf

--- httpdocs/theme/react/category-tree/app/category-tree.js
+++ httpdocs/theme/react/category-tree/app/category-tree.js
@@ -277,22 +277,25 @@ function CategoryPanelsContainer(props){
     React.useEffect(() => {
 
         let showback = true, showForward = false;
-        let homePageUrl = window.config.baseUrlStore + "/";
+        let minSliderPosition = 0, homePageUrl = window.config.baseUrlStore + "/";
         if (props.storeInfo && props.storeInfo.is_show_in_menu === "0"){
+            minSliderPosition = containerWidth;
             homePageUrl = props.storeInfo.menuhref + "/";
         }
 
         console.log(homePageUrl);
         console.log(window.location.href);
 
-        if (sliderPosition === 0){
+        if (sliderPosition === minSliderPosition){
             showback = false;
             if (homePageUrl === window.location.href){
                 showForward = false;
             } else {
                 showForward = true;
             }            
-        } else if (sliderPosition < ((panels.length - 1) * containerWidth)){
+        } 
+        
+        if (sliderPosition < ((panels.length - 1) * containerWidth)){
             if (homePageUrl === window.location.href){
                 showForward = false;
             } else {
