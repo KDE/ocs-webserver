6cd4a09a2ce3e402774bfe92264e76524dd550fa
David Nelband <dnelband@gmail.com>
2019-07-12 16:29:06 +0200

    cts

--- httpdocs/theme/react/category-tree/app/category-tree.js
+++ httpdocs/theme/react/category-tree/app/category-tree.js
@@ -549,11 +549,9 @@ function CategoryMenuItem(props){
         if (c.id && props.categoryId === parseInt(c.id) || props.selectedCategoriesId.indexOf(c.id) > -1 || window.location.href === catLink ||  window.location.href === catLink + "/") categoryMenuItemClassName = "active";
     }
 
-    if (catLink.indexOf('browse') === -1){
-        if (window.location.href.indexOf(catLink) > -1) categoryMenuItemClassName = "active";
-    }
+    console.log(catTitle,json_store_name,cid,json_cat_id);
 
-    if (catTitle === json_store_name) categoryMenuItemClassName = "active";
+    if (catTitle === json_store_name && c.id === json_cat_id) categoryMenuItemClassName = "active";
 
     return(
         <li className={categoryMenuItemClassName} >
