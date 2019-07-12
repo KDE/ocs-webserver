0e71d2f0671a82857313329c3a608f8ab4f624d6
David Nelband <dnelband@gmail.com>
2019-07-12 13:07:54 +0200

    ctf

--- httpdocs/theme/react/category-tree/app/category-tree.js
+++ httpdocs/theme/react/category-tree/app/category-tree.js
@@ -437,7 +437,7 @@ function CategoryPanel(props){
     }
 
     function onSetCategoryPanelHeight(panelHeight){
-        adjustSliderHeight(panelHeight + 20);
+        adjustSliderHeight(panelHeight + 25);
     }
 
     let categoryPanelContent;
@@ -446,7 +446,7 @@ function CategoryPanel(props){
         if (props.categories.length > 0){
             categories = props.categories.sort(sortArrayAlphabeticallyByTitle);
             categories = categories.map((c,index) =>{
-                if (categories.length === (index + 1)){ onSetCategoryPanelHeight(categories.length * 20); } 
+                if (categories.length === (index + 1)){ onSetCategoryPanelHeight(categories.length * 25); } 
                 let showCategory = true;
                 if (c.is_show_in_menu){
                     if (c.is_show_in_menu === "0") showCategory = false;
