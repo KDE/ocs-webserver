defd7f5ae8e46bd23649673679c768059bc68449
siyuan dong <dongsiyuan@gmail.com>
2019-07-15 11:46:04 +0200

    metaheader

--- httpdocs/theme/react/src/metaheader/MobileLeftSidePanel.js
+++ httpdocs/theme/react/src/metaheader/MobileLeftSidePanel.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import DomainsMenuGroup from './DomainsMenuGroup';
-import DevelopmentDropDownMenu from './DevelopmentDropDownMenu';
+
 class MobileLeftSidePanel extends React.Component {
   constructor(props){
     super(props);
@@ -8,37 +7,38 @@ class MobileLeftSidePanel extends React.Component {
   }
 
   componentDidMount() {
-    let menuGroups = [];
-    this.props.domains.forEach(function(domain,index){
-      if (menuGroups.indexOf(domain.menugroup) === -1){
-        menuGroups.push(domain.menugroup);
-      }
-    });
-    this.setState({menuGroups:menuGroups});
+    // let menuGroups = [];
+    // this.props.domains.forEach(function(domain,index){
+    //   if (menuGroups.indexOf(domain.menugroup) === -1){
+    //     menuGroups.push(domain.menugroup);
+    //   }
+    // });
+    // this.setState({menuGroups:menuGroups});
   }
 
   render(){
-    let panelMenuGroupsDisplay;
-    if (this.state.menuGroups){
-      panelMenuGroupsDisplay = this.state.menuGroups.map((mg,i) => (
-        <DomainsMenuGroup
-          key={i}
-          domains={this.props.domains}
-          menuGroup={mg}
-          sName={this.props.sName}
-        />
-      ));
-    }
+    // let panelMenuGroupsDisplay;
+    // if (this.state.menuGroups){
+    //   panelMenuGroupsDisplay = this.state.menuGroups.map((mg,i) => (
+    //     <DomainsMenuGroup
+    //       key={i}
+    //       domains={this.props.domains}
+    //       menuGroup={mg}
+    //       sName={this.props.sName}
+    //     />
+    //   ));
+    // }
 
-    let faqLinkItem, apiLinkItem, aboutLinkItem;
+
+    let apiLinkItem, aboutLinkItem,aboutPlingItem, aboutopencodeItem;
+
+    aboutPlingItem = (<li><a id="faq" href={this.props.baseUrl +"/faq-pling"}>FAQ Pling</a></li>);
+    aboutopencodeItem = (<li><a id="faq" href={this.props.baseUrl +"/faq-opencode"}>FAQ Opencode</a></li>);
+    aboutLinkItem = (<li><a id="about" href={this.props.baseUrl +"/about"}>About</a></li>);
     if (this.props.isExternal === false){
-      faqLinkItem = (<li><a className="popuppanel" id="faq" href={"/plings"}>FAQ</a></li>);
       apiLinkItem = (<li><a className="popuppanel" id="api" href={"/partials/ocsapicontent.phtml"}>API</a></li>);
-      aboutLinkItem = (<li><a className="popuppanel" id="about" href={"/partials/about.phtml"}>About</a></li>);
     } else {
-      faqLinkItem = (<li><a className="popuppanel" target="_blank" id="faq" href={this.props.baseUrl + "/#faq"}>FAQ</a></li>);
       apiLinkItem = (<li><a className="popuppanel" target="_blank" id="api" href={this.props.baseUrl + "/#api"}>API</a></li>);
-      aboutLinkItem = (<li><a className="popuppanel" target="_blank" id="about" href={this.props.baseUrl + "/#about"}>About</a></li>);
     }
 
     return (
@@ -51,30 +51,24 @@ class MobileLeftSidePanel extends React.Component {
         </div>
         <div id="panel-menu">
           <ul>
-            {panelMenuGroupsDisplay}
+            <li><a href={this.props.baseUrlStore}>Publish</a></li>
+            <li><a href={this.props.gitlabUrl}>Code</a></li>
             <li>
-              <a className="groupname"><b>Discussion Boards</b></a>
+              <a className="groupname"><b>Community</b></a>
               <ul>
-                <li><a href={this.props.forumUrl }>General</a></li>
-                <li><a href={this.props.forumUrl + "/c/themes"}>Themes</a></li>
-                <li><a href={this.props.forumUrl + "/c/apps"}>Apps</a></li>
-                <li><a href={this.props.forumUrl + "/c/coding"}>Coding</a></li>
+                <li><a href={this.props.baseUrl + "/community"}>Member</a></li>
+                <li><a href={this.props.forumUrl}>Discussion</a></li>
               </ul>
             </li>
-            <DevelopmentDropDownMenu
-              user={this.props.user}
-              baseUrl={this.props.baseUrl}
-              gitlabUrl={this.props.gitlabUrl}
-              isAdmin={this.props.isAdmin}
-            />
+
             <li>
               <a className="groupname"><b>More</b></a>
               <ul>
-                <li><a href={this.props.baseUrl + "/community"}>Community</a></li>
                 <li><a href={this.props.baseUrl + "/support"}>Support</a></li>
-                <li><a href={this.props.blogUrl} target="_blank">Blog</a></li>
-                {faqLinkItem}
+                <li><a href={this.props.blogUrl} target="_blank">Blog</a></li>                
                 {apiLinkItem}
+                {aboutPlingItem}
+                {aboutopencodeItem}
                 {aboutLinkItem}
               </ul>
             </li>
