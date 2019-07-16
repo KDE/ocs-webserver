dabf454fb0c86338b423ffbb902c40ff34ce4824
David Nelband <dnelband@gmail.com>
2019-07-16 14:41:15 +0200

    pbf

--- httpdocs/theme/react/product-browse/app/product-browse.js
+++ httpdocs/theme/react/product-browse/app/product-browse.js
@@ -5,12 +5,12 @@ import {isMobile} from 'react-device-detect';
 function ProductBrowse(){
 
     React.useEffect(() => {
-        console.log(catId);
-        console.log(filters);
-        console.log(products);
-        console.log(topProducts);
-        console.log(pagination);
-        console.log(baseUrl);
+        console.log(window.filters);
+        console.log(window.products);
+        console.log(window.topProducts);
+        console.log(window.pagination);
+        console.log(window.baseUrl);
+        console.log(window.catId);
     },[])
 
     return (
