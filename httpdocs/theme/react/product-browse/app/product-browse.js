b539b0a014cd62bea1ecf501098b93d607f9dba7
David Nelband <dnelband@gmail.com>
2019-07-16 22:04:30 +0200

    pbf

--- httpdocs/theme/react/product-browse/app/product-browse.js
+++ httpdocs/theme/react/product-browse/app/product-browse.js
@@ -5,17 +5,78 @@ import {isMobile} from 'react-device-detect';
 function ProductBrowse(){
 
     React.useEffect(() => {
-        console.log(filters);
-        console.log(products);
-        console.log(topProducts);
-        console.log(pagination);
+        console.log('product browse')
         console.log(catId);
     },[])
 
     return (
         <div id="product-browse">
-
+            <ProductBrowseFilterContainer/>
+            <ProductBrowseItemList/>
+            <ProductBrowsePagination/>
         </div>
+    )
+}
+
+function ProductBrowseFilterContainer(){
+
+    React.useEffect(() => {
+        console.log('product browse filter container');
+        console.log(filters);
+    },[])
+
+    return (
+        <div id="product-browse-top-menu">
+        </div>
+    )
+}
+
+function ProductBrowseItemList(){
+    
+    React.useEffect(() => {
+        console.log('product browse item list')
+        console.log(products);
+        console.log(topProducts);
+        console.log(pagination);
+    },[])
+
+
+    const productsDisplay = products.map((p,index) => (
+        <ProductBrowseItem
+            key={index}
+            product={p}
+            index={index}
+        />
+    ))
+
+    return (
+        <div id="product-browse-item-list">
+            {productsDisplay}
+        </div>
+    )
+}
+
+function ProductBrowseItem(){
+
+    React.useEffect(() => {
+        console.log(props.product);
+    },[])
+
+
+    return (
+        <div className="product-browse-item"></div>
+    )
+}
+
+function ProductBrowsePagination(){
+
+    React.useEffect(() => {
+        console.log('product browse pagination')
+        console.log(pagination);
+    },[])
+
+    return (
+        <div id="product-browse-pagination"></div>
     )
 }
 
