5efd40d47a855fe8d5037ccce60f5300f2f46ac0
David Nelband <dnelband@gmail.com>
2019-08-01 10:51:12 +0200

    pbp

--- httpdocs/theme/react/product-browse/app/product-browse.js
+++ httpdocs/theme/react/product-browse/app/product-browse.js
@@ -133,163 +133,168 @@ function ProductBrowseItem(props){
 
 function ProductBrowseItemPreviewMusicPlayer(props){
 
-    let musicSrc, title;
+    let musicPlayerDisplay;
+    let musicSrc, title; 
+    
     if (props.file && props.file.url) {
         musicSrc = props.file.url.replace(/%2F/g,'/').replace(/%3A/g,':');
         title = props.file.title;
-    }
-
-    const options = {
-        //audio lists model
-        audioLists:[{
-            musicSrc:musicSrc,
-            name:title,
-            cover:''
-        }],
-        audioListsPanelVisible:false,
-        //default play index of the audio player  [type `number` default `0`]
-        defaultPlayIndex: 0,
-        //if you want dynamic change current play audio you can change it [type `number` default `0`]
-        // playIndex: 0,
-        //color of the music player theme    [ type `string: 'light' or 'dark'  ` default 'dark' ]
-        theme: "dark",
-        // Specifies movement boundaries. Accepted values:
-        // - `parent` restricts movement within the node's offsetParent
-        //    (nearest node with position relative or absolute), or
-        // - a selector, restricts movement within the targeted node
-        // - An object with `left, top, right, and bottom` properties.
-        //   These indicate how far in each direction the draggable
-        //   can be moved.
-        bounds: "product-"+props.projectId,
-        //Whether to load audio immediately after the page loads.  [type `Boolean | String`, default `false`]
-        //"auto|metadata|none" "true| false"
-        preload: false,
-        //Whether the player's background displays frosted glass effect  [type `Boolean`, default `false`]
-        glassBg: false,
-        //The next time you access the player, do you keep the last state  [type `Boolean` default `false`]
-        remember: false,
-        //The Audio Can be deleted  [type `Boolean`, default `true`]
-        remove: false,
-        //audio controller initial position    [ type `Object` default '{top:0,left:0}' ]
-        defaultPosition: {
-          top: 50,
-          left: 50
-        },
-        // play mode text config of the audio player
-        playModeText: {
-          order: "order",
-          orderLoop: "loop",
-          singleLoop: "single loop",
-          shufflePlay: "shuffle"
-        },
-        //audio controller open text  [ type `String | ReactNode` default 'open']
-        openText: "open",
-        //audio controller close text  [ type `String | ReactNode` default 'close']
-        closeText: "close",
-        //audio theme switch checkedText  [ type `String | ReactNode` default '-']
-        checkedText: "dark",      
-        //audio theme switch unCheckedText [ type `String | ReactNode` default '-']
-        unCheckedText: "light",
-        // audio list panel show text of the playlist has no songs [ type `String` | ReactNode  default 'no music']
-        notContentText: "No Music",
-        panelTitle: "Test",
-        defaultPlayMode: "order",
-        //audio mode        mini | full          [type `String`  default `mini`]
-        mode: "full",
-          // [ type `Boolean` default 'false' ]
-          // The default audioPlay handle function will be played again after each pause, If you only want to trigger it once, you can set 'true'
-        once: true,
-        //Whether the audio is played after loading is completed. [type `Boolean` default 'true']
-        autoPlay: false,
-        //Whether you can switch between two modes, full => mini  or mini => full   [type 'Boolean' default 'true']
-        toggleMode: false,
-        //audio cover is show of the "mini" mode [type `Boolean` default 'true']
-        showMiniModeCover: false,   
-        //audio playing progress is show of the "mini"  mode
-        showMiniProcessBar: false,
-        //audio controller is can be drag of the "mini" mode     [type `Boolean` default `true`]
-        drag: true,
-        //drag the audio progress bar [type `Boolean` default `true`]
-        seeked: false,
-        //audio controller title [type `String | ReactNode`  default <FaHeadphones/>]
-        // controllerTitle: <FaHeadphones />,
-        //Displays the audio load progress bar.  [type `Boolean` default `true`]
-        showProgressLoadBar: false,
-        //play button display of the audio player panel   [type `Boolean` default `true`]
-        showPlay: true,
-        //reload button display of the audio player panel   [type `Boolean` default `true`]
-        showReload: false,
-        //download button display of the audio player panel   [type `Boolean` default `true`]
-        showDownload: false,
-        //loop button display of the audio player panel   [type `Boolean` default `true`]
-        showPlayMode: false,
-        //theme toggle switch  display of the audio player panel   [type `Boolean` default `true`]
-        showThemeSwitch: false,
-        //lyric display of the audio player panel   [type `Boolean` default `false`]
-        showLyric: false,
-        //Extensible custom content       [type 'Array' default '[]' ]
-        extendsContent: [],
-        //default volume of the audio player [type `Number` default `100` range `0-100`]
-        defaultVolume: 30,
-        //playModeText show time [type `Number(ms)` default `700`]
-        playModeShowTime: 600,
-        //Whether to try playing the next audio when the current audio playback fails [type `Boolean` default `true`]
-        loadAudioErrorPlayNext: false,
-        //Music is downloaded handle
-        //onAudioDownload(audioInfo) { console.log("audio download", audioInfo); },
-        //audio play handle
-        onAudioPlay(audioInfo) { 
-            $('#project-'+props.projectId).find('.play-btn[title="Click to play"]').trigger("click");
-         },
-        //audio pause handle
-        onAudioPause(audioInfo) { 
-          console.log("audio pause"); 
-        },
-        //When the user has moved/jumped to a new location in audio
-        onAudioSeeked(audioInfo) { console.log("audio seeked", audioInfo); },
-        //When the volume has changed  min = 0.0  max = 1.0
-        onAudioVolumeChange(currentVolume) { console.log("audio volume change", currentVolume); },
-        //The single song is ended handle
-        onAudioEnded(audioInfo) { console.log("audio ended", audioInfo); },
-        //audio load abort The target event like {...,audioName:xx,audioSrc:xx,playMode:xx}
-        onAudioAbort(e) { console.log("audio abort", e); },
-        //audio play progress handle
-        onAudioProgress(audioInfo) { 
-            if (audioInfo.currentTime >= 30){
-                console.log('stop audio');
+        const options = {
+            //audio lists model
+            audioLists:[{
+                musicSrc:musicSrc,
+                name:title,
+                cover:''
+            }],
+            audioListsPanelVisible:false,
+            //default play index of the audio player  [type `number` default `0`]
+            defaultPlayIndex: 0,
+            //if you want dynamic change current play audio you can change it [type `number` default `0`]
+            // playIndex: 0,
+            //color of the music player theme    [ type `string: 'light' or 'dark'  ` default 'dark' ]
+            theme: "dark",
+            // Specifies movement boundaries. Accepted values:
+            // - `parent` restricts movement within the node's offsetParent
+            //    (nearest node with position relative or absolute), or
+            // - a selector, restricts movement within the targeted node
+            // - An object with `left, top, right, and bottom` properties.
+            //   These indicate how far in each direction the draggable
+            //   can be moved.
+            bounds: "product-"+props.projectId,
+            //Whether to load audio immediately after the page loads.  [type `Boolean | String`, default `false`]
+            //"auto|metadata|none" "true| false"
+            preload: false,
+            //Whether the player's background displays frosted glass effect  [type `Boolean`, default `false`]
+            glassBg: false,
+            //The next time you access the player, do you keep the last state  [type `Boolean` default `false`]
+            remember: false,
+            //The Audio Can be deleted  [type `Boolean`, default `true`]
+            remove: false,
+            //audio controller initial position    [ type `Object` default '{top:0,left:0}' ]
+            defaultPosition: {
+              top: 50,
+              left: 50
+            },
+            // play mode text config of the audio player
+            playModeText: {
+              order: "order",
+              orderLoop: "loop",
+              singleLoop: "single loop",
+              shufflePlay: "shuffle"
+            },
+            //audio controller open text  [ type `String | ReactNode` default 'open']
+            openText: "open",
+            //audio controller close text  [ type `String | ReactNode` default 'close']
+            closeText: "close",
+            //audio theme switch checkedText  [ type `String | ReactNode` default '-']
+            checkedText: "dark",      
+            //audio theme switch unCheckedText [ type `String | ReactNode` default '-']
+            unCheckedText: "light",
+            // audio list panel show text of the playlist has no songs [ type `String` | ReactNode  default 'no music']
+            notContentText: "No Music",
+            panelTitle: "Test",
+            defaultPlayMode: "order",
+            //audio mode        mini | full          [type `String`  default `mini`]
+            mode: "full",
+              // [ type `Boolean` default 'false' ]
+              // The default audioPlay handle function will be played again after each pause, If you only want to trigger it once, you can set 'true'
+            once: true,
+            //Whether the audio is played after loading is completed. [type `Boolean` default 'true']
+            autoPlay: false,
+            //Whether you can switch between two modes, full => mini  or mini => full   [type 'Boolean' default 'true']
+            toggleMode: false,
+            //audio cover is show of the "mini" mode [type `Boolean` default 'true']
+            showMiniModeCover: false,   
+            //audio playing progress is show of the "mini"  mode
+            showMiniProcessBar: false,
+            //audio controller is can be drag of the "mini" mode     [type `Boolean` default `true`]
+            drag: true,
+            //drag the audio progress bar [type `Boolean` default `true`]
+            seeked: false,
+            //audio controller title [type `String | ReactNode`  default <FaHeadphones/>]
+            // controllerTitle: <FaHeadphones />,
+            //Displays the audio load progress bar.  [type `Boolean` default `true`]
+            showProgressLoadBar: false,
+            //play button display of the audio player panel   [type `Boolean` default `true`]
+            showPlay: true,
+            //reload button display of the audio player panel   [type `Boolean` default `true`]
+            showReload: false,
+            //download button display of the audio player panel   [type `Boolean` default `true`]
+            showDownload: false,
+            //loop button display of the audio player panel   [type `Boolean` default `true`]
+            showPlayMode: false,
+            //theme toggle switch  display of the audio player panel   [type `Boolean` default `true`]
+            showThemeSwitch: false,
+            //lyric display of the audio player panel   [type `Boolean` default `false`]
+            showLyric: false,
+            //Extensible custom content       [type 'Array' default '[]' ]
+            extendsContent: [],
+            //default volume of the audio player [type `Number` default `100` range `0-100`]
+            defaultVolume: 30,
+            //playModeText show time [type `Number(ms)` default `700`]
+            playModeShowTime: 600,
+            //Whether to try playing the next audio when the current audio playback fails [type `Boolean` default `true`]
+            loadAudioErrorPlayNext: false,
+            //Music is downloaded handle
+            //onAudioDownload(audioInfo) { console.log("audio download", audioInfo); },
+            //audio play handle
+            onAudioPlay(audioInfo) { 
+                $('#project-'+props.projectId).find('.play-btn[title="Click to play"]').trigger("click");
+             },
+            //audio pause handle
+            onAudioPause(audioInfo) { 
+              console.log("audio pause"); 
+            },
+            //When the user has moved/jumped to a new location in audio
+            onAudioSeeked(audioInfo) { console.log("audio seeked", audioInfo); },
+            //When the volume has changed  min = 0.0  max = 1.0
+            onAudioVolumeChange(currentVolume) { console.log("audio volume change", currentVolume); },
+            //The single song is ended handle
+            onAudioEnded(audioInfo) { console.log("audio ended", audioInfo); },
+            //audio load abort The target event like {...,audioName:xx,audioSrc:xx,playMode:xx}
+            onAudioAbort(e) { console.log("audio abort", e); },
+            //audio play progress handle
+            onAudioProgress(audioInfo) { 
+                if (audioInfo.currentTime >= 30){
+                    console.log('stop audio');
+                }
+            },
+            //audio reload handle
+            onAudioReload(audioInfo) { console.log("audio reload:", audioInfo);},
+            //audio load failed error handle
+            onAudioLoadError(e) { console.log("audio load err", e); },
+            //theme change handle
+            onThemeChange(theme) { console.log("theme change:", theme); },
+            //audio lists change
+            onAudioListsChange(currentPlayId, audioLists, audioInfo) {
+              console.log("[currentPlayId] audio lists change:", currentPlayId);
+              console.log("[audioLists] audio lists change:", audioLists);
+              console.log("[audioInfo] audio lists change:", audioInfo);
+              console.log(audioInfo)
+            },
+            onAudioPlayTrackChange(currentPlayId, audioLists, audioInfo) { console.log( "audio play track change:", currentPlayId, audioLists, audioInfo ); },
+            onPlayModeChange(playMode) { console.log("play mode change:", playMode); },
+            onModeChange(mode) { console.log("mode change:", mode); },
+            onAudioListsPanelChange(panelVisible) {
+              const newShowPlayListValue = showPlaylist === true ? false : true;
+              setShowPlaylist(newShowPlayListValue);
+            }, 
+            onAudioListsDragEnd(fromIndex, endIndex) {
+              console.log("audio lists drag end:", fromIndex, endIndex);
+            },
+            onAudioLyricChange(lineNum, currentLyric) {
+              console.log("audio lyric change:", lineNum, currentLyric);
             }
-        },
-        //audio reload handle
-        onAudioReload(audioInfo) { console.log("audio reload:", audioInfo);},
-        //audio load failed error handle
-        onAudioLoadError(e) { console.log("audio load err", e); },
-        //theme change handle
-        onThemeChange(theme) { console.log("theme change:", theme); },
-        //audio lists change
-        onAudioListsChange(currentPlayId, audioLists, audioInfo) {
-          console.log("[currentPlayId] audio lists change:", currentPlayId);
-          console.log("[audioLists] audio lists change:", audioLists);
-          console.log("[audioInfo] audio lists change:", audioInfo);
-          console.log(audioInfo)
-        },
-        onAudioPlayTrackChange(currentPlayId, audioLists, audioInfo) { console.log( "audio play track change:", currentPlayId, audioLists, audioInfo ); },
-        onPlayModeChange(playMode) { console.log("play mode change:", playMode); },
-        onModeChange(mode) { console.log("mode change:", mode); },
-        onAudioListsPanelChange(panelVisible) {
-          const newShowPlayListValue = showPlaylist === true ? false : true;
-          setShowPlaylist(newShowPlayListValue);
-        }, 
-        onAudioListsDragEnd(fromIndex, endIndex) {
-          console.log("audio lists drag end:", fromIndex, endIndex);
-        },
-        onAudioLyricChange(lineNum, currentLyric) {
-          console.log("audio lyric change:", lineNum, currentLyric);
-        }
-    };
+        };
+    
+        musicPlayerDisplay = <ReactJkMusicPlayer {...options} />
+        
+        
+    }
 
     return (
         <div className="product-browse-item-preview-music-player">
-            <ReactJkMusicPlayer {...options} />
+            {musicPlayerDisplay}
         </div>
     )
 }
