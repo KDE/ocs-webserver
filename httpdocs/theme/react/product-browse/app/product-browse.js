6d9f431f0c75d9cb2aa866c0d23b9d730b251d19
David Nelband <dnelband@gmail.com>
2019-08-09 11:55:29 +0200

    pbp fix

--- httpdocs/theme/react/product-browse/app/product-browse.js
+++ httpdocs/theme/react/product-browse/app/product-browse.js
@@ -116,7 +116,7 @@ function ProductBrowseItem(props){
     else {
         imgUrl = "https://cn.opendesktop.";
         imgUrl += window.location.host.endsWith('org') === true || window.location.host.endsWith('com') === true  ? "org" : "cc";
-        imgUrl += "/cache/" + (props.itemWidth * 2) + "x" + (props.imgHeight * 2) + "/img/" + p.image_small;    
+        imgUrl += "/cache/" + Math.ceil(props.itemWidth * 2) + "x" + Math.ceil(props.imgHeight * 2) + "/img/" + p.image_small;    
     }
 
     let itemLink = json_serverUrl;
