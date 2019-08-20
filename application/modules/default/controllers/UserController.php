c553285ea86034a78bf6b813b5edf7ffc5251556
Ronald von Schlegell <rvs75@email.de>
2019-08-20 15:34:07 +0200

    support

--- application/modules/default/controllers/UserController.php
+++ application/modules/default/controllers/UserController.php
@@ -824,6 +824,25 @@ class UserController extends Local_Controller_Action_DomainSwitch
 
 
     }
+    
+    public function fundingAction()
+    {
+
+        $helperUserRole = new Backend_View_Helper_UserRole();
+        $userRoleName = $helperUserRole->userRole();
+        if (Default_Model_DbTable_MemberRole::ROLE_NAME_ADMIN == $userRoleName) {
+            $tableMember = new Default_Model_Member();
+            $this->view->view_member = $tableMember->fetchMemberData($this->_memberId);
+            $this->view->member = $this->view->view_member;
+        } else {
+            $this->view->member = $this->_authMember;
+        }
+
+        $model = new Default_Model_DbTable_Support();
+        $this->view->supporterlist = $model->getSupporterDonationList($this->view->member->member_id);
+
+
+    }
 
     public function payoutAction()
     {
