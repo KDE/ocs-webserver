9c270db186cf19b7667c3cf01ca00742ab332110
siyuan dong <dongsiyuan@gmail.com>
2019-08-23 10:39:47 +0200

    ip dl

--- application/modules/default/controllers/DlController.php
+++ application/modules/default/controllers/DlController.php
@@ -122,54 +122,15 @@ class DlController extends Local_Controller_Action_DomainSwitch
                 
                 // $data = array('project_id' => $projectId, 'member_id' => $memberId,'anonymous_cookie'=>$storedInCookie, 'file_id' => $file_id, 'file_type' => $file_type, 'file_name' => $file_name, 'file_size' => $file_size,'downloaded_ip' => $this->getRealIpAddr());               
 
-                $indicesServer = array('PHP_SELF',
-                    'argv',
-                    'argc',
-                    'GATEWAY_INTERFACE',
-                    'SERVER_ADDR',
-                    'SERVER_NAME',
-                    'SERVER_SOFTWARE',
-                    'SERVER_PROTOCOL',
-                    'REQUEST_METHOD',
-                    'REQUEST_TIME',
-                    'REQUEST_TIME_FLOAT',
-                    'QUERY_STRING',
-                    'DOCUMENT_ROOT',
-                    'HTTP_ACCEPT',
-                    'HTTP_ACCEPT_CHARSET',
-                    'HTTP_ACCEPT_ENCODING',
-                    'HTTP_ACCEPT_LANGUAGE',
-                    'HTTP_CONNECTION',
-                    'HTTP_HOST',
-                    'HTTP_REFERER',
-                    'HTTP_USER_AGENT',
-                    'HTTPS',
-                    'REMOTE_ADDR',
-                    'REMOTE_HOST',
-                    'REMOTE_PORT',
-                    'REMOTE_USER',
-                    'REDIRECT_REMOTE_USER',
-                    'SCRIPT_FILENAME',
-                    'SERVER_ADMIN',
-                    'SERVER_PORT',
-                    'SERVER_SIGNATURE',
-                    'PATH_TRANSLATED',
-                    'SCRIPT_NAME',
-                    'REQUEST_URI',
-                    'PHP_AUTH_DIGEST',
-                    'PHP_AUTH_USER',
-                    'PHP_AUTH_PW',
-                    'AUTH_TYPE',
-                    'PATH_INFO',
-                    'ORIG_PATH_INFO') ;  
 
-                $server_info = '';
-                foreach ($indicesServer as $arg) {
-                    if (isset($_SERVER[$arg])) {
+                $server_info = '';               
+                foreach ($_SERVER as $arg) {
+                    if ($_SERVER[$arg]) {
                         $server_info = $server_info.$arg.': '.$_SERVER[$arg].' ';                        
                     }                    
                 } 
-        
+                
+
                 $data = array('project_id' => $projectId, 'member_id' => $memberId,'anonymous_cookie'=>$storedInCookie, 'file_id' => $file_id, 'file_type' => $file_type, 'file_name' => $file_name, 'file_size' => $file_size,'downloaded_ip' => $this->getRealIpAddr()
                              ,'HTTP_X_FORWARDED_FOR' => $_SERVER['HTTP_X_FORWARDED_FOR']
                              ,'HTTP_X_FORWARDED' =>     $_SERVER['HTTP_X_FORWARDED']
