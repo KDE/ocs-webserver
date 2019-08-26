9c5829f6d5a5b74e5fd92d88770d7e98f7f3ae93
siyuan dong <dongsiyuan@gmail.com>
2019-08-26 14:40:37 +0200

    trigger sql change

--- sql_code/20190826_major_update_at_trigger.sql
+++ sql_code/20190826_major_update_at_trigger.sql
@@ -1,9 +1,12 @@
-CREATE DEFINER=`root`@`%` TRIGGER `trg_project_major_updated_at`
+DELIMITER $$
+DROP TRIGGER IF EXISTS `trg_project_major_updated_at`$$
+CREATE DEFINER ='root' TRIGGER `trg_project_major_updated_at`
     BEFORE UPDATE
     ON `project`
     FOR EACH ROW
 BEGIN
-    IF `NEW`.`changed_at` <> `OLD`.`changed_at` AND DATEDIFF(`NEW`.`changed_at`, `OLD`.`major_updated_at`) >= 6 THEN
+    IF `NEW`.`changed_at` <> `OLD`.`changed_at` AND DATEDIFF(`NEW`.`changed_at`, `OLD`.`major_updated_at`) > 6 THEN
         SET `NEW`.`major_updated_at` = `NEW`.`changed_at`;
     END IF;
-END
+END$$
+DELIMITER ;
