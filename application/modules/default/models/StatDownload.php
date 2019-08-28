9f5a43dac24ba11be27e97e8738bb26a026aee84
Ronald von Schlegell <rvs75@email.de>
2019-08-28 15:29:13 +0200

    payout2

--- application/modules/default/models/StatDownload.php
+++ application/modules/default/models/StatDownload.php
@@ -69,6 +69,12 @@ class Default_Model_StatDownload
     
     public function getUserDownloadsForMonth($member_id, $yearmonth)
     {
+        return $this->getUserDownloadsForMonthAndSection($member_id, $yearmonth, null);
+    }
+    
+    
+    public function getUserDownloadsForMonthAndSection($member_id, $yearmonth, $section_id = null)
+    {
         $sql = "
                 SELECT 
                     `member_dl_plings`.*,
@@ -100,9 +106,64 @@ class Default_Model_StatDownload
                 LEFT JOIN section s ON s.section_id = sc.section_id
                 WHERE
                     `member_dl_plings`.`member_id` = :member_id
-                    AND `member_dl_plings`.`yearmonth` = :yearmonth
-                    
-                ORDER BY `member_dl_plings`.`yearmonth` DESC, `project_category`.`title`, `project`.`title`
+                    AND `member_dl_plings`.`yearmonth` = :yearmonth ";
+        
+        if(null != $section_id) {
+            $sql .=  " AND `member_dl_plings`.`section_id` = ".$section_id;
+        }
+        
+        $sql .=  " ORDER BY `member_dl_plings`.`yearmonth` DESC, `project_category`.`title`, `project`.`title`
+            ";
+        $result = Zend_Db_Table::getDefaultAdapter()->query($sql, array('member_id' => $member_id, 'yearmonth' => $yearmonth));
+
+        if ($result->rowCount() > 0) {
+            return $result->fetchAll();
+        } else {
+            return array();
+
+        }
+    }
+    
+    public function getUserSectionsForMonth($member_id, $yearmonth)
+    {
+        $sql = "
+                SELECT yearmonth, section_id, section_name, section_payout_factor, COUNT(project_id) AS count_projects, SUM(num_downloads) AS num_downloads, SUM(probably_payout_amount) AS sum_probably_payout_amount, SUM(real_payout_amount) AS sum_real_payout_amount, MAX(amount) AS payout_amount, MAX(STATUS) AS payout_status, MAX(payment_transaction_id) AS payout_payment_transaction_id, MAX(paypal_mail) AS paypal_mail
+                FROM (
+                    SELECT 
+                        `member_dl_plings`.*,
+                        CASE WHEN (SELECT count(1) AS `sum_plings` FROM `project_plings` `pp` WHERE `pp`.`project_id` = `member_dl_plings`.`project_id` AND `pp`.`is_deleted` = 0 AND `is_active` = 1 GROUP BY `pp`.`project_id`) > 0 THEN (SELECT count(1) AS `sum_plings` FROM `project_plings` `pp` WHERE `pp`.`project_id` = `member_dl_plings`.`project_id` AND `pp`.`is_deleted` = 0 AND `is_active` = 1 GROUP BY `pp`.`project_id`) + 1 ELSE 1 END AS `num_plings_now`,
+                        `project`.`title`,
+                        `project`.`image_small`,
+                        `project_category`.`title` AS `cat_title`,
+                        laplace_score(`project`.`count_likes`, `project`.`count_dislikes`)/100 AS `laplace_score`,
+                        `member_payout`.`amount`,
+                        `member_payout`.`status`,
+                        `member_payout`.`payment_transaction_id`,
+                        CASE WHEN `tag_object`.`tag_item_id` IS NULL THEN 1 ELSE 0 END AS `is_license_missing_now`,
+                        CASE WHEN ((`project_category`.`source_required` = 1 AND `project`.`source_url` IS NOT NULL AND LENGTH(`project`.`source_url`) > 0) OR  (`project_category`.`source_required` = 0)) THEN 0 ELSE 1 END AS `is_source_missing_now`,
+                        `project`.`pling_excluded` AS `is_pling_excluded_now`,
+                        (SELECT u.num_downloads FROM member_dl_plings_nouk u WHERE u.member_id = `member_dl_plings`.`member_id` and u.project_id = `member_dl_plings`.`project_id` AND u.yearmonth = `member_dl_plings`.yearmonth) AS num_downloads_nouk,
+                        (SELECT u.probably_payout_amount FROM member_dl_plings_nouk u WHERE u.member_id = `member_dl_plings`.`member_id` and u.project_id = `member_dl_plings`.`project_id` AND u.yearmonth = `member_dl_plings`.yearmonth) AS probably_payout_amount_nouk
+                        ,s.name AS section_name
+                        , case when is_license_missing = 1 OR is_source_missing = 1 OR is_pling_excluded = 1 then 0 ELSE probably_payout_amount END AS real_payout_amount
+
+                    FROM
+                        `member_dl_plings`
+                    STRAIGHT_JOIN
+                        `project` ON `project`.`project_id` = `member_dl_plings`.`project_id`
+                    STRAIGHT_JOIN 
+                        `project_category` ON `project_category`.`project_category_id` = `member_dl_plings`.`project_category_id`
+                    LEFT JOIN
+                        `member_payout` ON `member_payout`.`member_id` = `member_dl_plings`.`member_id`
+                            AND `member_payout`.`yearmonth` = `member_dl_plings`.`yearmonth`
+                    LEFT JOIN `tag_object` ON `tag_object`.`tag_type_id` = 1 AND `tag_object`.`tag_group_id` = 7 AND `tag_object`.`is_deleted` = 0 AND `tag_object`.`tag_object_id` = `project`.`project_id`
+                    LEFT JOIN section_category sc ON sc.project_category_id = `member_dl_plings`.`project_category_id`
+                    LEFT JOIN section s ON s.section_id = sc.section_id
+                    WHERE
+                        `member_dl_plings`.`member_id` = :member_id 
+                        AND `member_dl_plings`.`yearmonth` = :yearmonth
+                ) A
+                GROUP BY yearmonth, section_id, section_name, section_payout_factor                 
             ";
         $result = Zend_Db_Table::getDefaultAdapter()->query($sql, array('member_id' => $member_id, 'yearmonth' => $yearmonth));
 
