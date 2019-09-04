c66e06290fc8302c562e9b5b6a36251e61196743
David Nelband <dnelband@gmail.com>
2019-09-04 12:15:19 +0200

    pms addFileToGallery

--- httpdocs/theme/react/product-media-slider/app/product-media-slider-helpers.js
+++ httpdocs/theme/react/product-media-slider/app/product-media-slider-helpers.js
@@ -23,6 +23,8 @@ export function GenerateGalleryArray(product){
 
                 if (f.type.indexOf("image") > -1 && noGallery === true && noLogo === true) addFileToGallery = true;
 
+                console.log(addFileToGallery);
+
                 if (addFileToGallery === true){
                     let type;
                     if (f.type.indexOf('video') > -1 || f.type.indexOf('audio') > -1 ) type = f.type.split('/')[0]
