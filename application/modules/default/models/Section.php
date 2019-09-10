87e27179325331fa4e17cc598b587712e1830bb6
Ronald von Schlegell <rvs75@email.de>
2019-09-10 12:32:32 +0200

    section

--- application/modules/default/models/Section.php
+++ application/modules/default/models/Section.php
@@ -362,52 +362,14 @@ class Default_Model_Section
      */
     public function fetchSectionStats($yearmonth = null, $section_id, $isForAdmin = false)
     {
-        $sql = "SELECT p.yearmonth, s.section_id, s.name AS section_name
-                ,(SELECT ROUND(SUM(ss.tier),2) AS sum_support FROM section_support_paypements ss
-                    JOIN support su2 ON su2.id = ss.support_id
-                    WHERE s.section_id = ss.section_id
-                    AND ss.yearmonth = :yearmonth 
-                    GROUP BY ss.section_id
-                ) AS sum_support
-                ,(SELECT SUM(sp.amount * (ssp.percent_of_sponsoring/100)) AS sum_sponsor FROM sponsor sp
-                LEFT JOIN section_sponsor ssp ON ssp.sponsor_id = sp.sponsor_id
-                WHERE sp.is_active = 1
-                AND ssp.section_id = s.section_id) AS sum_sponsor
-                , SUM(p.num_downloads) AS sum_dls
-                , ROUND(SUM(p.probably_payout_amount),2) AS sum_amount
-                , p3.num_downloads AS sum_dls_payout, p3.amount AS sum_amount_payout
-                ,(SELECT COUNT(1) AS num_supporter FROM (
-						 	SELECT COUNT(1) AS num_supporter,ss.section_id, su2.member_id FROM section_support_paypements ss
-	                    JOIN support su2 ON su2.id = ss.support_id
-	                    WHERE ss.yearmonth = :yearmonth 
-	                    GROUP BY ss.section_id, su2.member_id
-                    ) A
-                    WHERE A.section_id = s.section_id
-                ) AS num_supporter
-                FROM member_dl_plings p
-                LEFT JOIN section_category sc ON sc.project_category_id = p.project_category_id
-                LEFT JOIN section s ON s.section_id = sc.section_id
-                LEFT JOIN (
-                        SELECT yearmonth, section_id, SUM(num_downloads) AS num_downloads, SUM(amount) AS amount FROM (
-                                SELECT m.yearmonth, `m`.`member_id`,`m`.`paypal_mail`, s.section_id, sum(`m`.`num_downloads`) AS `num_downloads`,round(sum(`m`.`probably_payout_amount`),2) AS `amount` 
-                                from `member_dl_plings` `m` 
-                                LEFT JOIN section_category sc ON sc.project_category_id = m.project_category_id
-                                LEFT JOIN section s ON s.section_id = sc.section_id
-                                where ((`m`.`yearmonth` = :yearmonth) 
-                                and (length(`m`.`paypal_mail`) > 0) and (`m`.`paypal_mail` regexp '^[A-Z0-9._%-]+@[A-Z0-9.-]+.[A-Z]{2,4}$') and (`m`.`is_license_missing` = 0) and (`m`.`is_source_missing` = 0) and (`m`.`is_pling_excluded` = 0) and (`m`.`is_member_pling_excluded` = 0)) 
-                                group by m.yearmonth, `m`.`member_id`,`m`.`paypal_mail`, s.section_id
-                                HAVING sum(`m`.`probably_payout_amount`) >= 1
-                        ) A GROUP BY yearmonth, section_id
-                ) p3 ON p3.yearmonth = p.yearmonth AND p3.section_id = s.section_id
+        $sql = "SELECT * FROM section_funding_stats p
                 WHERE p.yearmonth = :yearmonth
-                AND sc.section_id IS NOT null
-                AND s.section_id = :section_id ";
+                AND p.section_id = :section_id";
         
         if(!$isForAdmin) {
             $sql .= " AND p.yearmonth >= DATE_FORMAT((NOW() - INTERVAL 1 MONTH),'%Y%m')";
         }
         
-        $sql .= " GROUP BY p.yearmonth, s.section_id, s.name";
         if(empty($yearmonth)) {
             $yearmonth = "DATE_FORMAT(NOW(),'%Y%m')";
         }
