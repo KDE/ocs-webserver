29d0453efcffdb284b63efb50972f21a767e8e2d
siyuan dong <dongsiyuan@gmail.com>
2019-09-12 14:42:32 +0200

    section

--- application/modules/default/controllers/SectionController.php
+++ application/modules/default/controllers/SectionController.php
@@ -30,7 +30,7 @@ class SectionController extends Local_Controller_Action_DomainSwitch
         if(Zend_Auth::getInstance()->hasIdentity() AND Zend_Auth::getInstance()->getIdentity()->roleName == 'admin') {
             $isAdmin = true;
         }
-        
+
         $section_id = $this->getParam('id',null);
 
         $products = self::fetchProducts($section_id, $isAdmin);
@@ -38,16 +38,17 @@ class SectionController extends Local_Controller_Action_DomainSwitch
         $supporters = self::fetchSupporters($section_id);
 
         $model = new Default_Model_Section();
-        $section = null;        
+        $section = null;
         if($section_id)
         {
             $section = $model->fetchSection($section_id);
             $this->view->section = $section;
-            $this->view->section_id = $section_id;            
+            $this->view->section_id = $section_id;
         }
-        
-        $amount = $model->fetchProbablyPayoutLastMonth($section_id);        
+
+        $amount = $model->fetchProbablyPayoutLastMonth($section_id);
         $amount_factor = $amount;
+        $sectionStats = $model->fetchSectionStatsLastMonth($section_id);
         if($sectionStats['factor'] != null) {
             $amount_factor = $amount * $sectionStats['factor'];
         }
@@ -59,10 +60,10 @@ class SectionController extends Local_Controller_Action_DomainSwitch
             $this->view->probably_payout_amount = number_format($amount, 2, '.', '');
         }else{
             $this->view->probably_payout_amount = -1;
-        } 
+        }
         $this->view->probably_payout_amount_factor = number_format($amount_factor, 2, '.', '');
         $goal = ceil( $amount_factor / 500 ) * 500;
-        $this->view->probably_payout_goal = ($goal ==0 ? 500: $goal);        
+        $this->view->probably_payout_goal = ($goal ==0 ? 500: $goal);
 
         $title = 'Section';
         if($section){
@@ -73,7 +74,7 @@ class SectionController extends Local_Controller_Action_DomainSwitch
 
 
     public function topAction()
-    {        
+    {
         $this->_helper->layout->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
         $isAdmin = false;
@@ -83,7 +84,7 @@ class SectionController extends Local_Controller_Action_DomainSwitch
         $section_id = $this->getParam('id',null);
         $products = self::fetchProducts($section_id, $isAdmin);
         $creators = self::fetchCreators($section_id, $isAdmin);
-        $this->_helper->json(array('status' => 'ok', 'products' => $products,'creators' => $creators));        
+        $this->_helper->json(array('status' => 'ok', 'products' => $products,'creators' => $creators));
     }
 
     public function fetchCreators($section_id,$isAdmin)
@@ -94,10 +95,10 @@ class SectionController extends Local_Controller_Action_DomainSwitch
         $sectionStats = $model->fetchSectionStatsLastMonth($section_id);
 
         $creators = $model->fetchTopCreatorPerSection($section_id);
-        
+
         foreach ($creators as &$p) {
           $p['profile_image_url'] = $helperImage->Image($p['profile_image_url'], array('width' => 100, 'height' => 100));
-          
+
           $p['probably_payout_amount_factor'] = number_format($p['probably_payout_amount']*($sectionStats['factor']?$sectionStats['factor']:1), 2, '.', '');
 
           $p['section_factor'] = $sectionStats['factor'];
@@ -123,7 +124,7 @@ class SectionController extends Local_Controller_Action_DomainSwitch
         foreach ($products as &$p) {
           $p['image_small'] = $helperImage->Image($p['image_small'], array('width' => 200, 'height' => 200));
           $p['updated_at'] = $helpPrintDate->printDate(($p['changed_at']==null?$p['created_at']:$p['changed_at']));
-          
+
           $p['probably_payout_amount_factor'] = number_format($p['probably_payout_amount']*($sectionStats['factor']?$sectionStats['factor']:1), 2, '.', '');
 
           $p['section_factor'] = $sectionStats['factor'];
@@ -132,7 +133,7 @@ class SectionController extends Local_Controller_Action_DomainSwitch
             $p['probably_payout_amount'] =  number_format($p['probably_payout_amount'], 2, '.', '') ;
           }else{
             $p['probably_payout_amount'] = -1;
-          }           
+          }
         }
 
         return $products;
@@ -142,31 +143,31 @@ class SectionController extends Local_Controller_Action_DomainSwitch
     {
 
         $info = new Default_Model_Info();
-        $helperImage = new Default_View_Helper_Image();    
+        $helperImage = new Default_View_Helper_Image();
         if($section_id)
-        {                    
+        {
             $supporters = $info->getNewActiveSupportersForSection($section_id,1000);
         }else{
             $supporters = $info->getNewActiveSupportersForSectionAll(1000);
         }
 
         foreach ($supporters as &$p) {
-          $p['profile_image_url'] = $helperImage->Image($p['profile_image_url'], array('width' => 100, 'height' => 100));                  
+          $p['profile_image_url'] = $helperImage->Image($p['profile_image_url'], array('width' => 100, 'height' => 100));
         }
 
         return $supporters;
-        
+
     }
 
     public function topcatAction()
-    {        
+    {
         $this->_helper->layout->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
         $model = new Default_Model_Section();
         $helpPrintDate = new Default_View_Helper_PrintDate();
         $helperImage = new Default_View_Helper_Image();
         $cat_id = $this->getParam('cat_id');
-        $products=$model->fetchTopProductsPerCategory($cat_id);        
+        $products=$model->fetchTopProductsPerCategory($cat_id);
         $section = $model->fetchSectionForCategory($cat_id);
         $sectionStats = $model->fetchSectionStatsLastMonth($section['section_id']);
         $isAdmin = false;
@@ -177,7 +178,7 @@ class SectionController extends Local_Controller_Action_DomainSwitch
         foreach ($products as &$p) {
           $p['image_small'] = $helperImage->Image($p['image_small'], array('width' => 200, 'height' => 200));
           $p['updated_at'] = $helpPrintDate->printDate(($p['changed_at']==null?$p['created_at']:$p['changed_at']));
-           
+
             $p['probably_payout_amount_factor'] = number_format($p['probably_payout_amount']*($sectionStats['factor']?$sectionStats['factor']:1), 2, '.', '');
 
           $p['section_factor'] = $sectionStats['factor'];
@@ -186,7 +187,7 @@ class SectionController extends Local_Controller_Action_DomainSwitch
             $p['probably_payout_amount'] =  number_format($p['probably_payout_amount'], 2, '.', '') ;
           }else{
             $p['probably_payout_amount'] = -1;
-          }           
+          }
         }
 
         $creators = $model->fetchTopCreatorPerCategory($cat_id);
@@ -201,9 +202,9 @@ class SectionController extends Local_Controller_Action_DomainSwitch
           }else{
             $p['probably_payout_amount'] = -1;
           }
-          
+
         }
-        $this->_helper->json(array('status' => 'ok', 'cat_id'=>$cat_id,'products' => $products,'creators' => $creators));        
+        $this->_helper->json(array('status' => 'ok', 'cat_id'=>$cat_id,'products' => $products,'creators' => $creators));
     }
 
 
@@ -211,5 +212,5 @@ class SectionController extends Local_Controller_Action_DomainSwitch
     {
         $this->_helper->layout()->setLayout('layout_pling_home');
     }
-    
+
 }
