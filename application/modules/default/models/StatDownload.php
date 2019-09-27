0b29dad080ed3331c9d79fbed096848c3b3f317d
Ronald von Schlegell <rvs75@email.de>
2019-09-27 10:59:05 +0200

    history

--- application/modules/default/models/StatDownload.php
+++ application/modules/default/models/StatDownload.php
@@ -725,23 +725,44 @@ class Default_Model_StatDownload
         }
         
         
-        $sql = "SELECT yearmonth, case when yearmonth = DATE_FORMAT(NOW(),'%Y%m') then round(num_plings*now_section_payout_factor/100,2) ELSE amount END AS amount
-                ,(select count(1) from member_payout p where p.yearmonth=A.yearmonth and p.member_id = A.member_id) cnt
+        $sql = "SELECT A2.yearmonth,TRUNCATE(SUM(sum_payout),2) AS amount, cnt
                 FROM (
-                select
-                                m.yearmonth, m.member_id, round(SUM(credits_section)/100,2) AS amount,SUM(credits_plings) AS num_plings, SUM(m.num_plings) AS num_downloads
-                                ,(SELECT round(sfs.sum_support/DATE_FORMAT(NOW() + INTERVAL 1 MONTH - INTERVAL DATE_FORMAT(NOW(),'%d') DAY,'%d')*DATE_FORMAT(NOW(),'%d') /sfs.sum_amount_payout,2) AS factor  FROM section_funding_stats sfs WHERE sfs.yearmonth = m.yearmonth AND sfs.section_id = m.section_id) AS now_section_payout_factor
-                        from micro_payout m
-                        WHERE #m.yearmonth =  date_format((now() - INTERVAL 0 month),'%Y%m')
-                        (length(`m`.`paypal_mail`) > 0) and (`m`.`paypal_mail` regexp '^[A-Z0-9._%-]+@[A-Z0-9.-]+.[A-Z]{2,4}$')
-                        and (`m`.`is_license_missing` = 0) and (`m`.`is_source_missing` = 0) and (`m`.`is_pling_excluded` = 0) and (`m`.`is_member_pling_excluded` = 0)
-                        AND m.member_id = :member_id
-                        AND m.yearmonth > 201704
-                        group by m.yearmonth, m.member_id
-
-
-                ) A	
-                ORDER BY yearmonth";
+                    SELECT yearmonth, section_id
+                        ,case when yearmonth = DATE_FORMAT(NOW(),'%Y%m') then sum(real_credits_plings*now_section_payout_factor)/100 ELSE sum(real_credits_plings*section_payout_factor)/100 END AS sum_payout
+                        ,(select count(1) from member_payout p where p.yearmonth=A.yearmonth and p.member_id = A.member_id) cnt
+                    FROM (
+                    
+                        SELECT 
+                            SUM(case when is_license_missing = 1 OR is_source_missing = 1 OR is_pling_excluded = 1 then 0 ELSE credits_plings END) AS real_credits_plings,
+                            `micro_payout`.yearmonth,
+                            `micro_payout`.section_id,
+                            `member_payout`.member_id,
+                            `micro_payout`.section_payout_factor,
+                            (SELECT round(sfs.sum_support/DATE_FORMAT(NOW() + INTERVAL 1 MONTH - INTERVAL DATE_FORMAT(NOW(),'%d') DAY,'%d')*DATE_FORMAT(NOW(),'%d') /sfs.sum_amount_payout,2) AS factor  FROM section_funding_stats sfs WHERE sfs.yearmonth = `micro_payout`.yearmonth AND sfs.section_id = `micro_payout`.section_id) AS now_section_payout_factor
+                            FROM
+                                `micro_payout`
+                            STRAIGHT_JOIN
+                                `project` ON `project`.`project_id` = `micro_payout`.`project_id`
+                            STRAIGHT_JOIN 
+                                `project_category` ON `project_category`.`project_category_id` = `project`.`project_category_id`
+                            LEFT JOIN
+                                `member_payout` ON `member_payout`.`member_id` = `project`.`member_id`
+                                    AND `member_payout`.`yearmonth` = `micro_payout`.`yearmonth`
+                            LEFT JOIN `tag_object` ON `tag_object`.`tag_type_id` = 1 AND `tag_object`.`tag_group_id` = 7 AND `tag_object`.`is_deleted` = 0 AND `tag_object`.`tag_object_id` = `project`.`project_id`
+                            LEFT JOIN section_category sc ON sc.project_category_id = `project`.`project_category_id`
+                            LEFT JOIN section s ON s.section_id = sc.section_id
+                            WHERE `micro_payout`.`member_id` = :member_id 
+                            AND `micro_payout`.yearmonth > 201704
+                            GROUP BY `micro_payout`.yearmonth, `micro_payout`.`project_id`
+                        ) A
+                        GROUP BY yearmonth,section_id
+                        
+                ) A2
+                
+                GROUP BY yearmonth
+                HAVING amount > 0
+                order by yearmonth
+					 	";
         
         $resultSet = Zend_Db_Table::getDefaultAdapter()->fetchAll($sql, array('member_id' => $member_id));
         
