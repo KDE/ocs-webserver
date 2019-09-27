217b3653a0c1d788ac596214a0861d31aae52b0d
siyuan dong <dongsiyuan@gmail.com>
2019-09-27 14:39:06 +0200

    header

--- httpdocs/theme/react/src/header/Support.js
+++ httpdocs/theme/react/src/header/Support.js
@@ -96,19 +96,10 @@ class Support extends Component {
       }
       const labeltext = 'Goal:'+s+'/'+goal;
       let  barStyle;
-      if(Math.round((s*100)/goal)>=30)
-      {
-          barStyle= {
-            width: (s/goal)*100 + "%",
-            color: "#f1f1f1"
-          };
-      }else{
-          barStyle= {
-            width: (s/goal)*100 + "%",
-            color: "#f1f1f1"
-          };
-      }
-
+      barStyle= {
+        width: (s/goal)*100 + "%",
+      };
+      //color: "#1E2881"
     return(
       <div className={"pling-section-header "+ this.props.headerStyle}>
           <div className="header-body">
