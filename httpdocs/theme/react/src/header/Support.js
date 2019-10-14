922e568ddf828fcfccc2a3c4d226cdf6955f9b06
siyuan dong <dongsiyuan@gmail.com>
2019-10-14 09:30:21 +0200

    header

--- httpdocs/theme/react/src/header/Support.js
+++ httpdocs/theme/react/src/header/Support.js
@@ -52,32 +52,17 @@ class Support extends Component {
                                       >
                                        <a className="cls-tab-month">Months</a>
                                     </li>
-                                    <li key={'amount'}
-                                      className={(this.state.selected=="amount")?'active':''}
-                                      onClick={()=>this.tabSwitch('amount')}
-                                      >
-                                       <a className="cls-tab-amount">Amount</a>
-                                    </li>
                                   </ul>
                                     <a href="/support" role="button" className="btn btn-primary active">Become a Supporter</a>
                                   </div>
 
-        let content;
-        if(this.state.selected=="amount")
-        {
-          content = this.props.section.supporters.sort((a, b) => Number(a.sum_support) < Number(b.sum_support)).map((mg,i) => (
-                  <div className="section"><div className="section-name"><a href={"/u/"+mg.username}><img src={mg.profile_image_url}></img></a>
-                  <span><a href={"/u/"+mg.username}>{mg.username}</a></span></div>
-                  <div className="section-value"> ${mg.sum_support}</div></div>
-                  ))
-        }else{
           // default show month panel
-          content = this.props.section.supporters.sort((a, b) => Number(a.active_months) < Number(b.active_months)).map((mg,i) => (
+        let  content = this.props.section.supporters.sort((a, b) => Number(a.active_months) < Number(b.active_months)).map((mg,i) => (
                   <div className="section"><div className="section-name"><a href={"/u/"+mg.username}><img src={mg.profile_image_url}></img></a>
                   <span><a href={"/u/"+mg.username}>{mg.username}</a></span></div>
                   <div className="section-value"> {mg.active_months+' months'}</div></div>
                   ))
-        }
+
 
         const t = (
                         <div className="user-pling-section-container">
@@ -88,31 +73,13 @@ class Support extends Component {
 
         supporters = <ul className="dropdown-menu dropdown-menu-right">{t}</ul>
       }
-
-      const s =this.props.section.supporters.length;
-      let goal = Math.ceil((s/50))*50;
-      if(goal==0)
-      {
-        goal = 50;
-      }
-      const labeltext = ' Goal: '+s+' / '+goal;
-      let  barStyle;
-      barStyle= {
-        width: (s/goal)*100 + "%",
-      };
+      const s = this.props.section.supporters?this.props.section.supporters.length:0;
     return(
       <div className={"pling-section-header "+this.props.headerStyle}>
           <div className="header-body">
-            <div className="score-container">
-              <span>{this.props.section ? this.props.section.name:''}</span>
-              <div className="score-bar-container">
-                <div className={"score-bar"} style={barStyle}>
-                  {labeltext}
-                </div>
-              </div>
-            </div>
+
             <div ref={node => this.node = node} className={'supporter-container '+this.state.dropdownClass }>
-              <a className="header-supporters" > Supporters &#8964; </a>
+              <a className="header-supporters" >{this.props.section ? this.props.section.name:''} {s} Supporters &#8964; </a>
               {supporters}
             </div>
           </div>
