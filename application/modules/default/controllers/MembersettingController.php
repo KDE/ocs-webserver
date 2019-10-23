dde15766472a781c573c78a8f82d1f30cdcac14b
siyuan dong <dongsiyuan@gmail.com>
2019-10-23 16:30:05 +0200

    metaheader update

--- application/modules/default/controllers/MembersettingController.php
+++ application/modules/default/controllers/MembersettingController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *  ocs-webserver
  *
@@ -19,13 +20,13 @@
  *    You should have received a copy of the GNU Affero General Public License
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **/
-
 class MembersettingController extends Zend_Controller_Action
 {
 
-	const GROUP_METAHEADER = 1;
-	protected $_format = 'json';
-	public function init()
+    const GROUP_METAHEADER = 1;
+    protected $_format = 'json';
+
+    public function init()
     {
         parent::init();
         $this->initView();
@@ -44,6 +45,31 @@ class MembersettingController extends Zend_Controller_Action
         $this->_sendErrorResponse(999, 'unknown request');
     }
 
+    public function getsettingsAction()
+    {
+        $this->_initResponseHeader();
+        $identity = Zend_Auth::getInstance()->getStorage()->read();
+        if ($identity == null || $identity->member_id == null) {
+            $response = array(
+                'status' => 'error',
+                'msg'    => 'no user found'
+            );
+            $this->_sendResponse($response, $this->_format);
+
+            return;
+        }
+        $model = new Default_Model_MemberSettingValue();
+        $member_id = $identity->member_id;
+
+        $results = $model->findMemberSettings($member_id, $this::GROUP_METAHEADER);
+        $response = array(
+            'status'    => 'ok',
+            'member_id' => $member_id,
+            'results'   => $results
+        );
+        $this->_sendResponse($response, $this->_format);
+    }
+
     protected function _initResponseHeader()
     {
         http_response_code(200);
@@ -57,9 +83,13 @@ class MembersettingController extends Zend_Controller_Action
 
         if (!empty($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
             header('Access-Control-Allow-Methods: ' . implode(', ', array_unique([
-                'OPTIONS', 'HEAD', 'GET', 'POST','PUT',
-                strtoupper($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])
-            ])), true);
+                    'OPTIONS',
+                    'HEAD',
+                    'GET',
+                    'POST',
+                    'PUT',
+                    strtoupper($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])
+                ])), true);
         }
 
         if (!empty($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
@@ -75,147 +105,103 @@ class MembersettingController extends Zend_Controller_Action
 
     }
 
-    protected function _sendResponse($response, $format = 'json', $xmlRootTag = 'ocs')
-    {
-
-    	header('Content-Type: application/json; charset=UTF-8');
-    	echo json_encode($response);
-    }
-
     /*public function getsettingsAction()
     {
         $this->_helper->layout->disableLayout();
     }*/
 
-    public function getsettingsAction()
+    protected function _sendResponse($response, $format = 'json', $xmlRootTag = 'ocs')
     {
-			$this->_initResponseHeader();
-    	$identity = Zend_Auth::getInstance()->getStorage()->read();
-    	if($identity==null || $identity->member_id==null)
-    	{
-    			$response = array(
-    		            'status'     => 'error',
-    		            'msg'	 => 'no user found'
-    		        );
-    			$this->_sendResponse($response, $this->_format);
-    			return;
-    	}
-    	$model = new Default_Model_MemberSettingValue();
-    	$member_id = $identity->member_id;
-
-    	$results = $model->findMemberSettings($member_id,$this::GROUP_METAHEADER);
-    	$response = array(
-                'status'     => 'ok',
-                'member_id'  => $member_id,
-                'results'    => $results
-            );
-    	$this->_sendResponse($response, $this->_format);
+        header('Content-Type: application/json; charset=UTF-8');
+        echo json_encode($response);
     }
-
 
     public function setsettingsAction()
     {
-			$this->_initResponseHeader();
-    	$identity = Zend_Auth::getInstance()->getStorage()->read();
-    	if($identity==null || $identity->member_id==null)
-    	{
-    			$response = array(
-    		            'status'     => 'error',
-    		            'msg'	 => 'no user found'
-    		        );
-    	}else
-    	{
-    		$model = new Default_Model_MemberSettingValue();
-    		$member_id = $identity->member_id;
-    		$member_setting_item_id = $this->getParam('itemid');
-    		$value = $this->getParam('itemvalue');
-    		$model->updateOrInsertSetting($member_id,$member_setting_item_id,null,$value);
-    		$response = array(
-                'status'     => 'ok'
+        $this->_initResponseHeader();
+        $identity = Zend_Auth::getInstance()->getStorage()->read();
+        if ($identity == null || $identity->member_id == null) {
+            $response = array(
+                'status' => 'error',
+                'msg'    => 'no user found'
             );
-    	}
-    	$this->_sendResponse($response, $this->_format);
+        } else {
+            $model = new Default_Model_MemberSettingValue();
+            $member_id = $identity->member_id;
+            $member_setting_item_id = $this->getParam('itemid');
+            $value = $this->getParam('itemvalue');
+            $model->updateOrInsertSetting($member_id, $member_setting_item_id, null, $value);
+            $response = array(
+                'status' => 'ok'
+            );
+        }
+        $this->_sendResponse($response, $this->_format);
     }
 
-		public function notificationAction()
-		{
-
-			$this->_initResponseHeader();
-        	$identity = Zend_Auth::getInstance()->getStorage()->read();
-        	if($identity==null || $identity->member_id==null)
-        	{
-        			$response = array(
-        		            'status'     => 'error',
-        		            'msg'	 => 'no user found'
-        		        );
-        			$this->_sendResponse($response, $this->_format);
-        			return;
-        	}
-            $modelForum = new Default_Model_Ocs_Forum();
-            $data = $modelForum->getUserNotifications($identity->member_id);
-            $results = $data;
-
-            /*
-    		$url_forum = Zend_Registry::get('config')->settings->client->default->url_forum;
-				$api_key = Zend_Registry::get('config')->settings->client->default->forum_api_key;
-				$url=$url_forum.'/notifications.json?api_key='.$api_key.'&api_username='.$username;
-				$ch = curl_init();
-            curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-            $data = curl_exec($ch);
-            curl_close($ch);
-    		$results = json_decode($data);  */
-
-        	$this->_sendResponse($results, $this->_format);
-
-		}
-
-        // public function anonymousdlAction()
-        // {
-				//
-        //     $this->_initResponseHeader();
-        //     $identity = Zend_Auth::getInstance()->getStorage()->read();
-        //     if($identity==null)
-        //     {
-        //         $config = Zend_Registry::get('config');
-        //         $cookieName = $config->settings->session->auth->anonymous;
-        //         $storedInCookie = isset($_COOKIE[$cookieName]) ? $_COOKIE[$cookieName] : NULL;
-        //         if($storedInCookie)
-        //         {
-        //             $memberDlAnonymous = new Default_Model_DbTable_MemberDownloadAnonymous();
-        //             $dls = $memberDlAnonymous->countDownloads($storedInCookie);
-				//
-        //             $response = array(
-        //             'status'     => 'ok',
-        //             'dls'    => $dls
-        //             );
-        //             $this->_sendResponse($response, $this->_format);
-        //             return;
-        //         }
-				//
-        //     }
-				//
-        //     $response = array(
-        //         'status'     => 'ok',
-        //         'dls'    => 0
-        //         );
-        //     $this->_sendResponse($response, $this->_format);
-				//
-        // }
-
-		public function memberjsonAction()
+    public function notificationAction()
     {
-				$this->_initResponseHeader();
-  			$identity = Zend_Auth::getInstance()->getStorage()->read();
-	    	$member_id = $this->getParam('member_id');
+        $this->_initResponseHeader();
+        $identity = Zend_Auth::getInstance()->getStorage()->read();
+        if ($identity == null || $identity->member_id == null) {
+            $response = array(
+                'status' => 'error',
+                'msg'    => 'no user found'
+            );
+            $this->_sendResponse($response, $this->_format);
+
+            return;
+        }
+        $modelForum = new Default_Model_Ocs_Forum();
+        $data = $modelForum->getUserNotifications($identity->member_id);
+        $results = $data;
+
+        $this->_sendResponse($results, $this->_format);
+
+    }
+
+    // public function anonymousdlAction()
+    // {
+    //
+    //     $this->_initResponseHeader();
+    //     $identity = Zend_Auth::getInstance()->getStorage()->read();
+    //     if($identity==null)
+    //     {
+    //         $config = Zend_Registry::get('config');
+    //         $cookieName = $config->settings->session->auth->anonymous;
+    //         $storedInCookie = isset($_COOKIE[$cookieName]) ? $_COOKIE[$cookieName] : NULL;
+    //         if($storedInCookie)
+    //         {
+    //             $memberDlAnonymous = new Default_Model_DbTable_MemberDownloadAnonymous();
+    //             $dls = $memberDlAnonymous->countDownloads($storedInCookie);
+    //
+    //             $response = array(
+    //             'status'     => 'ok',
+    //             'dls'    => $dls
+    //             );
+    //             $this->_sendResponse($response, $this->_format);
+    //             return;
+    //         }
+    //
+    //     }
+    //
+    //     $response = array(
+    //         'status'     => 'ok',
+    //         'dls'    => 0
+    //         );
+    //     $this->_sendResponse($response, $this->_format);
+    //
+    // }
+
+    public function memberjsonAction()
+    {
+        $this->_initResponseHeader();
+        $identity = Zend_Auth::getInstance()->getStorage()->read();
+        $member_id = $this->getParam('member_id');
         $results = null;
-        if($member_id){
+        if ($member_id) {
             $info = new Default_Model_Info();
             $commentsOpendeskop = $info->getDiscussionOpendeskop($member_id);
-            $results=array('commentsOpendeskop' => $commentsOpendeskop);
+            $results = array('commentsOpendeskop' => $commentsOpendeskop);
         }
         $this->_sendResponse($results, $this->_format);
     }
@@ -239,14 +225,14 @@ class MembersettingController extends Zend_Controller_Action
         $this->_sendResponse($results, $this->_format);
     }
 
+   
     public function userinfoAction()
-		{
-			$this->_initResponseHeader();
-			$member_id = $this->getParam('member_id');
-			$info = new Default_Model_Info();
-			$data = $info->getTooptipForMember($member_id);
-			$this->_sendResponse($data, $this->_format);
-		}
-
+    {
+        $this->_initResponseHeader();
+        $member_id = $this->getParam('member_id');
+        $info = new Default_Model_Info();
+        $data = $info->getTooptipForMember($member_id);
+        $this->_sendResponse($data, $this->_format);
+    }
 
 }
