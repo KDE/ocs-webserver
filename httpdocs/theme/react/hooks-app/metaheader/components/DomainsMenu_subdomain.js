b8a905a6cb5671cd996d3fb2ef9c1d6a3c511856
siyuan dong <dongsiyuan@gmail.com>
2019-10-28 16:27:43 +0100

    metaheader

---
+++ httpdocs/theme/react/hooks-app/metaheader/components/DomainsMenu_subdomain.js
@@ -0,0 +1,44 @@
+import React , {useEffect, useState,useRef}from 'react'
+
+const DomainsMenu_subdomain = (props) => {
+
+    const [dropdownClass, setDropdownClass] = useState('');
+    const [state, setState] = useState('test');
+    const toggleEl = useRef(null);
+
+    useEffect(() => {
+        document.addEventListener('mousedown',handleClick, false);
+        return () => {
+            document.removeEventListener('mousedown',handleClick, false);
+        };
+    }, [])
+
+    const handleClick= e => {                       
+        let cls = "";
+        if (toggleEl.current.contains(e.target)){                 
+          if (dropdownClass === "open"){              
+            if (e.target.className === "domains-menu-link-item"){
+                cls = "";
+            } else {
+                cls = "open";
+            }
+          } else {
+            cls = "open";
+          }
+        }
+        setDropdownClass(cls);
+      }
+    return (
+       <div ref={toggleEl} id="domains-dropdown-menu" className={dropdownClass}>
+        <a className="domains-menu-link-item">{props.storeConfig.name} &#8964;</a>
+        <ul className="dropdown-menu dropdown-menu-right">
+         {props.domains.filter(domain=>domain.is_show_in_menu==1)
+                       .sort((a, b) => a.name > b.name)
+                       .map((domain, index)=><li key={index}>
+                           <a href={domain.is_show_real_domain_as_url==1?domain.host:props.baseUrlStore+'/s/'+domain.name}>{domain.name}</a></li>)}        
+        </ul>
+      </div>
+    )
+}
+
+export default DomainsMenu_subdomain
