29c991133b95de44dafbdd188adc274b3d5a3648
siyuan dong <dongsiyuan@gmail.com>
2019-10-28 13:37:14 +0100

    metaheader

--- httpdocs/theme/react/hooks-app/metaheader/components/DomainsMenu.js
+++ httpdocs/theme/react/hooks-app/metaheader/components/DomainsMenu.js
@@ -50,7 +50,7 @@ class DomainsMenu extends React.Component {
                   <img src={this.props.baseUrl + "/images/system/ocs-logo-rounded-16x16.png"} className="logo" />
                   openDesktop.org :
                 </a>   
-                                                            
+                                                  
               </li>
               
               <li><a href={this.props.baseUrlStore}>Pling</a></li>
