cf7ba11bc1c898b3ec45bbfcb315fbaae81267e3
Ronald von Schlegell <rvs75@email.de>
2019-10-29 11:08:40 +0100

    add/edit

--- application/modules/default/controllers/ProductController.php
+++ application/modules/default/controllers/ProductController.php
@@ -978,6 +978,8 @@ class ProductController extends Local_Controller_Action_DomainSwitch
         
         $modelTags->processTagProductOriginal($this->_projectId,$values['is_original']);
         
+        var_dump(values['is_original_or_modification[]']);
+        
         $modelTags->processTagProductOriginalOrModification($this->_projectId,$values['is_original_or_modification[]']);
 
         if($values['tagsuser']) {
