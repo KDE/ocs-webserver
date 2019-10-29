8c58cca464c4a8666e11ed37383d113327104209
Ronald von Schlegell <rvs75@email.de>
2019-10-29 10:57:42 +0100

    add/edit

--- application/modules/default/forms/Product.php
+++ application/modules/default/forms/Product.php
@@ -451,7 +451,8 @@ class Default_Form_Product extends Zend_Form
     private function getIsOriginalOrModification()
     {
         $element = new Zend_Form_Element_Select('is_original_or_modification', array('multiple' => false));
-        $element->setIsArray(true);
+        $element->setIsArray(true)->setName("is_original_or_modification");
+        
         
         $option = array();
         $option[0] = "";
