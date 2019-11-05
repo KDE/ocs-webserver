22ea0cffb3b669015146cf53fdd77ecb730c9026
Ronald von Schlegell <rvs75@email.de>
2019-11-05 10:33:39 +0100

    org

--- application/modules/default/models/StatDownload.php
+++ application/modules/default/models/StatDownload.php
@@ -137,7 +137,7 @@ class Default_Model_StatDownload
 							
                     (SELECT round(sfs.sum_support/DATE_FORMAT(NOW() + INTERVAL 1 MONTH - INTERVAL DATE_FORMAT(NOW(),'%d') DAY,'%d')*DATE_FORMAT(NOW(),'%d') /sfs.sum_amount_payout,2) AS factor  FROM section_funding_stats sfs WHERE sfs.yearmonth = `micro_payout`.yearmonth AND sfs.section_id = `micro_payout`.section_id) AS now_section_payout_factor,
 							
-                    `micro_payout`.section_id, `micro_payout`.section_payout_factor
+                    `micro_payout`.section_id, `micro_payout`.section_payout_factor, `micro_payout`.org_factor
                     
                 FROM
                     `micro_payout`
@@ -236,7 +236,7 @@ class Default_Model_StatDownload
 							
                     (SELECT round(sfs.sum_support/DATE_FORMAT(NOW() + INTERVAL 1 MONTH - INTERVAL DATE_FORMAT(NOW(),'%d') DAY,'%d')*DATE_FORMAT(NOW(),'%d') /sfs.sum_amount_payout,2) AS factor  FROM section_funding_stats sfs WHERE sfs.yearmonth = `micro_payout`.yearmonth AND sfs.section_id = `micro_payout`.section_id) AS now_section_payout_factor,
 							
-                    `micro_payout`.section_id, `micro_payout`.section_payout_factor
+                    `micro_payout`.section_id, `micro_payout`.section_payout_factor, `micro_payout`.org_factor
                     
                 FROM
                     `micro_payout`
