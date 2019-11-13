da628a82cc9969c9523764714d63fdd164229aea
siyuan dong <dongsiyuan@gmail.com>
2019-11-13 16:27:28 +0100

    react cleanup

--- httpdocs/theme/react/src/entry-home-main-container.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import HomeMainContainer from './opendesktop-home/HomeMainContainer';
-
-ReactDOM.render(<HomeMainContainer />, document.getElementById('main-content'));
--- httpdocs/theme/react/src/entry-metaheader-local.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import MetaHeader from './metaheader/MetaHeader';
-
-ReactDOM.render(<MetaHeader config={window.config} hostname={window.location.hostname}/>, document.getElementById('metaheader'));
