1da8e9a9cc7b49f13f19751b77826adef29b74a8
siyuan dong <dongsiyuan@gmail.com>
2019-11-14 10:30:35 +0100

    supporters

--- httpdocs/theme/react/hooks-app/app-supporters/components/RecentPlingedProduct.js
+++ httpdocs/theme/react/hooks-app/app-supporters/components/RecentPlingedProduct.js
@@ -26,20 +26,16 @@ const RecentPlingedProduct = (props) => {
       <div style={stylePlaceholder} onClick={handleOnClick}>
 
       </div>
-      <a href={memberUrl}  className="tooltipuser" data-tooltip-content="#tooltip_content" data-user={props.product.member_id}>
-        <figure>
-          <img src={props.product.profile_image_url}></img>
-        </figure>
-      </a>
-      <h3>by {props.product.username}</h3>
-      <h3 style={{ color: '#ccc' }}>{props.product.catTitle}</h3>
+      <h3 >{props.product.title}</h3>
+      <h3 style={{ color: '#ccc',fontSize:'small' }}>{props.product.catTitle}</h3>
 
       <span className="small">
         <a className="tooltipuserplings" data-tooltip-content="#tooltip_content" data-user={props.product.project_id} >
           <img style={{ width: '15px', height: '15px', float: 'left' }} src={props.baseUrlStore + '/images/system/pling-btn-active.png'}></img>
         </a>
         {props.product.sum_plings}
-        {props.product.sum_plings_all ? '[' + props.product.sum_plings_all + ']' : ''}
+        <span style={{color:'ccc'}}>{props.product.sum_plings_all ? '[' + props.product.sum_plings_all + ']' : ''}
+        </span>
       </span>
     </div>
   )
