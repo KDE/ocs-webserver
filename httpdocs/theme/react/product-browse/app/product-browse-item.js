48509eb81acfe64cafcdac97e319fb70da6ff3eb
Ronald von Schlegell <rvs75@email.de>
2019-11-22 14:34:25 +0100

    browse

--- httpdocs/theme/react/product-browse/app/product-browse-item.js
+++ httpdocs/theme/react/product-browse/app/product-browse-item.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import ReactJkMusicPlayer from "react-jinke-music-player";
 import { isMobile   } from 'react-device-detect';
 import { getImageUrl } from './product-browse-helpers';
 
@@ -18,7 +17,7 @@ export function ProductBrowseItem(props){
     }
 
     React.useEffect(() => {
-        if (browseListType === "music" && productFilesFetched === false) onMusicProductLoad()
+        if (browseListType === "music"  && productFilesFetched === false ||browseListType ===  "music-test" && productFilesFetched === false) onMusicProductLoad()
     },[])
 
     function onMusicProductLoad(){
@@ -108,7 +107,7 @@ export function ProductBrowseItem(props){
             </div>
         )
     }
-    else if (browseListType === "music"){
+    else if (browseListType === "music" || browseListType === "music-test"){
         musicItemInfoDisplay = (
             <div className="product-browse-music-item-info">
                 <h2>{p.title}</h2>
@@ -180,7 +179,7 @@ export function ProductBrowseItem(props){
             <span className="index">{props.rowIndex + 1}</span>
         )
     }
-    console.log(is_show_real_domain_as_url);
+    
     let itemLink = json_serverUrl;
     itemLink = is_show_real_domain_as_url === 1 ? "/" : "/s/" + json_store_name + "/";
     itemLink += p.type_id === "3" ? "c" : "p";
@@ -203,293 +202,11 @@ export function ProductBrowseItem(props){
     )
 }
 
-function ProductBrowseItemPreviewMusicPlayer(props){
-
-    const [ productFiles, setProductFiles ] = useState(props.productFiles)
-
-    const [ showAudioControls, setShowAudioControls ] = useState(false);
-    const [ playIndex, setPlayIndex ] = useState();
-    let initialPLayedAudioArray = [];
-    if (productFiles){
-        productFiles.forEach(function(i,index){
-          let pl = 0;
-          if (index === 0) pl = -1;
-          const pa = {
-            ...i,
-            played:pl,
-            stopped:0
-          }
-          initialPLayedAudioArray.push(pa);
-        })
-    }
-    const [ playedAudioArray, setPlayedAudioArray ] = useState(initialPLayedAudioArray);
-
-
-    React.useEffect(() => {
-        $('.product-browse-item-preview-music-player').disableSelection();
-    },[])
-
-    function onReportAudioPlay(audioInfo){
-
-        console.log(audioInfo);
-        const audioItem = playedAudioArray.find((i => i.musicSrc === audioInfo.musicSrc));
-        const audioItemIndex = playedAudioArray.findIndex((i => i.musicSrc === audioInfo.musicSrc));
-        const newAudioItem = {
-          ...audioItem,
-          played:audioItem.played + 1
-        }
-        const newPLayedAudioArray = [
-          ...playedAudioArray.slice(0,audioItemIndex),
-          newAudioItem,
-          ...playedAudioArray.slice(audioItemIndex + 1, playedAudioArray.length)
-        ];
-        setPlayedAudioArray(newPLayedAudioArray);
-    
-        if (playedAudioArray[audioItemIndex].played === 0){
-          const audioStartUrl = window.location.href + "/p/" + props.projectId + "/" + 'startmediaviewajax?collection_id='+audioItem.collection_id+'&file_id='+audioItem.id+'&type_id=2';
-          $.ajax({url: audioStartUrl}).done(function(res) { 
-            console.log(res);
-            const newAudioItem = {
-              ...audioItem,
-              mediaViewId:res.MediaViewId,
-              played:audioItem.played + 1
-            }
-            const newPLayedAudioArray = [
-              ...playedAudioArray.slice(0,audioItemIndex),
-              newAudioItem,
-              ...playedAudioArray.slice(audioItemIndex + 1, playedAudioArray.length)
-            ];
-            setPlayedAudioArray(newPLayedAudioArray);
-          });
-        }    
-    }
-    
-    function onReportAudioStop(audioInfo){
-        const audioItem = playedAudioArray.find((i => i.musicSrc === audioInfo.musicSrc));
-        const audioItemIndex = playedAudioArray.findIndex((i => i.musicSrc === audioInfo.musicSrc));
-        const newAudioItem = {
-            ...audioItem,
-            stopped:audioItem.stopped + 1
-        }
-        const newPLayedAudioArray = [
-            ...playedAudioArray.slice(0,audioItemIndex),
-            newAudioItem,
-            ...playedAudioArray.slice(audioItemIndex + 1, playedAudioArray.length)
-        ];
-        setPlayedAudioArray(newPLayedAudioArray);
-        // console.log('stppped - ' + playedAudioArray[audioItemIndex].stopped)
-        if  (playedAudioArray[audioItemIndex].stopped === 0){
-            const audioStopUrl = window.location.href + "/p/" + props.projectId + "/" + "stopmediaviewajax?media_view_id=" + playedAudioArray[audioItemIndex].mediaViewId;
-            $.ajax({url: audioStopUrl}).done(function(res) { 
-            console.log(res);
-            });
-        }
-    }
-
-    let musicPlayerDisplay;
-
-    if (productFiles) {
-
-        const options = {
-            //audio lists model
-            audioLists:productFiles,
-            audioListsPanelVisible:false,
-            //default play index of the audio player  [type `number` default `0`]
-            defaultPlayIndex: 0,
-            //if you want dynamic change current play audio you can change it [type `number` default `0`]
-            // playIndex: 0,
-            //color of the music player theme    [ type `string: 'light' or 'dark'  ` default 'dark' ]
-            theme: "dark",
-            // Specifies movement boundaries. Accepted values:
-            // - `parent` restricts movement within the node's offsetParent
-            //    (nearest node with position relative or absolute), or
-            // - a selector, restricts movement within the targeted node
-            // - An object with `left, top, right, and bottom` properties.
-            //   These indicate how far in each direction the draggable
-            //   can be moved.
-            bounds: "product-"+props.projectId,
-            //Whether to load audio immediately after the page loads.  [type `Boolean | String`, default `false`]
-            //"auto|metadata|none" "true| false"
-            preload: false,
-            //Whether the player's background displays frosted glass effect  [type `Boolean`, default `false`]
-            glassBg: false,
-            //The next time you access the player, do you keep the last state  [type `Boolean` default `false`]
-            remember: false,
-            //The Audio Can be deleted  [type `Boolean`, default `true`]
-            remove: false,
-            //audio controller initial position    [ type `Object` default '{top:0,left:0}' ]
-            defaultPosition: {
-              top: 50,
-              left: 50
-            },
-            // play mode text config of the audio player
-            playModeText: {
-              order: "order",
-              orderLoop: "loop",
-              singleLoop: "single loop",
-              shufflePlay: "shuffle"
-            },
-            //audio controller open text  [ type `String | ReactNode` default 'open']
-            openText: "open",
-            //audio controller close text  [ type `String | ReactNode` default 'close']
-            closeText: "close",
-            //audio theme switch checkedText  [ type `String | ReactNode` default '-']
-            checkedText: "dark",      
-            //audio theme switch unCheckedText [ type `String | ReactNode` default '-']
-            unCheckedText: "light",
-            // audio list panel show text of the playlist has no songs [ type `String` | ReactNode  default 'no music']
-            notContentText: "No Music",
-            panelTitle: "Test",
-            defaultPlayMode: "order",
-            //audio mode        mini | full          [type `String`  default `mini`]
-            mode: "full",
-              // [ type `Boolean` default 'false' ]
-              // The default audioPlay handle function will be played again after each pause, If you only want to trigger it once, you can set 'true'
-            once: true,
-            //Whether the audio is played after loading is completed. [type `Boolean` default 'true']
-            autoPlay: false,
-            //Whether you can switch between two modes, full => mini  or mini => full   [type 'Boolean' default 'true']
-            toggleMode: false,
-            //audio cover is show of the "mini" mode [type `Boolean` default 'true']
-            showMiniModeCover: false,   
-            //audio playing progress is show of the "mini"  mode
-            showMiniProcessBar: false,
-            //audio controller is can be drag of the "mini" mode     [type `Boolean` default `true`]
-            drag: true,
-            //drag the audio progress bar [type `Boolean` default `true`]
-            seeked: false,
-            //audio controller title [type `String | ReactNode`  default <FaHeadphones/>]
-            // controllerTitle: <FaHeadphones />,
-            //Displays the audio load progress bar.  [type `Boolean` default `true`]
-            showProgressLoadBar: false,
-            //play button display of the audio player panel   [type `Boolean` default `true`]
-            showPlay: true,
-            //reload button display of the audio player panel   [type `Boolean` default `true`]
-            showReload: false,
-            //download button display of the audio player panel   [type `Boolean` default `true`]
-            showDownload: false,
-            //loop button display of the audio player panel   [type `Boolean` default `true`]
-            showPlayMode: false,
-            //theme toggle switch  display of the audio player panel   [type `Boolean` default `true`]
-            showThemeSwitch: false,
-            //lyric display of the audio player panel   [type `Boolean` default `false`]
-            showLyric: false,
-            //Extensible custom content       [type 'Array' default '[]' ]
-            extendsContent: [],
-            //default volume of the audio player [type `Number` default `100` range `0-100`]
-            defaultVolume: 50,
-            //playModeText show time [type `Number(ms)` default `700`]
-            playModeShowTime: 600,
-            //Whether to try playing the next audio when the current audio playback fails [type `Boolean` default `true`]
-            loadAudioErrorPlayNext: false,
-            //Music is downloaded handle
-            //onAudioDownload(audioInfo) { console.log("audio download", audioInfo); },
-            //audio play handle
-            onAudioPlay(audioInfo) {
-                console.log('audio play');
-                setShowAudioControls(true);
-                const currentIndex = productFiles.findIndex(f => audioInfo.name === f.title);
-                setPlayIndex(currentIndex + 1);
-                const playBtnElement = document.getElementById('music-player-'+props.projectId).querySelector('span[title="Click to play"]');
-
-                if (window.matchMedia("(max-width: 768px)").matches) {
-                        // createEvent(), event.initEvent() are Depricated see Ref: [enter link description here][1]
-                        // var event = document.createEvent("Event"); 
-                        // event.initEvent("touchstart", false, true);
-                        // event.initEvent("touchend", false, true);
-                        // So the solution is:
-                        var event1 = new Event('touchstart');
-                        var event2 = new Event('touchend'); 
-                        playBtnElement.dispatchEvent(event1); 
-                        playBtnElement.dispatchEvent(event2);
-                } else {
-                    playBtnElement.click();
-                }
-                onReportAudioPlay(audioInfo);
-            },
-            //audio pause handle
-            onAudioPause(audioInfo) { 
-              console.log("audio pause"); 
-              setShowAudioControls(false)
-              onReportAudioStop(audioInfo)
-            },
-            //When the user has moved/jumped to a new location in audio
-            onAudioSeeked(audioInfo) { console.log("audio seeked", audioInfo); },
-            //When the volume has changed  min = 0.0  max = 1.0
-            onAudioVolumeChange(currentVolume) { console.log("audio volume change", currentVolume); },
-            //The single song is ended handle
-            onAudioEnded(audioInfo) { console.log("audio ended", audioInfo); },
-            //audio load abort The target event like {...,audioName:xx,audioSrc:xx,playMode:xx}
-            onAudioAbort(e) { console.log("audio abort", e); },
-            //audio play progress handle
-            onAudioProgress(audioInfo) { 
-                if (audioInfo.paused === false){
-                    $('#music-player-'+props.projectId).find('.play-btn.play').trigger("click");
-                }
-            },
-            //audio reload handle
-            onAudioReload(audioInfo) { 
-                console.log("audio reload:", audioInfo);
-            },
-            //audio load failed error handle
-            onAudioLoadError(e) { 
-                console.log("audio load err", e); 
-            },
-            //theme change handle
-            onThemeChange(theme) { 
-                console.log("theme change:", theme); 
-            },
-            //audio lists change
-            onAudioListsChange(currentPlayId, audioLists, audioInfo) {
-              console.log("[currentPlayId] audio lists change:", currentPlayId);
-              console.log("[audioLists] audio lists change:", audioLists);
-              console.log("[audioInfo] audio lists change:", audioInfo);
-            },
-            onAudioPlayTrackChange(currentPlayId, audioLists, audioInfo) {
-                const currentIndex = productFiles.findIndex(f => audioInfo.name === f.title);
-                setPlayIndex(currentIndex + 1);
-            },
-            onPlayModeChange(playMode) { 
-                console.log("play mode change:", playMode); 
-            },
-            onModeChange(mode) { 
-                console.log("mode change:", mode); 
-            },
-            onAudioListsPanelChange(panelVisible) {
-              /*const newShowPlayListValue = showPlaylist === true ? false : true;
-              setShowPlaylist(newShowPlayListValue);*/
-            }, 
-            onAudioListsDragEnd(fromIndex, endIndex) {
-              console.log("audio lists drag end:", fromIndex, endIndex);
-            },
-            onAudioLyricChange(lineNum, currentLyric) {
-              console.log("audio lyric change:", lineNum, currentLyric);
-            }
-        };
-    
-        musicPlayerDisplay = (
-            <div>
-                <ReactJkMusicPlayer {...options} />
-                <span className="music-player-counter">{playIndex}/{productFiles.length}</span>
-            </div>
-        )
-    }
-
-    let showControlsCssClass = "";
-    if (showAudioControls === true) {
-        showControlsCssClass = "show-controls"
-    }
-
-    return (
-        <div className={"product-browse-item-preview-music-player " + showControlsCssClass} id={"music-player-"+props.projectId}>
-            {musicPlayerDisplay}
-        </div>
-    )
-}
-
 function ProductBrowseItemPreviewMusicPlayerTwo(props){
 
     const [ productFiles, setProductFiles ] = useState(props.productFiles)
+    console.log('files:');
+    console.log(productFiles);
     const [ showAudioControls, setShowAudioControls ] = useState(false);
     const [ playIndex, setPlayIndex ] = useState(0);
     let initialPLayedAudioArray = [];
@@ -553,7 +270,6 @@ function ProductBrowseItemPreviewMusicPlayerTwo(props){
     }
 
     function onReportAudioPlay(src){
-        console.log(src);
         const audioItem = playedAudioArray.find((i => i.musicSrc === src));
         const audioItemIndex = playedAudioArray.findIndex((i => i.musicSrc === src));
         const newAudioItem = {
@@ -566,12 +282,12 @@ function ProductBrowseItemPreviewMusicPlayerTwo(props){
           ...playedAudioArray.slice(audioItemIndex + 1, playedAudioArray.length)
         ];
         setPlayedAudioArray(newPLayedAudioArray);
-        console.log(playedAudioArray);
+
         if (playedAudioArray[audioItemIndex].played === 0){
           const audioStartUrl = window.location.href + "/p/" + props.projectId + "/" + 'startmediaviewajax?collection_id='+audioItem.collection_id+'&file_id='+audioItem.id+'&type_id=2';
-          console.log(audioStartUrl)
+
           $.ajax({url: audioStartUrl}).done(function(res) { 
-            console.log(res);
+
             const newAudioItem = {
               ...audioItem,
               mediaViewId:res.MediaViewId,
@@ -588,7 +304,7 @@ function ProductBrowseItemPreviewMusicPlayerTwo(props){
     }
     
     function onReportAudioStop(src){
-        console.log(src);
+
         const audioItem = playedAudioArray.find((i => i.musicSrc === src));
         const audioItemIndex = playedAudioArray.findIndex((i => i.musicSrc === src));
         const newAudioItem = {
@@ -603,10 +319,10 @@ function ProductBrowseItemPreviewMusicPlayerTwo(props){
         setPlayedAudioArray(newPLayedAudioArray);
         // console.log('stppped - ' + playedAudioArray[audioItemIndex].stopped)
         if  (playedAudioArray[audioItemIndex].stopped === 0){
-            console.log(playedAudioArray);
+
             const audioStopUrl = window.location.href + "/p/" + props.projectId + "/" + "stopmediaviewajax?media_view_id=" + playedAudioArray[audioItemIndex].mediaViewId;
             $.ajax({url: audioStopUrl}).done(function(res) { 
-                console.log(res);
+
             });
         }
     }
