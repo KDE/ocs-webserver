2ad26ff21db3d13790b36d443cc8cec72d32a3e2
siyuan dong <dongsiyuan@gmail.com>
2020-01-17 12:34:27 +0100

    startpage

--- httpdocs/theme/react/hooks-app/opendesktop-home/function/WatchlistContainer.js
+++ httpdocs/theme/react/hooks-app/opendesktop-home/function/WatchlistContainer.js
@@ -4,7 +4,7 @@ function WatchlistContainer(props)
   return (
         <ul id="admin-links-container">
           <li id="product-moderation-link-item">
-            <a href={props.baseUrlStore+"/moderation/productmoderation/"} >
+            <a href={props.baseUrlStore+"/watchlist-productmoderation"} >
               <span>Product moderation</span>
             </a>
           </li>
@@ -31,36 +31,36 @@ function WatchlistContainer(props)
           </li>
 
           <li id="new-products-link-item">
-            <a href={props.baseUrlStore+"/newproducts/"} >
+            <a href={props.baseUrlStore+"/watchlist-newproducts"} >
               <span>Most New Products</span>
             </a>
           </li>
 
           <li id="product-files-link-item">
-            <a href={props.baseUrlStore+"/products-10-files"} >
+            <a href={props.baseUrlStore+"/watchlist-products-10-files"} >
               <span>New Products with min.10 files</span>
             </a>
           </li>
 
           <li id="same-paypal-link-item">
-            <a href={props.baseUrlStore+"/samepaypal"} >
+            <a href={props.baseUrlStore+"/watchlist-samepaypal"} >
               <span>Same paypal check</span>
             </a>
           </li>
 
           <li id="same-paypal-link-item">
-            <a href={props.baseUrlStore+"/spam/mdsum"} >
+            <a href={props.baseUrlStore+"/watchlist-md5sum-duplicated"} >
               <span>File uploaded md5sum duplicated</span>
             </a>
           </li>
 
           <li id="unpublished-link-item">
-            <a href={props.baseUrlStore+"/spam/unpublishedproduct"} >
+            <a href={props.baseUrlStore+"/watchlist-unpublishedproduct"} >
               <span>List of unpublished products</span>
             </a>
           </li>
           <li id="list-newproduct-link-item">
-            <a href={props.baseUrlStore+"/spam/newproduct"} >
+            <a href={props.baseUrlStore+"/watchlist-newproduct-2-month"} >
               <span>List of new products (published or unpublished) less than 2 months </span>
             </a>
           </li>
