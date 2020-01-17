a700efaf1709ed3e0bc9adaf8c6fb5767d534ee5
Ronald von Schlegell <rvs75@email.de>
2020-01-17 15:09:22 +0100

    portal user

--- application/modules/portal/controllers/UserController.php
+++ application/modules/portal/controllers/UserController.php
@@ -24,10 +24,43 @@
  */
 class Portal_UserController extends Local_Controller_Action_Portal
 {
-
+    protected $_memberId;
+    protected $_userName;
+    
+    
     public function indexAction()
     {
-        $dummy = 1;
+        if ($this->hasParam('user_name')) {
+            $this->_userName = $this->getParam('user_name');
+            $this->_userName = urldecode($this->_userName);
+
+            $modelMember = new Default_Model_Member();
+            $this->_memberId = $modelMember->fetchActiveUserByUsername($this->_userName);
+        } else {
+            $this->_memberId = (int)$this->getParam('member_id');
+        }
+        
+        $tableMember = new Default_Model_Member();
+        $tableProject = new Default_Model_Project();
+        $earnModel = new Default_Model_StatDownload();
+        $helperUserRole = new Backend_View_Helper_UserRole();
+        $pageLimit = 500;
+
+        $this->view->authMember = $this->_authMember;
+        $this->view->member = $tableMember->fetchMemberData($this->_memberId);
+        
+        if (null == $this->view->member) {
+            $this->redirect("/");
+        }
+        if ($this->view->member->is_deleted == 1 or $this->view->member->is_active == 0) {
+            $this->redirect("/");
+        }
+
+        $this->view->headTitle($this->view->member->username . ' - ' . $this->getHeadTitle(), 'SET');
+        $this->view->mainProject = $this->view->member->findDependentRowset($tableProject, 'MainProject')->current();
+
+        $this->view->userProjectCategories = $tableProject->getUserCreatingCategorys($this->_memberId);
+        $this->view->aboutmeUserInfo = $this->getAboutmeUserInfo($this->_memberId, $this->view->member->username);
     }
 
 }
