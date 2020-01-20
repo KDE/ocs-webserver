511b7298ca7079ca6a577fcf3d450f0fa6635699
siyuan dong <dongsiyuan@gmail.com>
2020-01-20 12:47:54 +0100

    solr search

--- application/modules/default/controllers/JsonController.php
+++ application/modules/default/controllers/JsonController.php
@@ -262,6 +262,10 @@ class JsonController extends Zend_Controller_Action
         if($this->hasParam('s')){
             $store = $this->getParam('s');
         }
+        if($store=='Opendesktop')
+        {
+            $store = null;
+        }
 
         // $storemodel = new Default_Model_DbTable_ConfigStore(); 
         // $s = $storemodel->fetchDomainObjectsByName($store);
