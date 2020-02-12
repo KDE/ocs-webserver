5f5a82c16786021f4562007af9286d50611c3b1f
Alexander Schmidt <2f5ed1c5@opayq.com>
2020-02-12 15:56:01 +0100

    updated json controller

--- application/modules/default/controllers/JsonController.php
+++ application/modules/default/controllers/JsonController.php
@@ -463,15 +463,14 @@ class JsonController extends Zend_Controller_Action
     }
 
     public function newsAction()
-    {        
+    {
         $this->_initResponseHeader();
 
+        /** @var Zend_Cache_Backend_Memcached $cache */
         $cache = Zend_Registry::get('cache');
         $cacheName = __FUNCTION__;
-        if (false !== ($news = $cache->load($cacheName))) {
-            $results=$news;
-        }else{
-            $url = 'https://blog.opendesktop.org/?json=1';        
+        if (false === ($news = $cache->load($cacheName))) {
+            $url = 'https://blog.opendesktop.org/?json=1';
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_AUTOREFERER, true);
             curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -480,11 +479,12 @@ class JsonController extends Zend_Controller_Action
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
             $data = curl_exec($ch);
             curl_close($ch);
-            $results = json_decode($data);  
-            $results->posts = array_slice($results->posts,0,3);
-            $cache->save($results, $cacheName, array(), 60*60);
-        }                
-        $this->_sendResponse($results, $this->_format);
+            $news = json_decode($data);
+            $news->posts = array_slice($news->posts, 0, 3);
+            $cache->save($news, $cacheName, array(), 60 * 60);
+        }
+
+        $this->_sendResponse($news, $this->_format);
     }
 
 
