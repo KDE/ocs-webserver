3038d10096d32ff75144dfb2857a275ec81f6d1c
siyuan dong <dongsiyuan@gmail.com>
2020-02-12 14:37:03 +0100

    home

--- application/modules/default/controllers/JsonController.php
+++ application/modules/default/controllers/JsonController.php
@@ -420,7 +420,39 @@ class JsonController extends Zend_Controller_Action
         $this->_sendResponse($results, $this->_format);
     }
 
-   
+    public function socialtimelineAction()
+    {
+        $this->_initResponseHeader();
+        $cache = Zend_Registry::get('cache');
+        $cacheName = __FUNCTION__;        
+        
+        if (false !== ($news = $cache->load($cacheName))) {
+            $results=$news;
+        }else{
+            $config = Zend_Registry::get('config')->settings->client->default;
+            $url_mastodon = $config->url_mastodon;
+            $url = $url_mastodon.'/api/v1/timelines/public?limit=5';        
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            $data = curl_exec($ch);
+            curl_close($ch);
+            $results = json_decode($data);   
+
+
+            $helpPrintDate = new Default_View_Helper_PrintDateSince();
+            foreach ($results as &$m) {                                              
+                $m->created_at = $helpPrintDate->printDateSince(str_replace('T', ' ', substr($m->created_at, 0, 19)));                
+            }
+            
+            
+            $cache->save($results, $cacheName, array(), 60*60);
+        }                   
+        $this->_sendResponse($results, $this->_format);
+    }
 
     public function newsAction()
     {        
