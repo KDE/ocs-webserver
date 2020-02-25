62e439ffc45a13a4b305b2ede18bd08c82685f5d
siyuan dong <dongbeibei@hotmail.com>
2020-02-25 10:36:49 +0100

    homecontroller

--- application/modules/default/controllers/HomeController.php
+++ application/modules/default/controllers/HomeController.php
@@ -44,10 +44,9 @@ class HomeController extends Local_Controller_Action_DomainSwitch
                 } else {
 
                     if ($storeConfig->config_id_name=='opendesktop' && !Zend_Auth::getInstance()->hasIdentity()){
-                        
-                        $this->_helper->viewRenderer('index-' . $storeConfig->config_id_name);
-                    }else{
                         $this->forward('start', 'home', 'default',null);
+                    }else{
+                        $this->_helper->viewRenderer('index-' . $storeConfig->config_id_name);
                     }                            
                 }
 
