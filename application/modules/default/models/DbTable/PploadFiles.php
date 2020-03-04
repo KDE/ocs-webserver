e4fc13dc1b994fa2917cfac8d479375b58902287
Alexander Schmidt <2f5ed1c5@opayq.com>
2020-03-04 10:16:59 +0100

    PploadFiles unblocking select for download count

--- application/modules/default/models/DbTable/PploadFiles.php
+++ application/modules/default/models/DbTable/PploadFiles.php
@@ -130,9 +130,9 @@ class Default_Model_DbTable_PploadFiles extends Local_Model_Table
         $today = (new DateTime())->modify('-1 day');
         $filterDownloadToday = $today->format("Y-m-d H:i:s");
 
-        $this->_db->beginTransaction();
-        
         $this->_db->query("SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED")->execute();
+
+        $this->_db->beginTransaction();
         
         $sql = "    SELECT COUNT(1) AS cnt
                     FROM ppload.ppload_files_downloaded_unique f
