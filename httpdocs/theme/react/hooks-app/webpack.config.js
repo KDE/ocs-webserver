7466efed4eed3739faacbb316bbb6faa1070c3b4
david nelband <dnelband@gmail.com>
2020-05-06 16:11:33 +0200

    update for entry metaheader

--- httpdocs/theme/react/hooks-app/webpack.config.js
+++ httpdocs/theme/react/hooks-app/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: {
-          //'metaheader':'./metaheader/entry-metaheader.js',
-          'metaheader-local':'./metaheader/entry-metaheader-local.js',  
+          'metaheader':'./metaheader/entry-metaheader.js',
+          //'metaheader-local':'./metaheader/entry-metaheader-local.js',  
           //'home-main-container':'./opendesktop-home/entry-home-main-container.js',
           //'header':'./header/entry-header.js',       
           //'category-tree':'./category-tree/entry-category-tree.js',   
