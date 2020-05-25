dcd08ca544f376ac3dc37fad6d696f087c5fd3a2
david nelband <dnelband@gmail.com>
2020-05-25 20:03:30 +0200

    test commit

--- httpdocs/theme/react/hooks-app/webpack.config.js
+++ httpdocs/theme/react/hooks-app/webpack.config.js
@@ -1,3 +1,21 @@
+const entries = {
+  //'app-product-relationship':'./modules/app-product-relationship/entry-app-product-relationship.js,
+  //'app-supporters':'./modules/app-supporters/entry-app-supporters.js',
+  //'category-blocks':'./modules/category-blocks/entry-category-blocks.js',
+  //'category-tree':'./modules/category-tree/entry-category-tree.js',
+  //'header':'./modules/header/entry-header.js',
+  //'metaheader':'./modules/metaheader/entry-metaheader.js',
+  //'metaheader-local':'./modules/metaheader/entry-metaheader-local.js',  
+  //'home-main-container':'./modules/opendesktop-home/entry-home-main-container.js',
+  // 'pling-section':'./modules/pling-section/entry-pling-section.js'
+  //'portal-index':'./modules/portal-index/protal-index.js,
+  //'product-browse':'./modules/product-browse/entry-product-browse.js',
+  'product-media-slider':'./modules/product-media-slider/entry-product-media-slider.js',
+  //'tag-rating':'./modules/tag-rating/entry-tag-rating.js
+}
+
+const entryFileNames = Object.keys(entries);
+
 module.exports = {
   entry: {
           //'app-product-relationship':'./modules/app-product-relationship/entry-app-product-relationship.js,
@@ -8,15 +26,17 @@ module.exports = {
           //'metaheader':'./modules/metaheader/entry-metaheader.js',
           //'metaheader-local':'./modules/metaheader/entry-metaheader-local.js',  
           //'home-main-container':'./modules/opendesktop-home/entry-home-main-container.js',
-          'pling-section':'./modules/pling-section/entry-pling-section.js'
+          // 'pling-section':'./modules/pling-section/entry-pling-section.js'
           //'portal-index':'./modules/portal-index/protal-index.js,
           //'product-browse':'./modules/product-browse/entry-product-browse.js',
-          //'product-media-slider':'./modules/product-media-slider/entry-product-media-slider.js',
+          'product-media-slider':'./modules/product-media-slider/entry-product-media-slider.js',
           //'tag-rating':'./modules/tag-rating/entry-tag-rating.js
         },
   output: {
      path: `${__dirname}/../bundle`,
-    filename: '[name]-bundle.js'
+     filename: '[name]-bundle.js',
+     chunkFilename: 'chunks/' + entryFileNames[0] + '/[id].js',
+     publicPath: '/theme/react/bundle/'
   },
   module: {
     rules: [
